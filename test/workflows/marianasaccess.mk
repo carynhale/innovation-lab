1f86dbc58dbebed190e833964bc0d0f0b46a8902
David Brown <ndbrown6@users.noreply.github.com>
2019-08-14 14:24:40 -0400

    MSK-ACCESS

--- test/workflows/marianasaccess.mk
+++ test/workflows/marianasaccess.mk
@@ -1,11 +1,11 @@
 include modules/Makefile.inc
 
 LOGDIR ?= log/marianas_access.$(NOW)
-PHONY += bam marianas cnvaccess
+PHONY += marianas
 
 MSK_ACCESS_WORKFLOW += clip_umi
-MSK_ACCESS_WORKFLOW += align_fastq
-MSK_ACCESS_WORKFLOW += umi_collapse
+#MSK_ACCESS_WORKFLOW += align_fastq
+#MSK_ACCESS_WORKFLOW += umi_collapse
 
 msk_access_workflow : $(MSK_ACCESS_WORKFLOW)
 
