e61193eb34858c80be4a00f8a92a0bd202e66c86
David Brown <ndbrown6@users.noreply.github.com>
2019-08-15 18:18:05 -0400

    Update aligncollapsed.mk

--- test/bam_tools/aligncollapsed.mk
+++ test/bam_tools/aligncollapsed.mk
@@ -15,7 +15,12 @@ SAMTOOLS_MEM_THREAD = 2G
 GATK_THREADS = 8
 GATK_MEM_THREAD = 2G
 
-align_collapsed : $(foreach sample,$(SAMPLES),marianas/$(sample)/$(sample).collapsed.fixed.bam)
+JAVA = /home/${USER}/share/usr/jdk1.8.0_74/bin/java
+MARIANAS = /home/${USER}/share/usr/marianas-1.8.1/Marianas-1.8.1.jar
+WALTZ = /home/${USER}/share/usr/marianas-1.8.1/Waltz-3.0.jar
+BED_FILE=/home/${USER}/share/reference/target_panels/MSK-ACCESS-v1_0-probe-AB.waltz.bed
+
+align_collapsed : $(foreach sample,$(SAMPLES),marianas/$(sample)/timestamp)
 
 define fastq-to-bam
 marianas/$1/$1.collapsed.bwamem.bam : marianas/$1/collapsed_R1_.fastq marianas/$1/collapsed_R1_.fastq
@@ -40,47 +45,12 @@ marianas/$1/$1.collapsed.fixed.bam : marianas/$1/$1.collapsed.sorted.bam
 									  		   CREATE_INDEX=true \
 									  		   VALIDATION_STRINGENCY=LENIENT")
 
-# marianas/$1/$1.intervals : marianas/$1/$1.fixed.bam
-#	$$(call RUN,-c -n $(GATK_THREADS) -s 1G -m $(GATK_MEM_THREAD) -w 1440,"set -o pipefail && \
-#									   							   		   /home/$(USER)/share/usr/jdk1.8.0_121/bin/java -Djava.io.tmpdir=$(TMPDIR) -Xms1G -Xmx12G -jar /home/$(USER)/share/usr/lib/java/GenomeAnalysisTK-3.7.jar \
-#									   							   		   -S LENIENT -T RealignerTargetCreator -I $$(^) -nt 8 -R $(REF_FASTA) -o $$(@) --known /home/$(USER)/share/reference/GATK_bundle/2.3/Mills_and_1000G_gold_standard.indels.b37.vcf.gz")
-#
-# marianas/$1/$1.realn.bam : marianas/$1/$1.sorted.bam marianas/$1/$1.intervals
-#	$$(call RUN,-c -n $(GATK_THREADS) -s 1G -m $(GATK_MEM_THREAD) -w 1440,"set -o pipefail && \
-#									   							   		   /home/$(USER)/share/usr/jdk1.8.0_121/bin/java -Djava.io.tmpdir=$(TMPDIR) -Xms1G -Xmx12G -jar /home/$(USER)/share/usr/lib/java/GenomeAnalysisTK-3.7.jar \
-#									   							   		   -S LENIENT -T IndelRealigner -I $$(<) -R $(REF_FASTA) -targetIntervals $$(<<) -o $$(@) --knownAlleles /home/brownd7/share/reference/GATK_bundle/2.3/Mills_and_1000G_gold_standard.indels.b37.vcf.gz")
-#									   							   
-# marianas/$1/$1.dedup.bam : marianas/$1/$1.realn.bam
-#	$$(call RUN, -c -n 1 -s 12G -m 18G -w 1440,"java -Djava.io.tmpdir=$$(TMPDIR) -Xms2G -Xmx16G -jar $$(PICARD_JAR) MarkDuplicates \
-#												I=$$(<) \
-#												O=$$(@) \
-#												M=marianas/$1/$1.txt \
-#												TMP_DIR=$(TMPDIR)")
-#
-# marianas/$1/$1.recal.grp : marianas/$1/$1.dedup.bam
-#	$$(call RUN,-c -n $(GATK_THREADS) -s 1G -m $(GATK_MEM_THREAD) -w 1440,"set -o pipefail && \
-#																		   samtools index $$(<) && \
-#									   							   		   /home/$(USER)/share/usr/jdk1.8.0_121/bin/java -Djava.io.tmpdir=$(TMPDIR) -Xms1G -Xmx12G -jar /home/$(USER)/share/usr/lib/java/GenomeAnalysisTK-3.7.jar \
-#									   							   		   -S LENIENT -T BaseRecalibrator -R $(REF_FASTA) -knownSites /home/brownd7/share/reference/dbsnp_138.b37.gmaf.vcf.gz \
-#									   							   		   -I $$(<) -o $$(@)")
-#
-# marianas/$1/$1.recal.bam : marianas/$1/$1.dedup.bam marianas/$1/$1.recal.grp
-#	$$(call RUN,-c -n $(GATK_THREADS) -s 1G -m $(GATK_MEM_THREAD) -w 1440,"set -o pipefail && \
-#									   							   		   /home/$(USER)/share/usr/jdk1.8.0_121/bin/java -Djava.io.tmpdir=$(TMPDIR) -Xms1G -Xmx12G -jar /home/$(USER)/share/usr/lib/java/GenomeAnalysisTK-3.7.jar \
-#									   							   		   -S LENIENT -T PrintReads -R $(REF_FASTA) -I $$(<) -BQSR $$(<<) -o $$(@)")
-#
-# marianas/$1/$1.bam : marianas/$1/$1.recal.bam
-#	$$(call RUN, -c -n 1 -s 12G -m 18G -w 1440,"java -Djava.io.tmpdir=$(TMPDIR) -Xms2G -Xmx16G -jar $$(PICARD_JAR) AddOrReplaceReadGroups \
-#												I=$$(<) \
-#												O=$$(@) \
-#												RGID=$1 \
-#												RGLB=$1 \
-#												RGPL=illumina \
-#												RGPU=NA \
-#												RGSM=$1 \
-#												TMP_DIR=$(TMPDIR) && \
-#												samtools index $$(@) && \
-#												cp marianas/$1/$1.bam.bai marianas/$1/$1.bai")
+marianas/$1/timestamp : marianas/$1/$1.collapsed.fixed.bam
+	$$(call RUN,-c -n 1 -s 8G -m 12G,"set -o pipefail && \
+									  cd marianas/$1 && \
+									  $(JAVA) -server -Xms2G -Xmx8G -cp $(MARIANAS) org.mskcc.marianas.umi.duplex.postprocessing.SeparateBams $1.collapsed.fixed.bam && \
+									  touch timestamp && \
+									  cd ../..")
 
 endef
 $(foreach sample,$(SAMPLES),\
