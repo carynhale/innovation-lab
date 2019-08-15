42a4d9f2c6c61c5bb7cde80918643be6e2dfa62b
David Brown <ndbrown6@users.noreply.github.com>
2019-08-15 18:06:01 -0400

    Update aligncollapsed.mk

--- test/bam_tools/aligncollapsed.mk
+++ test/bam_tools/aligncollapsed.mk
@@ -15,19 +15,19 @@ SAMTOOLS_MEM_THREAD = 2G
 GATK_THREADS = 8
 GATK_MEM_THREAD = 2G
 
-align_collapsed : $(foreach sample,$(SAMPLES),marianas/$(sample)/$(sample).collapsed.bwamem.bam)
+align_collapsed : $(foreach sample,$(SAMPLES),marianas/$(sample)/$(sample).collapsed.sorted.bam)
 
 define fastq-to-bam
 marianas/$1/$1.collapsed.bwamem.bam : marianas/$1/collapsed_R1_.fastq marianas/$1/collapsed_R1_.fastq
 	$$(call RUN,-c -n $(BWAMEM_THREADS) -s 1G -m $(BWAMEM_MEM_PER_THREAD) -w 1440,"set -o pipefail && \
 																		           $(BWA) mem -t $(BWAMEM_THREADS) $(BWA_ALN_OPTS) -R \"@RG\tID:$1\tLB:$1\tPL:${SEQ_PLATFORM}\tSM:$1\" $(BWAMEM_REF_FASTA) $$(^) | $(SAMTOOLS) view -bhS - > $$(@)")
 																		           
-# marianas/$1/$1.collapsed.sorted.bam : marianas/$1/$1.collapsed.bwamem.bam
-#	$$(call RUN,-c -n $(SAMTOOLS_THREADS) -s 1G -m $(SAMTOOLS_MEM_THREAD) -w 1440,"set -o pipefail && \
-#									  									   		   samtools sort -@ $(SAMTOOLS_THREADS) -m $(SAMTOOLS_MEM_THREAD) $$(^) -o $$(@) -T $(TMPDIR) && \
-#									  									   		   samtools index $$(@) && \
-#									  									   		   cp marianas/$1/$1.collapsed.sorted.bam.bai marianas/$1/$1.collapsed.sorted.bai")
-#									  									   		   
+marianas/$1/$1.collapsed.sorted.bam : marianas/$1/$1.collapsed.bwamem.bam
+$$(call RUN,-c -n $(SAMTOOLS_THREADS) -s 1G -m $(SAMTOOLS_MEM_THREAD) -w 1440,"set -o pipefail && \
+								  									   		   samtools sort -@ $(SAMTOOLS_THREADS) -m $(SAMTOOLS_MEM_THREAD) $$(^) -o $$(@) -T $(TMPDIR) && \
+								  									   		   samtools index $$(@) && \
+								  									   		   cp marianas/$1/$1.collapsed.sorted.bam.bai marianas/$1/$1.collapsed.sorted.bai")
+								  									   		   
 # marianas/$1/$1.collapsed.fixed.bam : marianas/$1/$1.collapsed.sorted.bam
 #	$$(call RUN,-c -n 1 -s 12G -m 18G -w 1440,"set -o pipefail && \
 #									  		   /home/${USER}/share/usr/jdk1.8.0_74/bin/java -Djava.io.tmpdir=$(TMPDIR) -Xms2G -Xmx16G -jar /home/${USER}/share/usr/picard/bin/picard.jar \
