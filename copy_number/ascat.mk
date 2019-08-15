19e8223a7bfdab942aed910b10b95d9a28e58976
David Brown <ndbrown6@users.noreply.github.com>
2019-08-15 15:55:25 -0400

    Update ascat.mk

--- copy_number/ascat.mk
+++ copy_number/ascat.mk
@@ -3,11 +3,20 @@ include modules/Makefile.inc
 LOGDIR ?= log/ascat.$(NOW)
 PHONY += ascat ascat/log2 ascat/bafall ascat/bafhet ascat/mad ascat/log2nbaf ascat/ascat ascat/total ascat/bychr
 
-ascat : $(foreach pair,$(SAMPLE_PAIRS),ascat/log2/$(pair).pdf ascat/bafall/$(pair).pdf ascat/bafhet/$(pair).pdf ascat/mad/$(pair).RData ascat/log2nbaf/$(pair).pdf ascat/ascat/$(pair).pdf ascat/total/$(pair).pdf ascat/bychr/$(pair)/timestamp)
+ascat : $(foreach pair,$(SAMPLE_PAIRS),ascat/log2/$(pair).pdf \
+									   ascat/bafall/$(pair).pdf \
+									   ascat/bafhet/$(pair).pdf \
+									   ascat/mad/$(pair).RData \
+									   ascat/log2nbaf/$(pair).pdf \
+									   ascat/ascat/$(pair).pdf \
+									   ascat/total/$(pair).pdf \
+									   ascat/bychr/$(pair)/timestamp)
 
 define ascat-plot-log2
 ascat/log2/$1_$2.pdf : facets/cncf/$1_$2.Rdata
-	$$(call RUN,-c -v $(ASCAT_ENV) -s 1G -m 2G,"$(RSCRIPT) modules/copy_number/ascat.R --type log2 --file_in $$< --file_out ascat/log2/$1_$2.pdf")
+	$$(call RUN,-c -v $(ASCAT_ENV) -s 1G -m 2G,"$(RSCRIPT) modules/copy_number/ascat.R --type log2 \
+												--file_in $$< \
+												--file_out ascat/log2/$1_$2.pdf")
 	
 endef
 $(foreach pair,$(SAMPLE_PAIRS),\
@@ -15,7 +24,9 @@ $(foreach pair,$(SAMPLE_PAIRS),\
 		
 define ascat-plot-bafall
 ascat/bafall/$1_$2.pdf : facets/cncf/$1_$2.Rdata
-	$$(call RUN,-c -v $(ASCAT_ENV) -s 1G -m 2G,"$(RSCRIPT) modules/copy_number/ascat.R --type bafall --file_in $$< --file_out ascat/bafall/$1_$2.pdf")
+	$$(call RUN,-c -v $(ASCAT_ENV) -s 1G -m 2G,"$(RSCRIPT) modules/copy_number/ascat.R --type bafall \
+												--file_in $$< \
+												--file_out ascat/bafall/$1_$2.pdf")
 	
 endef
 $(foreach pair,$(SAMPLE_PAIRS),\
@@ -23,7 +34,9 @@ $(foreach pair,$(SAMPLE_PAIRS),\
 
 define ascat-plot-bafhet
 ascat/bafhet/$1_$2.pdf : facets/cncf/$1_$2.Rdata
-	$$(call RUN,-c -v $(ASCAT_ENV) -s 1G -m 2G,"$(RSCRIPT) modules/copy_number/ascat.R --type bafhet --file_in $$< --file_out ascat/bafhet/$1_$2.pdf")
+	$$(call RUN,-c -v $(ASCAT_ENV) -s 1G -m 2G,"$(RSCRIPT) modules/copy_number/ascat.R --type bafhet \
+												--file_in $$< \
+												--file_out ascat/bafhet/$1_$2.pdf")
 	
 endef
 $(foreach pair,$(SAMPLE_PAIRS),\
@@ -31,7 +44,10 @@ $(foreach pair,$(SAMPLE_PAIRS),\
 		
 define ascat-aspcf
 ascat/mad/$1_$2.RData : facets/cncf/$1_$2.Rdata
-	$$(call RUN,-c -v $(ASCAT_ENV) -s 3G -m 6G,"$(RSCRIPT) modules/copy_number/ascat.R --type aspcf --file_in $$< --file_out ascat/mad/$1_$2.RData --gamma '$${aspcf_gamma.$1}'")
+	$$(call RUN,-c -v $(ASCAT_ENV) -s 3G -m 6G,"$(RSCRIPT) modules/copy_number/ascat.R --type aspcf \
+												--file_in $$< \
+												--file_out ascat/mad/$1_$2.RData \
+												--gamma '$${aspcf_gamma.$1}'")
 	
 endef
 $(foreach pair,$(SAMPLE_PAIRS),\
@@ -39,7 +55,11 @@ $(foreach pair,$(SAMPLE_PAIRS),\
 
 define ascat-plot-aspcf
 ascat/log2nbaf/$1_$2.pdf : ascat/mad/$1_$2.RData
-	$$(call RUN,-c -v $(ASCAT_ENV) -s 3G -m 6G,"$(RSCRIPT) modules/copy_number/ascat.R --type plot-aspcf --file_in $$< --file_out ascat/log2nbaf/$1_$2.pdf --nlog2 '$${aspcf_nlog2.$1}' --nbaf '$${aspcf_nbaf.$1}'")
+	$$(call RUN,-c -v $(ASCAT_ENV) -s 3G -m 6G,"$(RSCRIPT) modules/copy_number/ascat.R --type plot-aspcf \
+												--file_in $$< \
+												--file_out ascat/log2nbaf/$1_$2.pdf \
+												--nlog2 '$${aspcf_nlog2.$1}' \
+												--nbaf '$${aspcf_nbaf.$1}'")
 	
 endef
 $(foreach pair,$(SAMPLE_PAIRS),\
@@ -47,10 +67,18 @@ $(foreach pair,$(SAMPLE_PAIRS),\
 		
 define ascat-run-ascat
 ascat/ascat/$1_$2.pdf : ascat/mad/$1_$2.RData
-	$$(call RUN,-c -v $(ASCAT_ENV) -s 3G -m 6G,"$(RSCRIPT) modules/copy_number/ascat.R --type run-ascat --file_in $$< --file_out ascat/ascat/$1_$2.pdf --rho '$${ascat_rho.$1}' --psi '$${ascat_psi.$1}' --nlog2 '$${aspcf_nlog2.$1}' --nbaf '$${aspcf_nbaf.$1}'")
+	$$(call RUN,-c -v $(ASCAT_ENV) -s 3G -m 6G,"$(RSCRIPT) modules/copy_number/ascat.R --type run-ascat \
+												--file_in $$< \
+												--file_out ascat/ascat/$1_$2.pdf \
+												--rho '$${ascat_rho.$1}' \
+												--psi '$${ascat_psi.$1}' \
+												--nlog2 '$${aspcf_nlog2.$1}' \
+												--nbaf '$${aspcf_nbaf.$1}'")
 	
 ascat/total/$1_$2.pdf : facets/cncf/$1_$2.Rdata ascat/ascat/$1_$2.pdf
-	$$(call RUN,-c -v $(ASCAT_ENV) -s 6G -m 12G,"$(RSCRIPT) modules/copy_number/ascat.R --type total-copy --file_in $$< --file_out ascat/total/$1_$2.pdf")	
+	$$(call RUN,-c -v $(ASCAT_ENV) -s 6G -m 12G,"$(RSCRIPT) modules/copy_number/ascat.R --type total-copy \
+												 --file_in $$< \
+												 --file_out ascat/total/$1_$2.pdf")	
 
 endef
 
@@ -61,7 +89,9 @@ define ascat-plot-chr
 ascat/bychr/$1_$2/timestamp : facets/cncf/$1_$2.Rdata ascat/ascat/$1_$2.pdf
 	$$(call RUN, -v $(ASCAT_ENV) -s 6G -m 12G,"mkdir -p ascat/bychr/ && \
 											   mkdir -p ascat/bychr/$1_$2 && \
-											   $(RSCRIPT) modules/copy_number/ascat.R --type plot-chr --file_in $$< --file_out ascat/bychr/$1_$2")
+											   $(RSCRIPT) modules/copy_number/ascat.R --type plot-chr \
+											   --file_in $$< \
+											   --file_out ascat/bychr/$1_$2")
 		
 endef
 
