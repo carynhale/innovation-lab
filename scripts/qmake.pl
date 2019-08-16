dd6b94cd99a9eddbff71c9e0662ae2129ca71fef
David Brown <ndbrown6@users.noreply.github.com>
2019-08-16 13:41:12 -0400

    Update qmake.pl

--- scripts/qmake.pl
+++ scripts/qmake.pl
@@ -154,7 +154,7 @@ do {
         if ($opt{s} && ($retcode == 0 || $n == 0 || $n + 1 == $attempts)) {
             if ($retcode == 0) {
                 # op success
-                my $slack_msg = "*COMPLETED* $name :+1:";
+                my $slack_msg = "*COMPLETED* $name :ok_hand:";
                 &slack($fin_slack, "$pipeline_channel_msg $slack_msg");
                 &slack($opt{c}, $slack_msg) if $opt{c};
             } else {
@@ -162,7 +162,7 @@ do {
                 my $slack_msg = "*FAILED* $cwd/$logfile";
                 if ($n + 1 == $attempts) {
                     # final attempt
-                    $slack_msg = "$slack_msg :-1:";
+                    $slack_msg = "$slack_msg :middle_finger:";
                     &slack($opt{c}, $slack_msg) if $opt{c};
                 }
                 &slack($err_slack, "$pipeline_channel_msg $slack_msg");
