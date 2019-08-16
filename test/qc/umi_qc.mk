51c0b9a861441d3bc6db675e128b7851b43fdda0
David Brown <ndbrown6@users.noreply.github.com>
2019-08-16 15:05:13 -0400

    Update umi_qc.mk
    Fixed typo

--- test/qc/umi_qc.mk
+++ test/qc/umi_qc.mk
@@ -4,14 +4,14 @@ LOGDIR ?= log/umi_qc.$(NOW)
 PHONY += marianas
 
 umi_qc : $(foreach sample,$(SAMPLES),marianas/$(sample)/umi-info.RData) \
-		 $(foreach sample,$(SAMPLES),marianas/$(sample)/umi-compostite.pdf) \
+		 $(foreach sample,$(SAMPLES),marianas/$(sample)/umi-composite.pdf) \
 
 define umi-frequencies
 marianas/$1/umi-info.RData : marianas/$1/umi-frequencies.txt
 	$$(call RUN,-c -n 1 -s 4G -m 6G ,"set -o pipefail && \
 									 $(RSCRIPT) modules/test/qc/umi_qc.R --type 0 --sample_name $1")
 
-marianas/$1/umi-compostite.pdf : marianas/$1/umi-info.RData	
+marianas/$1/umi-composite.pdf : marianas/$1/umi-info.RData	
 	$$(call RUN,-c -n 1 -s 4G -m 6G ,"set -o pipefail && \
 									 $(RSCRIPT) modules/test/qc/umi_qc.R --type 1 --sample_name $1")
 																           
