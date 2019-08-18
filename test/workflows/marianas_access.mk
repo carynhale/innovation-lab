9882bc84f43fdb0e3171f3b3040db3c3438d8d1e
David Brown <ndbrown6@users.noreply.github.com>
2019-08-18 16:06:47 -0400

    Update marianas_access.mk

--- test/workflows/marianas_access.mk
+++ test/workflows/marianas_access.mk
@@ -6,7 +6,7 @@ PHONY += marianas
 MSK_ACCESS_WORKFLOW += clip_umi
 MSK_ACCESS_WORKFLOW += align_fastq
 MSK_ACCESS_WORKFLOW += umi_collapse
-#MSK_ACCESS_WORKFLOW += align_collapsed
+MSK_ACCESS_WORKFLOW += align_collapsed
 #MSK_ACCESS_WORKFLOW += umi_qc
 
 msk_access_workflow : $(MSK_ACCESS_WORKFLOW)
