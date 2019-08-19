6bcbd666dfa96d5f56bc97071e5c4696023ae15e
David Brown <ndbrown6@users.noreply.github.com>
2019-08-19 18:06:40 -0400

    Marianas
    Changed sort order

--- test/bam_tools/alignfastq.mk
+++ test/bam_tools/alignfastq.mk
@@ -50,23 +50,23 @@ marianas/$1/$1.realn.bam : marianas/$1/$1.sorted.bam marianas/$1/$1.intervals
 									   							   		   /home/$(USER)/share/usr/jdk1.8.0_121/bin/java -Djava.io.tmpdir=$(TMPDIR) -Xms1G -Xmx12G -jar /home/$(USER)/share/usr/lib/java/GenomeAnalysisTK-3.7.jar \
 									   							   		   -S LENIENT -T IndelRealigner -I $$(<) -R $(REF_FASTA) -targetIntervals $$(<<) -o $$(@) --knownAlleles /home/brownd7/share/reference/GATK_bundle/2.3/Mills_and_1000G_gold_standard.indels.b37.vcf.gz")
 									   							   
-# marianas/$1/$1.dedup.bam : marianas/$1/$1.realn.bam
-#	$$(call RUN, -c -n 1 -s 12G -m 18G -w 1440,"java -Djava.io.tmpdir=$$(TMPDIR) -Xms2G -Xmx16G -jar $$(PICARD_JAR) MarkDuplicates \
-#												I=$$(<) \
-#												O=$$(@) \
-#												M=marianas/$1/$1.txt \
-#												REMOVE_DUPLICATES=false \
-#												ASSUME_SORTED=true \
-#												TMP_DIR=$(TMPDIR)")
+marianas/$1/$1.dedup.bam : marianas/$1/$1.realn.bam
+	$$(call RUN, -c -n 1 -s 12G -m 18G -w 1440,"java -Djava.io.tmpdir=$$(TMPDIR) -Xms2G -Xmx16G -jar $$(PICARD_JAR) MarkDuplicates \
+												I=$$(<) \
+												O=$$(@) \
+												M=marianas/$1/$1.txt \
+												REMOVE_DUPLICATES=false \
+												ASSUME_SORTED=true \
+												TMP_DIR=$(TMPDIR)")
 												
-marianas/$1/$1.recal.grp : marianas/$1/$1.realn.bam
+marianas/$1/$1.recal.grp : marianas/$1/$1.dedup.bam
 	$$(call RUN,-c -n $(GATK_THREADS) -s 1G -m $(GATK_MEM_THREAD) -w 1440,"set -o pipefail && \
 																		   samtools index $$(<) && \
 									   							   		   /home/$(USER)/share/usr/jdk1.8.0_121/bin/java -Djava.io.tmpdir=$(TMPDIR) -Xms1G -Xmx12G -jar /home/$(USER)/share/usr/lib/java/GenomeAnalysisTK-3.7.jar \
 									   							   		   -S LENIENT -T BaseRecalibrator -R $(REF_FASTA) -knownSites /home/brownd7/share/reference/dbsnp_138.b37.gmaf.vcf.gz \
 									   							   		   -I $$(<) -o $$(@)")
 
-marianas/$1/$1.recal.bam : marianas/$1/$1.realn.bam marianas/$1/$1.recal.grp
+marianas/$1/$1.recal.bam : marianas/$1/$1.dedup.bam marianas/$1/$1.recal.grp
 	$$(call RUN,-c -n $(GATK_THREADS) -s 1G -m $(GATK_MEM_THREAD) -w 1440,"set -o pipefail && \
 									   							   		   /home/$(USER)/share/usr/jdk1.8.0_121/bin/java -Djava.io.tmpdir=$(TMPDIR) -Xms1G -Xmx12G -jar /home/$(USER)/share/usr/lib/java/GenomeAnalysisTK-3.7.jar \
 									   							   		   -S LENIENT -T PrintReads -R $(REF_FASTA) -I $$(<) -BQSR $$(<<) -o $$(@)")
--- test/bam_tools/collapseumi.mk
+++ test/bam_tools/collapseumi.mk
@@ -15,12 +15,11 @@ marianas/$1/$1-pileup.txt : marianas/$1/$1.standard.bam
 									  $(JAVA) -server -Xms2G -Xmx8G -cp $(WALTZ) org.mskcc.juber.waltz.Waltz PileupMetrics $(WALTZ_MIN_MAPQ) $1.standard.bam $(REF_FASTA) $(WALTZ_BED_FILE) && \
 									  cd ../..")
 									  
-## to check sorting
 marianas/$1/first-pass.mate-position-sorted.txt : marianas/$1/$1-pileup.txt
 	$$(call RUN,-c -n 1 -s 8G -m 12G,"set -o pipefail && \
 									  cd marianas/$1 && \
 									  $(JAVA) -server -Xms2G -Xmx8G -cp $(MARIANAS) org.mskcc.marianas.umi.duplex.DuplexUMIBamToCollapsedFastqFirstPass $1.standard.bam $1-pileup.txt $(MARIANAS_MIN_MAPQ) $(MARIANAS_MIN_BAQ) $(MARIANAS_MISMATCH) $(MARIANAS_WOBBLE) $(MARIANAS_MIN_CONSENSUS) $(REF_FASTA) && \
-									  sort -S 6G -k 8 first-pass.txt | sort -S 6G -k 6 > first-pass.mate-position-sorted.txt && \
+									  sort -n -s -S 6G -k 6 -k 8 first-pass.txt > first-pass.mate-position-sorted.txt && \
 									  cd ../..")
 
 
