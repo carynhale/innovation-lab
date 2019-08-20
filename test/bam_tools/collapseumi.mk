620c7bfdb6dd6abfd4c99c8593616e879e93e448
David Brown <ndbrown6@users.noreply.github.com>
2019-08-20 11:47:01 -0400

    Update collapseumi.mk
    Fixed target files

--- test/bam_tools/collapseumi.mk
+++ test/bam_tools/collapseumi.mk
@@ -9,18 +9,18 @@ umi_collapse : $(foreach sample,$(SAMPLES),marianas/$(sample)/second-pass-alt-al
 JAVA = /home/${USER}/share/usr/jdk1.8.0_74/bin/java
 
 define genotype-and-collapse
-marianas/$1/$1-pileup.txt : marianas/$1/$1.standard.bam
+marianas/$1/$1.standard-pileup.txt : marianas/$1/$1.standard.bam
 	$$(call RUN,-c -n 1 -s 8G -m 12G,"set -o pipefail && \
 									  cd marianas/$1 && \
 									  $(JAVA) -server -Xms2G -Xmx8G -cp $(WALTZ) org.mskcc.juber.waltz.Waltz PileupMetrics $(WALTZ_MIN_MAPQ) $1.standard.bam $(REF_FASTA) $(WALTZ_BED_FILE) && \
 									  cd ../..")
 									  
-marianas/$1/first-pass.mate-position-sorted.txt : marianas/$1/$1-pileup.txt
+marianas/$1/first-pass.mate-position-sorted.txt : marianas/$1/$1.standard-pileup.txt
 	$$(call RUN,-c -n 1 -s 8G -m 12G,"set -o pipefail && \
 									  cd marianas/$1 && \
 									  $(JAVA) -server -Xms2G -Xmx8G -cp $(MARIANAS) org.mskcc.marianas.umi.duplex.DuplexUMIBamToCollapsedFastqFirstPass \
 									  $1.standard.bam \
-									  $1-pileup.txt \
+									  $1.standard-pileup.txt \
 									  $(MARIANAS_MIN_MAPQ) \
 									  $(MARIANAS_MIN_BAQ) \
 									  $(MARIANAS_MISMATCH) \
@@ -36,7 +36,7 @@ marianas/$1/second-pass-alt-alleles.txt marianas/$1/collapsed_R1_.fastq marianas
 									  cd marianas/$1 && \
 									  $(JAVA) -server -Xms2G -Xmx8G -cp $(MARIANAS) org.mskcc.marianas.umi.duplex.DuplexUMIBamToCollapsedFastqSecondPass \
 									  $1.standard.bam \
-									  $1-pileup.txt \
+									  $1.standard-pileup.txt \
 									  $(MARIANAS_MIN_MAPQ) \
 									  $(MARIANAS_MIN_BAQ) \
 									  $(MARIANAS_MISMATCH) \
