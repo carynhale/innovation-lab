09f592aca6bb7ccdf0466fa7ce1113dd6295d5b4
David Brown <ndbrown6@users.noreply.github.com>
2019-08-22 12:18:40 -0400

    Update intervalmetrics.mk

--- test/qc/intervalmetrics.mk
+++ test/qc/intervalmetrics.mk
@@ -9,7 +9,8 @@ POOL_B_TARGET_FILE ?= $(HOME)/share/reference/target_panels/MSK-ACCESS-v1_0-prob
 
 interval_metrics : $(foreach sample,$(SAMPLES),metrics/standard/$(sample).idx_stats.txt) \
 				   $(foreach sample,$(SAMPLES),metrics/standard/$(sample).aln_metrics.txt) \
-				   $(foreach sample,$(SAMPLES),metrics/standard/$(sample).insert_metrics.txt)
+				   $(foreach sample,$(SAMPLES),metrics/standard/$(sample).insert_metrics.txt) \
+				   $(foreach sample,$(SAMPLES),metrics/standard/$(sample).oxog_metrics.txt)
 
 define picard-metrics
 metrics/standard/$1.idx_stats.txt : bam/$1-standard.bam
@@ -19,27 +20,27 @@ metrics/standard/$1.idx_stats.txt : bam/$1-standard.bam
 									   > $$(@)")
 									   
 metrics/standard/$1.aln_metrics.txt : bam/$1-standard.bam
-	$$(call RUN, -c -n 1 -s 12G -m 18G -w 1440,"java -Djava.io.tmpdir=$(TMPDIR) -Xms2G -Xmx16G -jar $$(PICARD_JAR) CollectAlignmentSummaryMetrics \
-												R=$(REF_FASTA) \
-												I=$$(<) \
-												O=$$(@) \
-												TMP_DIR=$(TMPDIR)")
+	$$(call RUN, -c -n 1 -s 6G -m 12G,"java -Djava.io.tmpdir=$(TMPDIR) -Xms2G -Xmx8G -jar $$(PICARD_JAR) CollectAlignmentSummaryMetrics \
+									   R=$(REF_FASTA) \
+									   I=$$(<) \
+									   O=$$(@) \
+									   TMP_DIR=$(TMPDIR)")
 
 metrics/standard/$1.insert_metrics.txt : bam/$1-standard.bam
-	$$(call RUN, -c -n 1 -s 12G -m 18G -w 1440,"java -Djava.io.tmpdir=$(TMPDIR) -Xms2G -Xmx16G -jar $$(PICARD_JAR) CollectInsertSizeMetrics \
-												I=$$(<) \
-												O=$$(@) \
-												H=metrics/standard/$1.insert_metrics.pdf \
-												M=0.5 \
-												TMP_DIR=$(TMPDIR)")
+	$$(call RUN, -c -n 1 -s 6G -m 12G,"java -Djava.io.tmpdir=$(TMPDIR) -Xms2G -Xmx16G -jar $$(PICARD_JAR) CollectInsertSizeMetrics \
+									   I=$$(<) \
+									   O=$$(@) \
+									   H=metrics/standard/$1.insert_metrics.pdf \
+									   M=0.5 \
+									   TMP_DIR=$(TMPDIR)")
 												
-#metrics/standard/$1.insert_metrics.txt : bam/%-standard.bam
-#	$$(call RUN, -c -n 1 -s 12G -m 18G -w 1440,"java -Djava.io.tmpdir=$(TMPDIR) -Xms2G -Xmx16G -jar $$(PICARD_JAR) CollectOxoGMetrics \
-#												R=$(REF_FASTA) \
-#												I=$$(<) \
-#												O=$$(@) \
-#												TMP_DIR=$(TMPDIR)")
-#
+metrics/standard/$1.oxog_metrics.txt : bam/$1-standard.bam
+	$$(call RUN, -c -n 1 -s 6G -m 12G,"java -Djava.io.tmpdir=$(TMPDIR) -Xms2G -Xmx16G -jar $$(PICARD_JAR) CollectOxoGMetrics \
+									   R=$(REF_FASTA) \
+									   I=$$(<) \
+									   O=$$(@) \
+									   TMP_DIR=$(TMPDIR)")
+
 #metrics/standard/$1.pool-A.hs_metrics.txt : bam/%-standard.bam
 #	$$(call RUN, -c -n 1 -s 12G -m 18G -w 1440,"java -Djava.io.tmpdir=$(TMPDIR) -Xms2G -Xmx16G -jar $$(PICARD_JAR) CollectHsMetrics \
 #												R=$(REF_FASTA) \
