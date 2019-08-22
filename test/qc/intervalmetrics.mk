fed6cd872d4fd7179a4c613c3a51141c36b0dec3
David Brown <ndbrown6@users.noreply.github.com>
2019-08-22 13:10:48 -0400

    Update intervalmetrics.mk

--- test/qc/intervalmetrics.mk
+++ test/qc/intervalmetrics.mk
@@ -6,11 +6,13 @@ PHONY += metrics metrics/standard metrics/unfiltered metrics/simplex metrics/dup
 
 POOL_A_TARGET_FILE ?= $(HOME)/share/reference/target_panels/MSK-ACCESS-v1_0-probe-A.sorted.list
 POOL_B_TARGET_FILE ?= $(HOME)/share/reference/target_panels/MSK-ACCESS-v1_0-probe-B.sorted.list
+WALTZ_MIN_MAPQ ?= 20
 
 interval_metrics : $(foreach sample,$(SAMPLES),metrics/standard/$(sample).idx_stats.txt) \
 				   $(foreach sample,$(SAMPLES),metrics/standard/$(sample).aln_metrics.txt) \
 				   $(foreach sample,$(SAMPLES),metrics/standard/$(sample).insert_metrics.txt) \
-				   $(foreach sample,$(SAMPLES),metrics/standard/$(sample).oxog_metrics.txt)
+				   $(foreach sample,$(SAMPLES),metrics/standard/$(sample).oxog_metrics.txt) \
+				   $(foreach sample,$(SAMPLES),metrics/standard/$(sample).probe-A.hs_metrics.txt)
 
 define picard-metrics
 metrics/standard/$1.idx_stats.txt : bam/$1-standard.bam
@@ -27,7 +29,7 @@ metrics/standard/$1.aln_metrics.txt : bam/$1-standard.bam
 									   TMP_DIR=$(TMPDIR)")
 
 metrics/standard/$1.insert_metrics.txt : bam/$1-standard.bam
-	$$(call RUN, -c -n 1 -s 6G -m 12G,"java -Djava.io.tmpdir=$(TMPDIR) -Xms2G -Xmx16G -jar $$(PICARD_JAR) CollectInsertSizeMetrics \
+	$$(call RUN, -c -n 1 -s 6G -m 12G,"java -Djava.io.tmpdir=$(TMPDIR) -Xms2G -Xmx8G -jar $$(PICARD_JAR) CollectInsertSizeMetrics \
 									   I=$$(<) \
 									   O=$$(@) \
 									   H=metrics/standard/$1.insert_metrics.pdf \
@@ -35,21 +37,22 @@ metrics/standard/$1.insert_metrics.txt : bam/$1-standard.bam
 									   TMP_DIR=$(TMPDIR)")
 												
 metrics/standard/$1.oxog_metrics.txt : bam/$1-standard.bam
-	$$(call RUN, -c -n 1 -s 6G -m 12G,"java -Djava.io.tmpdir=$(TMPDIR) -Xms2G -Xmx16G -jar $$(PICARD_JAR) CollectOxoGMetrics \
+	$$(call RUN, -c -n 1 -s 6G -m 12G,"java -Djava.io.tmpdir=$(TMPDIR) -Xms2G -Xmx8G -jar $$(PICARD_JAR) CollectOxoGMetrics \
 									   R=$(REF_FASTA) \
 									   I=$$(<) \
 									   O=$$(@) \
 									   TMP_DIR=$(TMPDIR)")
 
-#metrics/standard/$1.pool-A.hs_metrics.txt : bam/%-standard.bam
-#	$$(call RUN, -c -n 1 -s 12G -m 18G -w 1440,"java -Djava.io.tmpdir=$(TMPDIR) -Xms2G -Xmx16G -jar $$(PICARD_JAR) CollectHsMetrics \
-#												R=$(REF_FASTA) \
-#												I=$$(<) \
-#												O=$$(@) \
-#												BAIT_INTERVALS=$(POOL_A_TARGET_FILE) \
-#												TARGET_INTERVALS=$(POOL_A_TARGET_FILE) \
-#												TMP_DIR=$(TMPDIR)")
-#												
+metrics/standard/$1.probe-A.hs_metrics.txt : bam/$1-standard.bam
+	$$(call RUN, -c -n 1 -s 6G -m 12G,"java -Djava.io.tmpdir=$(TMPDIR) -Xms2G -Xmx8G -jar $$(PICARD_JAR) CollectHsMetrics \
+									   R=$(REF_FASTA) \
+									   I=$$(<) \
+									   O=$$(@) \
+									   BAIT_INTERVALS=$(POOL_A_TARGET_FILE) \
+									   TARGET_INTERVALS=$(POOL_A_TARGET_FILE) \
+									   MINIMUM_MAPPING_QUALITY=$(WALTZ_MIN_MAPQ) \
+									   TMP_DIR=$(TMPDIR)")
+												
 #metrics/standard/$1.pool-B.hs_metrics.txt : bam/%-standard.bam
 #	$$(call RUN, -c -n 1 -s 12G -m 18G -w 1440,"java -Djava.io.tmpdir=$(TMPDIR) -Xms2G -Xmx16G -jar $$(PICARD_JAR) CollectHsMetrics \
 #												R=$(REF_FASTA) \
