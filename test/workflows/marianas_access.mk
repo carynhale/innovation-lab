f4d3d80307c7e5dad9939406e1a6f305ea3db3f0
David Brown <ndbrown6@users.noreply.github.com>
2019-08-23 13:32:48 -0400

    Update marianas_access.mk

--- test/workflows/marianas_access.mk
+++ test/workflows/marianas_access.mk
@@ -2,15 +2,15 @@ include modules/Makefile.inc
 include modules/genome_inc/b37.inc
 
 LOGDIR ?= log/marianas_access.$(NOW)
-PHONY += marianas bam
+PHONY += marianas bam metrics
 
 MSK_ACCESS_WORKFLOW += clip_umi
 MSK_ACCESS_WORKFLOW += align_fastq
 MSK_ACCESS_WORKFLOW += umi_collapse
 MSK_ACCESS_WORKFLOW += align_collapsed
 MSK_ACCESS_WORKFLOW += copy_bam
-MSK_ACCESS_WORKFLOW += umi_qc
 MSK_ACCESS_WORKFLOW += interval_metrics
+#MSK_ACCESS_WORKFLOW += umi_qc
 
 msk_access_workflow : $(MSK_ACCESS_WORKFLOW)
 
@@ -19,8 +19,8 @@ include modules/test/bam_tools/alignfastq.mk
 include modules/test/bam_tools/collapseumi.mk
 include modules/test/bam_tools/aligncollapsed.mk
 include modules/test/bam_tools/copybam.mk
-include modules/test/qc/umiqc.mk
 include modules/test/qc/intervalmetrics.mk
+#include modules/test/qc/umiqc.mk
 
 .DELETE_ON_ERROR:
 .SECONDARY:
