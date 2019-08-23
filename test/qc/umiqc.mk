88fc51571af38bebfb51090cb7ff20b82c252640
David Brown <ndbrown6@users.noreply.github.com>
2019-08-23 13:59:06 -0400

    Update umiqc.mk

--- test/qc/umiqc.mk
+++ test/qc/umiqc.mk
@@ -6,7 +6,7 @@ PHONY += metrics metrics/summary
 umi_qc : metrics/summary/umi-frequencies.tsv
 		 
 metrics/summary/umi-frequencies.tsv : $(wildcard marianas/$(SAMPLES)/composite-umi-frequencies.txt)
-	$(call RUN, -c -n 1 -s 8G -m 12G,"$(RSCRIPT) modules/test/qc/umiqc.R --type 1 --sample_names $(SAMPLES)")
+	$(call RUN, -c -n 1 -s 8G -m 12G,"$(RSCRIPT) modules/test/qc/umiqc.R --type 1 --sample_names '$(SAMPLES)'")
 
 .DELETE_ON_ERROR:
 .SECONDARY:
