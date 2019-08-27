c8e62acc42551166aa891d598bbb35a2f776e08a
David Brown <ndbrown6@users.noreply.github.com>
2019-08-27 16:15:52 -0400

    Update clustersamples.mk

--- test/qc/clustersamples.mk
+++ test/qc/clustersamples.mk
@@ -4,7 +4,8 @@ include modules/variant_callers/gatk.inc
 LOGDIR = log/cluster_samples.$(NOW)
 PHONE += marianas metrics/summary metrics/report
 
-cluster_samples : $(foreach sample,$(SAMPLES),marianas/$(sample)/$(sample)-snps.vcf)
+cluster_samples : $(foreach sample,$(SAMPLES),marianas/$(sample)/$(sample)-snps.vcf) \
+				  $(foreach sample,$(SAMPLES),marianas/$(sample)/$(sample)-snps-filtered.vcf)
 
 DBSNP_SUBSET ?= $(HOME)/share/reference/dbsnp_tseq_intersect.bed
 CLUSTER_VCF ?= $(RSCRIPT) modules/test/qc/clustersample.R
@@ -16,6 +17,10 @@ marianas/$1/$1-snps.vcf : marianas/$1/$1.standard.bam
 									-L $(DBSNP_SUBSET) \
 									-o $$(@) \
 									--output_mode EMIT_ALL_SITES")
+									
+marianas/$1/$1-snps-filtered.vcf : marianas/$1/$1-snps.vcf
+	$$(call RUN,-n 1 -s 6G -m 12G,"grep '^#' $$(<) > $$(@) && \
+								   grep -e '0/1' -e '1/1' $$(<) >> $$(@)")
 
 endef
 $(foreach sample,$(SAMPLES),\
