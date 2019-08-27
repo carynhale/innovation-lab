c98947889f1da28082e05d8de62d19e84aa44b7d
David Brown <ndbrown6@users.noreply.github.com>
2019-08-27 15:54:17 -0400

    Update clustersamples.mk

--- test/qc/clustersamples.mk
+++ test/qc/clustersamples.mk
@@ -9,8 +9,13 @@ cluster_samples : $(foreach sample,$(SAMPLES),marianas/$(sample)/$(sample)-snps.
 DBSNP_SUBSET ?= $(HOME)/share/reference/dbsnp_tseq_intersect.bed
 CLUSTER_VCF ?= $(RSCRIPT) modules/test/qc/clustersample.R
 
-marianas/%/%-snps.vcf : bam/%-duplex.bam
-	$(call RUN,-n 4 -s 2.5G -m 3G,"$(call GATK_MEM,8G) -T UnifiedGenotyper -nt 4 -R $(REF_FASTA) --dbsnp $(DBSNP) $(foreach bam,$(filter %.bam,$^),-I $(bam) ) -L $(DBSNP_SUBSET) -o $@ --output_mode EMIT_ALL_SITES")
+define genotype-snps
+marianas/$1/$1-snps.vcf : bam/$1-duplex.bam
+	$$(call RUN,-n 4 -s 2.5G -m 3G,"$(call GATK_MEM,8G) -T UnifiedGenotyper -nt 4 -R $(REF_FASTA) --dbsnp $(DBSNP) $(foreach bam,$(filter %.bam,$^),-I $(bam) ) -L $(DBSNP_SUBSET) -o $@ --output_mode EMIT_ALL_SITES")
+
+endef
+$(foreach sample,$(SAMPLES),\
+		$(eval $(call genotype-snps,$(sample))))
 
 include modules/vcf_tools/vcftools.mk
 
