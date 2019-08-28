f3f22bc25132f9b2c2f23e95bb1a34687ea61479
David Brown <ndbrown6@users.noreply.github.com>
2019-08-27 20:43:28 -0400

    SNP clustering MSK-ACCESS

--- test/qc/clustersamples.R
+++ test/qc/clustersamples.R
@@ -28,4 +28,49 @@ if (as.character(opt$library)=="STANDARD") {
 	gt = matrix(as.integer(factor(X)), nrow = nrow(gt), ncol = ncol(gt), dimnames = list(rownames(gt), colnames(gt)))
 	write.table(gt, file="metrics/summary/snps_filtered-standard.tsv", sep="\t", col.names=TRUE, row.names=FALSE, quote=FALSE)
 
+} else if (as.character(opt$library)=="UNFILTERED") {
+
+	vcf_file = "metrics/summary/snps_filtered-unfiltered.vcf"
+	vcf = readVcf(vcf_file, "b37")
+	gt = geno(vcf)$GT
+	ad = geno(vcf)$AD
+	af = structure(sapply(ad, function(x) x[2] / sum(x)), dim = dim(ad))
+	X = matrix(0, nrow = nrow(gt), ncol = ncol(gt), dimnames = list(rownames(gt), colnames(gt)))
+	X[is.na(af)] = NA
+	X[af > 0.15 & af < 0.95] = 1
+	X[af >= 0.95] = 2
+	X[!gt %in% c("0/0", "0/1", "1/1")] = NA
+	gt = matrix(as.integer(factor(X)), nrow = nrow(gt), ncol = ncol(gt), dimnames = list(rownames(gt), colnames(gt)))
+	write.table(gt, file="metrics/summary/snps_filtered-unfiltered.tsv", sep="\t", col.names=TRUE, row.names=FALSE, quote=FALSE)
+
+} else if (as.character(opt$library)=="SIMPLEX") {
+
+	vcf_file = "metrics/summary/snps_filtered-simplex.vcf"
+	vcf = readVcf(vcf_file, "b37")
+	gt = geno(vcf)$GT
+	ad = geno(vcf)$AD
+	af = structure(sapply(ad, function(x) x[2] / sum(x)), dim = dim(ad))
+	X = matrix(0, nrow = nrow(gt), ncol = ncol(gt), dimnames = list(rownames(gt), colnames(gt)))
+	X[is.na(af)] = NA
+	X[af > 0.15 & af < 0.95] = 1
+	X[af >= 0.95] = 2
+	X[!gt %in% c("0/0", "0/1", "1/1")] = NA
+	gt = matrix(as.integer(factor(X)), nrow = nrow(gt), ncol = ncol(gt), dimnames = list(rownames(gt), colnames(gt)))
+	write.table(gt, file="metrics/summary/snps_filtered-simplex.tsv", sep="\t", col.names=TRUE, row.names=FALSE, quote=FALSE)
+
+} else if (as.character(opt$library)=="DUPLEX") {
+
+	vcf_file = "metrics/summary/snps_filtered-duplex.vcf"
+	vcf = readVcf(vcf_file, "b37")
+	gt = geno(vcf)$GT
+	ad = geno(vcf)$AD
+	af = structure(sapply(ad, function(x) x[2] / sum(x)), dim = dim(ad))
+	X = matrix(0, nrow = nrow(gt), ncol = ncol(gt), dimnames = list(rownames(gt), colnames(gt)))
+	X[is.na(af)] = NA
+	X[af > 0.15 & af < 0.95] = 1
+	X[af >= 0.95] = 2
+	X[!gt %in% c("0/0", "0/1", "1/1")] = NA
+	gt = matrix(as.integer(factor(X)), nrow = nrow(gt), ncol = ncol(gt), dimnames = list(rownames(gt), colnames(gt)))
+	write.table(gt, file="metrics/summary/snps_filtered-duplex.tsv", sep="\t", col.names=TRUE, row.names=FALSE, quote=FALSE)
+
 }
--- test/qc/clustersamples.mk
+++ test/qc/clustersamples.mk
@@ -13,7 +13,7 @@ cluster_samples : $(foreach sample,$(SAMPLES),metrics/standard/$(sample)-snps.vc
 DBSNP_SUBSET = $(HOME)/share/reference/dbsnp_tseq_intersect.bed
 CLUSTER_VCF = $(RSCRIPT) modules/test/qc/clustersamples.R
 
-define genotype-snps
+define genotype-snps-standard
 metrics/standard/$1-snps.vcf : bam/$1-standard.bam
 	$$(call RUN,-n 4 -s 2.5G -m 3G,"$(call GATK_MEM,8G) -T UnifiedGenotyper -nt 4 -R $(REF_FASTA) --dbsnp $(DBSNP) \
 									-I $$(<) \
@@ -23,8 +23,48 @@ metrics/standard/$1-snps.vcf : bam/$1-standard.bam
 									
 endef
 $(foreach sample,$(SAMPLES),\
-		$(eval $(call genotype-snps,$(sample))))
+		$(eval $(call genotype-snps-standard,$(sample))))
 		
+define genotype-snps-unfiltered
+metrics/unfiltered/$1-snps.vcf : bam/$1-unfiltered.bam
+	$$(call RUN,-n 4 -s 2.5G -m 3G,"$(call GATK_MEM,8G) -T UnifiedGenotyper -nt 4 -R $(REF_FASTA) --dbsnp $(DBSNP) \
+									-I $$(<) \
+									-L $(DBSNP_SUBSET) \
+									-o $$(@) \
+									--output_mode EMIT_ALL_SITES \
+									--min_base_quality_score 10")
+									
+endef
+$(foreach sample,$(SAMPLES),\
+		$(eval $(call genotype-snps-unfiltered,$(sample))))
+		
+define genotype-snps-simplex
+metrics/simplex/$1-snps.vcf : bam/$1-simplex.bam
+	$$(call RUN,-n 4 -s 2.5G -m 3G,"$(call GATK_MEM,8G) -T UnifiedGenotyper -nt 4 -R $(REF_FASTA) --dbsnp $(DBSNP) \
+									-I $$(<) \
+									-L $(DBSNP_SUBSET) \
+									-o $$(@) \
+									--output_mode EMIT_ALL_SITES \
+									--min_base_quality_score 10")
+									
+endef
+$(foreach sample,$(SAMPLES),\
+		$(eval $(call genotype-snps-simplex,$(sample))))
+
+define genotype-snps-duplex
+metrics/duplex/$1-snps.vcf : bam/$1-duplex.bam
+	$$(call RUN,-n 4 -s 2.5G -m 3G,"$(call GATK_MEM,8G) -T UnifiedGenotyper -nt 4 -R $(REF_FASTA) --dbsnp $(DBSNP) \
+									-I $$(<) \
+									-L $(DBSNP_SUBSET) \
+									-o $$(@) \
+									--output_mode EMIT_ALL_SITES \
+									--min_base_quality_score 10")
+									
+endef
+$(foreach sample,$(SAMPLES),\
+		$(eval $(call genotype-snps-duplex,$(sample))))
+
+
 metrics/summary/snps_combined-standard.vcf : $(foreach sample,$(SAMPLES),metrics/standard/$(sample)-snps.vcf)
 	$(call RUN,-s 16G -m 20G,"$(call GATK_MEM,14G) -T CombineVariants $(foreach vcf,$^,--variant $(vcf) ) \
 							  -o $@ --genotypemergeoption UNSORTED -R $(REF_FASTA)")
@@ -34,6 +74,36 @@ metrics/summary/snps_filtered-standard.vcf : metrics/summary/snps_combined-stand
 	
 metrics/summary/snps_filtered-standard.tsv : metrics/summary/snps_filtered-standard.vcf
 	$(INIT) $(CLUSTER_VCF) --library 'STANDARD'
+	
+metrics/summary/snps_combined-unfiltered.vcf : $(foreach sample,$(SAMPLES),metrics/unfiltered/$(sample)-snps.vcf)
+	$(call RUN,-s 16G -m 20G,"$(call GATK_MEM,14G) -T CombineVariants $(foreach vcf,$^,--variant $(vcf) ) \
+							  -o $@ --genotypemergeoption UNSORTED -R $(REF_FASTA)")
+							  
+metrics/summary/snps_filtered-unfiltered.vcf : metrics/summary/snps_combined-unfiltered.vcf
+	$(INIT) grep '^#' $< > $@ && grep -e '0/1' -e '1/1' $< >> $@
+	
+metrics/summary/snps_filtered-unfiltered.tsv : metrics/summary/snps_filtered-unfiltered.vcf
+	$(INIT) $(CLUSTER_VCF) --library 'UNFILTERED'
+	
+metrics/summary/snps_combined-simplex.vcf : $(foreach sample,$(SAMPLES),metrics/simplex/$(sample)-snps.vcf)
+	$(call RUN,-s 16G -m 20G,"$(call GATK_MEM,14G) -T CombineVariants $(foreach vcf,$^,--variant $(vcf) ) \
+							  -o $@ --genotypemergeoption UNSORTED -R $(REF_FASTA)")
+							  
+metrics/summary/snps_filtered-simplex.vcf : metrics/summary/snps_combined-simplex.vcf
+	$(INIT) grep '^#' $< > $@ && grep -e '0/1' -e '1/1' $< >> $@
+	
+metrics/summary/snps_filtered-simplex.tsv : metrics/summary/snps_filtered-simplex.vcf
+	$(INIT) $(CLUSTER_VCF) --library 'SIMPLEX'
+
+metrics/summary/snps_combined-duplex.vcf : $(foreach sample,$(SAMPLES),metrics/duplex/$(sample)-snps.vcf)
+	$(call RUN,-s 16G -m 20G,"$(call GATK_MEM,14G) -T CombineVariants $(foreach vcf,$^,--variant $(vcf) ) \
+							  -o $@ --genotypemergeoption UNSORTED -R $(REF_FASTA)")
+							  
+metrics/summary/snps_filtered-duplex.vcf : metrics/summary/snps_combined-duplex.vcf
+	$(INIT) grep '^#' $< > $@ && grep -e '0/1' -e '1/1' $< >> $@
+	
+metrics/summary/snps_filtered-duplex.tsv : metrics/summary/snps_filtered-duplex.vcf
+	$(INIT) $(CLUSTER_VCF) --library 'SIMPLEX'
 	
 metrics/report/snps_clustering-standard.pdf : metrics/summary/snps_filtered-standard.tsv
 	$(call RUN, -c -n 1 -s 12G -m 16G -v $(SUPERHEAT_ENV),"$(RSCRIPT) modules/test/qc/plotmetrics.R --type 16 && \
