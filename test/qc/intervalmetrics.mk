8d883d952ed3af412517c70b74710378a52cdaa8
David Brown <ndbrown6@users.noreply.github.com>
2019-08-28 15:40:36 -0400

    Update intervalmetrics.mk

--- test/qc/intervalmetrics.mk
+++ test/qc/intervalmetrics.mk
@@ -61,12 +61,12 @@ interval_metrics : $(foreach sample,$(SAMPLES),metrics/standard/$(sample).idx_st
 				   
 define coverage-metric
 metrics/standard/$1.ontarget.txt : bam/$1-standard.bam
-	$$(call RUN,-c -n 4 -s 6G -m 8G,"cnvkit.py coverage -p 4 -q 0 $$(<) $$(ONTARGET_FILE) -o metrics/standard/$$(*).ontarget.txt")
+	$$(call RUN,-n 4 -s 6G -m 8G,"cnvkit.py coverage -p 4 -q 0 $$(<) $$(ONTARGET_FILE) -o metrics/standard/$$(*).ontarget.txt")
 
 metrics/standard/$1.offtarget.txt : bam/$1-standard.bam
-	$$(call RUN,-c -n 4 -s 6G -m 8G,"cnvkit.py coverage -p 4 -q 0 $$(<) $$(OFFTARGET_FILE) -o metrics/standard/$$(*).offtarget.txt")
+	$$(call RUN,-n 4 -s 6G -m 8G,"cnvkit.py coverage -p 4 -q 0 $$(<) $$(OFFTARGET_FILE) -o metrics/standard/$$(*).offtarget.txt")
 endef
- $(foreach sample,$(SAMPLES),\
+$(foreach sample,$(SAMPLES),\
 		$(eval $(call coverage-metric,$(sample))))
 
 define picard-metrics-standard
