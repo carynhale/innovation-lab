c795b0fc10422a4e95e4e01aef1f8fd677f9a8b5
David Brown <ndbrown6@users.noreply.github.com>
2019-08-29 11:50:52 -0400

    Update intervalmetrics.mk

--- test/qc/intervalmetrics.mk
+++ test/qc/intervalmetrics.mk
@@ -6,8 +6,8 @@ PHONY += metrics metrics/standard metrics/unfiltered metrics/duplex metrics/simp
 
 POOL_A_TARGET_FILE ?= $(HOME)/share/reference/target_panels/MSK-ACCESS-v1_0-probe-A.sorted.list
 POOL_B_TARGET_FILE ?= $(HOME)/share/reference/target_panels/MSK-ACCESS-v1_0-probe-B.sorted.list
-ONTARGET_FILE_A ?= $(HOME)/share/reference/target_panels/MSK-ACCESS-v1_0-probe-A.ontarget.bed
-ONTARGET_FILE_B ?= $(HOME)/share/reference/target_panels/MSK-ACCESS-v1_0-probe-B.ontarget.bed
+ONTARGET_FILE_A ?= $(HOME)/share/reference/target_panels/MSK-ACCESS-v1_0-probe-A.sorted.bed
+ONTARGET_FILE_B ?= $(HOME)/share/reference/target_panels/MSK-ACCESS-v1_0-probe-B.sorted.bed
 OFFTARGET_FILE ?= $(HOME)/share/reference/target_panels/MSK-ACCESS-v1_0-probe-AB.offtarget.bed
 
 interval_metrics : $(foreach sample,$(SAMPLES),metrics/standard/$(sample).idx_stats.txt) \
