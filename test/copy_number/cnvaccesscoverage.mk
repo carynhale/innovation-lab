66ec6b6d8e9f8d591f5d85daea272dcb2624d5d8
David Brown <ndbrown6@users.noreply.github.com>
2019-08-31 13:58:43 -0400

    Update cnvaccesscoverage.mk

--- test/copy_number/cnvaccesscoverage.mk
+++ test/copy_number/cnvaccesscoverage.mk
@@ -8,10 +8,11 @@ PHONY += cnvaccess cnvaccess/cnn
 cnvaccess_coverage : $(foreach sample,$(SAMPLES),cnvaccess/cov/$(sample).probe-A.txt)
 
 R_COVERAGE ?= modules/test/copy_number/cnvaccesscoverage.R
+EXOME_DEPTH_ENV ?= $(HOME)/usr/anaconda-envs/exomedepth-1.1.12
 
 define cnvaccess-coverage
 cnvaccess/cov/%.probe-A.txt : bam/%-standard.bam
-	$$(call RUN,-c -n 1 -s 12G -m 26G,"$(RSCRIPT) $(R_COVERAGE) --sample_name $1 --probe 'A'")
+	$$(call RUN,-c -n 1 -s 12G -m 26G -v $(EXOME_DEPTH_ENV),"$(RSCRIPT) $(R_COVERAGE) --sample_name $1 --probe 'A'")
 	
 endef
  $(foreach sample,$(SAMPLES),\
