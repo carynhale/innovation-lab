d4d5c061a290484fc89ac8759133d731a0a6002e
David Brown <ndbrown6@users.noreply.github.com>
2019-09-04 16:26:59 -0400

    Update cnvaccesscoverage.mk

--- test/copy_number/cnvaccesscoverage.mk
+++ test/copy_number/cnvaccesscoverage.mk
@@ -6,8 +6,7 @@ PHONY += cnvaccess cnvaccess/cov
 
 
 cnvaccess_coverage : $(foreach sample,$(SAMPLES),cnvaccess/cov/$(sample).probe-A.txt) \
-					 $(foreach sample,$(SAMPLES),cnvaccess/cov/$(sample).probe-B.txt) \
-					 $(foreach sample,$(SAMPLES),cnvaccess/cov/$(sample).probe-AB.txt)
+					 $(foreach sample,$(SAMPLES),cnvaccess/cov/$(sample).probe-B.txt)
 
 R_COVERAGE ?= modules/test/copy_number/cnvaccesscoverage.R
 R_FIX ?= modules/test/copy_number/cnvaccessfix.R
@@ -20,9 +19,6 @@ cnvaccess/cov/$1.probe-A.txt : bam/$1-standard.bam
 	
 cnvaccess/cov/$1.probe-B.txt : bam/$1-standard.bam
 	$$(call RUN,-c -n 1 -s 6G -m 12G -v $(EXOME_DEPTH_ENV),"$(RSCRIPT) $(R_COVERAGE) --sample_name $1 --probe 'B'")
-	
-cnvaccess/cov/$1.probe-AB.txt : bam/$1-standard.bam
-	$$(call RUN,-c -n 1 -s 12G -m 24G -v $(EXOME_DEPTH_ENV),"$(RSCRIPT) $(R_COVERAGE) --sample_name $1 --probe 'NA'")
 	
 endef
  $(foreach sample,$(SAMPLES),\
