46e4cb29dda7d4ac4407f3d5791875d7bd902b80
David Brown <ndbrown6@users.noreply.github.com>
2019-09-04 20:20:57 -0400

    CNVkit

--- test/copy_number/cnvaccesscoverage.mk
+++ test/copy_number/cnvaccesscoverage.mk
@@ -2,16 +2,20 @@ include modules/Makefile.inc
 include modules/genome_inc/b37.inc
 
 LOGDIR ?= log/cnvaccess_coverage.$(NOW)
-PHONY += cnvaccess cnvaccess/cov
-
+PHONY += cnvaccess cnvaccess/cov cnvaccess/cnn
 
 cnvaccess_coverage : $(foreach sample,$(SAMPLES),cnvaccess/cov/$(sample).probe-A.txt) \
-					 $(foreach sample,$(SAMPLES),cnvaccess/cov/$(sample).probe-B.txt)
+					 $(foreach sample,$(SAMPLES),cnvaccess/cov/$(sample).probe-B.txt) \
+					 $(foreach sample,$(SAMPLES),cnvaccess/cnn/$(sample).antitargetcoverage.cnn) \
+					 $(foreach sample,$(SAMPLES),cnvaccess/cnn/$(sample).targetcoverage.cnn)
+					 
 
 R_COVERAGE ?= modules/test/copy_number/cnvaccesscoverage.R
 R_FIX ?= modules/test/copy_number/cnvaccessfix.R
 R_PLOT ?= modules/test/copy_number/cnvaccessplot.R
 EXOME_DEPTH_ENV ?= $(HOME)/share/usr/anaconda-envs/exomedepth-1.1.12
+OFFTARGET_FILE ?= $(HOME)/share/reference/target_panels/MSK-ACCESS-v1_0-probe-AB.offtarget.bed
+REFERENCE_FILE ?= $(HOME)/share/reference/cnvkit_reference/MSK-ACCESS-v1_0-noprobe.cnr
 
 define cnvaccess-coverage
 cnvaccess/cov/$1.probe-A.txt : bam/$1-standard.bam
@@ -23,5 +27,16 @@ cnvaccess/cov/$1.probe-B.txt : bam/$1-standard.bam
 endef
  $(foreach sample,$(SAMPLES),\
 		$(eval $(call cnvaccess-coverage,$(sample))))
+		
+define cnvaccess-cnvkit-cov
+cnvaccess/cnn/$1.antitargetcoverage.cnn : bam/$1-standard.bam
+		$$(call RUN,-c -n 4 -s 6G -m 8G,"cnvkit.py coverage -p 4 -q 0 $$(<) $$(OFFTARGET_FILE) -o $$(^)")
+	
+cnvaccess/cnn/$1.targetcoverage.cnn : bam/$1-standard.bam
+		$$(call RUN,-n 1 -s 1G -m 2G,"touch $$(^)")
+	
+endef
+ $(foreach sample,$(SAMPLES),\
+		$(eval $(call cnvaccess-cnvkit-cov,$(sample))))
 
 .PHONY: $(PHONY)
--- test/copy_number/cnvaccessfix.mk
+++ test/copy_number/cnvaccessfix.mk
@@ -2,14 +2,17 @@ include modules/Makefile.inc
 include modules/genome_inc/b37.inc
 
 LOGDIR ?= log/cnvaccess_fix.$(NOW)
-PHONY += cnvaccess cnvaccess/log2
+PHONY += cnvaccess cnvaccess/log2 cnvaccess/cnr
 
-cnvaccess_fix : $(foreach sample,$(SAMPLES),cnvaccess/log2/$(sample).txt)
+cnvaccess_fix : $(foreach sample,$(SAMPLES),cnvaccess/log2/$(sample).txt) \
+				$(foreach sample,$(SAMPLES),cnvaccess/cnr/$(sample).cnr)
 
 R_COVERAGE ?= modules/test/copy_number/cnvaccesscoverage.R
 R_FIX ?= modules/test/copy_number/cnvaccessfix.R
 R_PLOT ?= modules/test/copy_number/cnvaccessplot.R
 EXOME_DEPTH_ENV ?= $(HOME)/share/usr/anaconda-envs/exomedepth-1.1.12
+OFFTARGET_FILE ?= $(HOME)/share/reference/target_panels/MSK-ACCESS-v1_0-probe-AB.offtarget.bed
+REFERENCE_FILE ?= $(HOME)/share/reference/cnvkit_reference/MSK-ACCESS-v1_0-noprobe.cnr
 
 define cnvaccess-fix
 cnvaccess/log2/$1.txt : cnvaccess/cov/$1.probe-A.txt cnvaccess/cov/$1.probe-B.txt
@@ -18,5 +21,13 @@ cnvaccess/log2/$1.txt : cnvaccess/cov/$1.probe-A.txt cnvaccess/cov/$1.probe-B.tx
 endef
  $(foreach sample,$(SAMPLES),\
 		$(eval $(call cnvaccess-fix,$(sample))))
+		
+define cnvaccess-cnvkit-fix
+cnvaccess/cnr/$1.cnr : cnvaccess/cnn/$1.targetcoverage.cnn cnvaccess/cnn/$1.antitargetcoverage.cnn
+	$$(call RUN,-c -s 6G -m 8G,"cnvkit.py fix $$(<) $$(<<) $(REFERENCE_FILE) -o $$(^)")
+	
+endef
+ $(foreach sample,$(SAMPLES),\
+		$(eval $(call cnvaccess-cnvkit-fix,$(sample))))
 
 .PHONY: $(PHONY)
