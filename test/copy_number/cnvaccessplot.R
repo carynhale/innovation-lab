75c2a63590e3ea548e86ece36249ef33404c46cb
David Brown <ndbrown6@users.noreply.github.com>
2019-09-04 21:54:52 -0400

    Offtarget CNA for MSK-ACCESS samples

--- test/copy_number/cnvaccessplot.R
+++ test/copy_number/cnvaccessplot.R
@@ -181,5 +181,50 @@ if (as.numeric(opt$type)==1) {
 	axis(1, at=.5*(start+end), labels=sort(as.numeric(unique(log2_$chr))), tcl=-.5, lwd=0, lwd.ticks=1.25, tcl=-.5, cex.axis=1.25)
 	dev.off()
 
-}
+} else if (as.numeric(opt$type)==5) {
 
+	log2_ = read.csv(file=paste0("cnvaccess/log2/", opt$sample_name, "-offtarget.txt"), header=TRUE, sep="\t", stringsAsFactors=FALSE)[,c("chromosome", "start", "end", "log2"),drop=FALSE]
+	log2_ = subset(log2_, log2_$chromosome %in% c(1:22) & !is.na(log2_$log2))
+	log2_ = winsorize(data = log2_[,c("chromosome", "start", "log2"),drop=FALSE], method = "mad", tau = 1.5, k = 25, verbose = FALSE)
+	segmented_ = pcf(data=log2_, kmin = 100, gamma = 60,
+					 normalize = FALSE, fast = FALSE, assembly = "hg19", digits = 4,
+           			 return.est = FALSE, save.res = FALSE, file.names = NULL, verbose = FALSE)[,2:7,drop=FALSE]
+	colnames(log2_) = c("chr", "pos", "log2")
+    colnames(segmented_) = c("chr", "arm", "start", "end", "n", "log2")
+	save(log2_, segmented_, file=paste0("cnvaccess/report/segmented/", opt$sample_name, "-offtarget.RData"))
+	
+	segmented_ = prune_(x=segmented_, n=5)
+
+	pdf(file=paste0("cnvaccess/report/segmented/", opt$sample_name, "-offtarget.pdf"), width=14, height=5)
+	par(mar=c(6.1, 6.5, 4.1, 1.1))
+	plot(log2_$log2,
+		 col = "grey80",
+		 pch = 16,
+		 cex = .5,
+		 ylim = c(-4,4),
+		 las = 1,
+		 xlab = "",
+		 ylab = "",
+		 frame.plot = FALSE,
+		 axes = FALSE)
+	segmented_[,"n"] = cumsum(segmented_[,"n"])
+	for (j in 2:nrow(segmented_)) {
+ 		lines(x=c(segmented_[j-1,"n"], segmented_[j,"n"]), y=rep(segmented_[j,"log2"],2), lty=1, lwd=3.0, col="red")
+ 	}
+ 	lines(x=c(1, segmented_[1,"n"]), y=rep(segmented_[1,"log2"],2), lty=1, lwd=3.0, col="red")
+	points(c(0-.05*length(log2_$log2),length(log2_$log2)+.01*length(log2_$log2)), c(0,0), type="l", col="red")
+	axis(2, at=seq(-3, 3, by=1), labels=seq(-3,3, by=1), cex.axis=1.5, las=1, lwd=1.5)
+	mtext(side=1, text="Chromosome", line=4, cex=1.5)
+	mtext(side=2, text=expression(Log[2]~"Ratio"), line=4, cex=1.5)
+	title(main=opt$sample_name, cex.main=1.5)
+	start = end = NULL
+	for (ii in sort(as.numeric(unique(log2_$chr)))) {
+		inx = which(log2_$chr==ii)
+		start = c(start, min(inx))
+		end = c(end, max(inx))
+	}
+	axis(1, at=c(1, end), labels=rep("", length(end)+1), cex.axis=1.5, lwd=1.5, tcl=.5)
+	axis(1, at=.5*(start+end), labels=sort(as.numeric(unique(log2_$chr))), tcl=-.5, lwd=0, lwd.ticks=1.25, tcl=-.5, cex.axis=1.25)
+	dev.off()
+
+}
--- test/copy_number/cnvaccessplot.mk
+++ test/copy_number/cnvaccessplot.mk
@@ -7,6 +7,7 @@ PHONY += cnvaccess cnvaccess/report cnvaccess/report/log2 cnvaccess/report/ASCAT
 cnvaccess_plot : $(foreach sample,$(SAMPLES),cnvaccess/report/log2/$(sample)-ontarget.pdf) \
 				 $(foreach sample,$(SAMPLES),cnvaccess/report/log2/$(sample)-offtarget.pdf) \
 				 $(foreach sample,$(SAMPLES),cnvaccess/report/segmented/$(sample)-ontarget.RData) \
+				 $(foreach sample,$(SAMPLES),cnvaccess/report/segmented/$(sample)-offtarget.RData) \
 				 $(foreach sample,$(SAMPLES),cnvaccess/report/ASCAT/$(sample)-ontarget.pdf) \
 				 
 				 
@@ -40,7 +41,6 @@ endef
  $(foreach sample,$(SAMPLES),\
 		$(eval $(call cnvaccess-ascat,$(sample))))
 
-
 define cnvaccess-plot-cnvkit
 cnvaccess/report/log2/$1-offtarget.pdf : cnvaccess/log2/$1-offtarget.txt
 	$$(call RUN,-c -n 1 -s 4G -m 6G -v $(ASCAT_ENV),"$(RSCRIPT) $(R_PLOT) --type 4 --sample_name $1")
@@ -48,6 +48,14 @@ cnvaccess/report/log2/$1-offtarget.pdf : cnvaccess/log2/$1-offtarget.txt
 endef
  $(foreach sample,$(SAMPLES),\
 		$(eval $(call cnvaccess-plot-cnvkit,$(sample))))
+		
+define cnvaccess-segment-cnvkit
+cnvaccess/report/segmented/$1-offtarget.RData : cnvaccess/log2/$1-offtarget.txt
+	$$(call RUN,-c -n 1 -s 6G -m 8G -v $(ASCAT_ENV),"$(RSCRIPT) $(R_PLOT) --type 5 --sample_name $1")
+	
+endef
+ $(foreach sample,$(SAMPLES),\
+		$(eval $(call cnvaccess-segment-cnvkit,$(sample))))
 
 
 .PHONY: $(PHONY)
