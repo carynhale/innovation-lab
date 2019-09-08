13cd731458b5f5516b9dd8959d966f9b9011d6f9
David Brown <ndbrown6@users.noreply.github.com>
2019-09-08 15:24:46 -0400

    Update somatic_variants.mk

--- test/workflows/somatic_variants.mk
+++ test/workflows/somatic_variants.mk
@@ -14,7 +14,7 @@ vcf/%.somatic_variants.vcf : vcf/%.$(SNV_TYPE).vcf vcf/%.$(INDEL_TYPE).vcf
 	$(call RUN,-s 9G -m 12G,"$(CONCAT_VCF) $^ | $(VCF_SORT) $(REF_DICT) - > $@")
 
 include modules/variant_callers/somatic/mutect.mk
-include modules/test/workflow/somatic_indels.mk
+include modules/test/workflows/somatic_indels.mk
 include modules/copy_number/facets.mk
 include modules/vcf_tools/annotateSomaticVcf.mk
 
