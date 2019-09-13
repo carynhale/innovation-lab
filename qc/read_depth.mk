e68ebc15186b7686c7306417a58aa7c6afc478f1
David Brown <ndbrown6@users.noreply.github.com>
2019-09-13 16:36:41 -0400

    Update read_depth.mk

--- qc/read_depth.mk
+++ qc/read_depth.mk
@@ -1,7 +1,5 @@
-# Run gatk depth of coverage on bam files
-
 include modules/Makefile.inc
-include modules/variant_callers/gatk.inc
+include modules/config/gatk.inc
 
 LOGDIR = log/read_depth.$(NOW)
 
@@ -14,16 +12,6 @@ endif
 .PHONY: all
 
 all: $(foreach sample,$(SAMPLES),gatk/read_depth/$(sample).read_depth)
-
-#ifeq ($(SPLIT_CHR),true)
-#define read-depth-chr
-#gatk/chr_read_depth/%.$1.read_depth : %.bam
-#	$$(call INIT_MEM,8G,12G) $$(call GATK_MEM,7G) -T DepthOfCoverage -R $$(REF_FASTA) -L $1 -o $$@ -I $$< &> $$(LOG)
-#endef
-#$(foreach chr,$(CHROMOSOMES),$(eval $(call read-depth-chr,$(chr))))
-#gatk/read_depth/%.read_depth : $(foreach chr,$(CHROMOSOMES),gatk/chr_read_depth/%.$(chr).read_depth)
-#	$(INIT) head -1 $< > $@ && for x in $^; do sed '1d' $$x >> $@; done
-#else
 
 gatk/read_depth/%.read_depth : %.bam
 	$(call RUN,-s 8G -m 12G,"$(call GATK_MEM,7G) -T DepthOfCoverage -R $(REF_FASTA) $(READ_DEPTH_ARGS) -o $@ -I $<")
