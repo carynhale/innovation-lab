988906a1b2c3d73218c2243a62c8c29a18f36b9a
David Brown <ndbrown6@users.noreply.github.com>
2019-09-13 18:21:42 -0400

    Update ericScript.mk

--- structural_variants/ericScript.mk
+++ structural_variants/ericScript.mk
@@ -1,14 +1,7 @@
-# run EricScript
-# author: Raymond Lim
-
 ERICSCRIPT_ENV = $(HOME)/share/usr/anaconda-envs/ericscript-0.5.5/
 ERICSCRIPT = ericscript.pl
 ERICSCRIPT_OPTS ?= --refid $(ERICSCRIPT_SPECIES)  -db $(ERICSCRIPT_DB) --remove
-ERICSCRIPT_TO_USV = python modules/sv_callers/ericscript2usv.py
-
-.PHONY: ericscript
-.SECONDARY:
-.DELETE_ON_ERROR:
+ERICSCRIPT_TO_USV = python $(SCRIPTS_DIR)/structural_variants/ericscript2usv.py
 
 ericscript: $(foreach sample,$(SAMPLES),usv/$(sample).ericscript.tsv)
 
@@ -18,3 +11,7 @@ ericscript/%_ericscript.timestamp : fastq/%.1.fastq.gz fastq/%.2.fastq.gz
 
 usv/%.ericscript.tsv : ericscript/%_ericscript.timestamp
 	$(call RUN,,"$(ERICSCRIPT_TO_USV) < eriscript/$*/$*.results.filtered.tsv > $@")
+
+.PHONY: ericscript
+.SECONDARY:
+.DELETE_ON_ERROR:
