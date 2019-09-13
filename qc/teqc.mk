a09b87ec4ca0dd05dfee179804a39c8d234e3836
David Brown <ndbrown6@users.noreply.github.com>
2019-09-13 16:40:50 -0400

    Update teqc.mk

--- qc/teqc.mk
+++ qc/teqc.mk
@@ -1,18 +1,15 @@
-# Run TEQC R library on bams
-# vim: set ft=make :
 include modules/Makefile.inc
 
 LOGDIR ?= teqc/log
 
-.PHONY: teqc
-.DELETE_ON_ERROR:
-.SECONDARY:
-
 teqc : teqc_report/index.html
 
 teqc/%.Rdata : bam/%.bam bam/%.bam.bai
-	$(call INIT_MEM,12G,14G) $(RSCRIPT) modules/qc/TEQC.R --ref=$(REF) --outFile $@ $< $(TARGETS_FILE) &> $(LOGDIR)/$(@F).log
+	$(call INIT_MEM,12G,14G) $(RSCRIPT) $(SCRIPTS_DIR)/qc/teqc.R --ref=$(REF) --outFile $@ $< $(TARGETS_FILE) &> $(LOGDIR)/$(@F).log
 
 teqc_report/index.html : $(foreach sample,$(SAMPLES),teqc/$(sample).Rdata)
-	$(call INIT_MEM,12G,14G) $(MKDIR) teqc_report; $(RSCRIPT) modules/qc/TEQCreport.R --outDir=$(@D) $^
+	$(call INIT_MEM,12G,14G) $(MKDIR) teqc_report; $(RSCRIPT) $(SCRIPTS_DIR)/qc/teqc_report.R --outDir=$(@D) $^
 
+.PHONY: teqc
+.DELETE_ON_ERROR:
+.SECONDARY:
