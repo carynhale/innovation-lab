2fc783bd10becd7727c29036fffd4c4dc6191076
David Brown <ndbrown6@users.noreply.github.com>
2019-10-28 16:13:56 -0400

    Update merge_split_fastq.mk

--- fastq_tools/merge_split_fastq.mk
+++ fastq_tools/merge_split_fastq.mk
@@ -1,23 +1,27 @@
 include modules/Makefile.inc
 
 LOGDIR ?= log/merge_split_fastq.$(NOW)
-
-.SECONDARY:
-.DELETE_ON_ERROR: 
 .PHONY : fastq
 
 fastq: $(foreach sample,$(SAMPLES),fastq/$(sample).1.fastq.gz fastq/$(sample).2.fastq.gz)
 
 define merged-fastq
 fastq/$1.%.fastq.gz : $$(foreach split,$2,fastq/$$(split).%.fastq.gz)
-	$$(call RUN,,"zcat $$(^) | gzip -c > $$(@)")
+	$$(call RUN,-c -n 1 -s 2G -m 4G,"zcat $$(^) | gzip -c > $$(@)")
 endef
-$(foreach sample,$(SAMPLES),$(eval $(call merged-fastq,$(sample),$(split.$(sample)))))
+$(foreach sample,$(SAMPLES),\
+		$(eval $(call merged-fastq,$(sample),$(split.$(sample)))))
 
 define merged-fastq2
 fastq/$1.1.fastq.gz : $$(foreach split,$2,$$(word 1, $$(fq.$$(split))))
-	$$(call RUN,,"zcat $$(^) | gzip -c > $$(@)")
+	$$(call RUN,-c -n 1 -s 2G -m 4G,"zcat $$(^) | gzip -c > $$(@)")
 fastq/$1.2.fastq.gz : $$(foreach split,$2,$$(word 2, $$(fq.$$(split))))
-	$$(call RUN,,"zcat $$(^) | gzip -c > $$(@)")
+	$$(call RUN,-c -n 1 -s 2G -m 4G,"zcat $$(^) | gzip -c > $$(@)")
 endef
-$(foreach sample,$(SAMPLES),$(eval $(call merged-fastq2,$(sample),$(split.$(sample)))))
+$(foreach sample,$(SAMPLES),\
+		$(eval $(call merged-fastq2,$(sample),$(split.$(sample)))))
+
+
+.DELETE_ON_ERROR:
+.SECONDARY:
+.PHONY: $(PHONY)
