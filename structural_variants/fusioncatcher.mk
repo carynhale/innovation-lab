f5173b113c58e7edecd34ae0d995db9e70d268c9
David Brown <ndbrown6@users.noreply.github.com>
2019-10-28 21:06:33 -0400

    Update fusioncatcher.mk

--- structural_variants/fusioncatcher.mk
+++ structural_variants/fusioncatcher.mk
@@ -1,20 +1,36 @@
 include modules/Makefile.inc
-include modules/fastq_tools/merge_split_fastq.mk
 
 LOGDIR ?= log/fusion_catcher.$(NOW)
-PHONY += FUSION_CATCHER_WORKFLOW
+.PHONY += fusion_catcher
+
+export PATH=/home/dacruzpa/share/usr/src/fusioncatcher/bin:$PATH
+export PATH=/home/dacruzpa/share/usr/src/fusioncatcher/tools/bowtie-1.1.2-linux-x86_64:$PATH
+export PATH=/home/dacruzpa/share/usr/src/fusioncatcher/tools/bowtie2-2.2.9-linux-x86_64:$PATH
+export PATH=/home/dacruzpa/share/usr/src/fusioncatcher/tools/bwa/:$PATH
+export PATH=/home/dacruzpa/share/usr/src/fusioncatcher/tools/blat:$PATH
+export PATH=/home/dacruzpa/share/usr/src/fusioncatcher/tools/STAR-2.5.2b/source/:$PATH
+export PATH=/home/dacruzpa/share/usr/src/fusioncatcher/tools/liftover:$PATH
+export PATH=/home/dacruzpa/share/usr/src/fusioncatcher/tools/1.2-r101c:$PATH
+export PATH=/home/dacruzpa/share/usr/src/fusioncatcher/tools/sratoolkit/bin:$PATH
+export PATH=/home/dacruzpa/share/usr/src/fusioncatcher/tools/velvet/:$PATH
+export PATH=/home/dacruzpa/share/usr/src/fusioncatcher/tools/fatotwobit/:$PATH
+export PATH=/home/dacruzpa/share/usr/src/fusioncatcher/tools/lzop/src/:$PATH
+export PATH=/home/dacruzpa/share/usr/src/fusioncatcher/tools/coreutils/src/:$PATH
+export PATH=/home/dacruzpa/share/usr/src/fusioncatcher/tools/pigz/:$PATH
+export PATH=/home/dacruzpa/share/usr/src/fusioncatcher/tools/samtools/:$PATH
+export PATH=/home/dacruzpa/share/usr/src/fusioncatcher/tools/BBMap_37.28/:$PATH
+export PATH=/home/dacruzpa/share/usr/src/fusioncatcher/tools/picard/:$PATH
 
 fusion_catcher : $(foreach sample,$(SAMPLES),fusioncatcher/$(sample)/$(sample).taskcomplete)
 
-FUSION_CATCHER_WORKFLOW += fastq
-FUSION_CATCHER_WORKFLOW += fusion_catcher
-
-FUSIONCATCHER = $(HOME)/share/usr/fusioncatcher/bin/fusioncatcher
+FUSION_CATCHER_ = $(HOME)/share/usr/fusioncatcher/bin/fusioncatcher
 FUSIONCATCHER_OPTS = -d $(HOME)/share/usr/fusioncatcher/data/current --extract-buffer-size=35000000000
 
 define fusion-catcher
 fusion_catcher/%/%.taskcomplete : fastq/%.1.fastq.gz fastq/%.2.fastq.gz
-	$(call RUN,-n 8 -s 1G -m 4G,"$(FUSIONCATCHER) $(FUSIONCATCHER_OPTS) -p 8 -o $(@D)/$* -i $<$(,)$(<<) && touch $@")
+	$(call RUN,-n 8 -s 1G -m 4G,"$(FUSIONCATCHER) $(FUSIONCATCHER_OPTS) -p 8 -o $(@D)/$* -i $<$(,)$(<<) && \
+								 touch fusion-ctahcer")
+
 	
 endef
 $(foreach sample,$(SAMPLES),\
@@ -26,13 +42,4 @@ $(foreach sample,$(SAMPLES),\
 .PHONY: $(PHONY)
 
 
-define defuse-single-sample
-fastq/%.1.fastq fastq/%.2.fastq : fastq/%.1.fastq.gz fastq/%.2.fastq.gz
-	$$(call RUN,-c -s 4G -m 9G,"gzip -d $$(<) $$(<<)")
-
-defuse/tables/%.results.filtered.tsv : fastq/%.1.fastq fastq/%.2.fastq
-	$$(call RUN,-c -n 10 -s 3G -m 4G -w 540,"$$(PERL) $$(DEFUSE_SCRIPTS)/defuse_run.pl -c $$(CONFIG) -d $$(DEFUSE75) -o defuse/$$* --res defuse/tables/$$*.results.tsv -resfil defuse/tables/$$*.results.filtered.tsv -1 fastq/$$*.1.fastq -2 fastq/$$*.2.fastq -p 10 -s direct")
-	
-defuse/%.taskcomplete : defuse/tables/%.results.filtered.tsv
-	$$(call RUN,-c -s 1G -m 3G,"echo $$* > defuse/$$*.taskcomplete")
-endef
+fusioncatcher -d ~/share/usr/src/fusioncatcher/data/human_v90 -i rawdata/PITT_0392/Sample_MCM101T_IGO_04835_J_1/ -o
