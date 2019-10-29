3c0600b00cd9fc98ef1b03224077d0e27ec99afc
David Brown <ndbrown6@users.noreply.github.com>
2019-10-29 11:40:12 -0400

    Update rnaseq_fusion.mk

--- test/workflows/rnaseq_fusion.mk
+++ test/workflows/rnaseq_fusion.mk
@@ -6,12 +6,14 @@ PHONY += fastq defuse fusion_catcher
 RNASEQ_FUSION_WORKFLOW += fastq
 RNASEQ_FUSION_WORKFLOW += defuse
 RNASEQ_FUSION_WORKFLOW += fusion_catcher
+RNASEQ_FUSION_WORKFLOW += integrate_rnaseq
 
 rnaseq_fusion_workflow : $(RNASEQ_FUSION_WORKFLOW)
 
 include modules/fastq_tools/merge_split_fastq.mk
 include modules/structural_variants/defuse.mk
 include modules/structural_variants/fusioncatcher.mk
+include modules/structural_variants/integrateRnaseq.mk
 
 .DELETE_ON_ERROR:
 .SECONDARY:
