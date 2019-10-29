9df8928186c2e99824dc0c202bd82667768c7414
David Brown <ndbrown6@users.noreply.github.com>
2019-10-28 22:21:58 -0400

    Update rnaseq_fusion.mk

--- test/workflows/rnaseq_fusion.mk
+++ test/workflows/rnaseq_fusion.mk
@@ -3,7 +3,9 @@ include modules/Makefile.inc
 LOGDIR ?= log/rnaseq_fusion.$(NOW)
 PHONY += fastq defuse fusion_catcher
 
-RNASEQ_FUSION_WORKFLOW += clip_umi
+RNASEQ_FUSION_WORKFLOW += fastq
+RNASEQ_FUSION_WORKFLOW += defuse
+RNASEQ_FUSION_WORKFLOW += fusion_catcher
 
 rnaseq_fusion_workflow : $(RNASEQ_FUSION_WORKFLOW)
 
