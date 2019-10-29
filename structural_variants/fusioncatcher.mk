81823423099934f34520ba98e39037698048b5f9
David Brown <ndbrown6@users.noreply.github.com>
2019-10-28 21:46:31 -0400

    Update fusioncatcher.mk

--- structural_variants/fusioncatcher.mk
+++ structural_variants/fusioncatcher.mk
@@ -3,25 +3,25 @@ include modules/Makefile.inc
 LOGDIR ?= log/fusion_catcher.$(NOW)
 .PHONY += fusion_catcher
 
-PATH=$(HOME)/share/usr/src/fusioncatcher/bin:$PATH
-PATH=$(HOME)/share/usr/src/fusioncatcher/tools/bowtie-1.1.2-linux-x86_64:$PATH
-PATH=$(HOME)/share/usr/src/fusioncatcher/tools/bowtie2-2.2.9-linux-x86_64:$PATH
-PATH=$(HOME)/share/usr/src/fusioncatcher/tools/bwa/:$PATH
-PATH=$(HOME)/share/usr/src/fusioncatcher/tools/blat:$PATH
-PATH=$(HOME)/share/usr/src/fusioncatcher/tools/STAR-2.5.2b/source/:$PATH
-PATH=$(HOME)/share/usr/src/fusioncatcher/tools/liftover:$PATH
-PATH=$(HOME)/share/usr/src/fusioncatcher/tools/1.2-r101c:$PATH
-PATH=$(HOME)/share/usr/src/fusioncatcher/tools/sratoolkit/bin:$PATH
-PATH=$(HOME)/share/usr/src/fusioncatcher/tools/velvet/:$PATH
-PATH=$(HOME)/share/usr/src/fusioncatcher/tools/fatotwobit/:$PATH
-PATH=$(HOME)/share/usr/src/fusioncatcher/tools/lzop/src/:$PATH
-PATH=$(HOME)/share/usr/src/fusioncatcher/tools/coreutils/src/:$PATH
-PATH=$(HOME)/share/usr/src/fusioncatcher/tools/pigz/:$PATH
-PATH=$(HOME)/share/usr/src/fusioncatcher/tools/samtools/:$PATH
-PATH=$(HOME)/share/usr/src/fusioncatcher/tools/BBMap_37.28/:$PATH
-PATH=$(HOME)/share/usr/src/fusioncatcher/tools/picard/:$PATH
-FUSION_CATCHER = $(HOME)/share/usr/fusioncatcher/bin/fusioncatcher
-FUSIONCATCHER_OPTS = -d $(HOME)/share/usr/fusioncatcher/data/current --extract-buffer-size=35000000000
+#PATH=$(HOME)/share/usr/src/fusioncatcher/bin:$PATH
+#PATH=$(HOME)/share/usr/src/fusioncatcher/tools/bowtie-1.1.2-linux-x86_64:$PATH
+#PATH=$(HOME)/share/usr/src/fusioncatcher/tools/bowtie2-2.2.9-linux-x86_64:$PATH
+#PATH=$(HOME)/share/usr/src/fusioncatcher/tools/bwa/:$PATH
+#PATH=$(HOME)/share/usr/src/fusioncatcher/tools/blat:$PATH
+#PATH=$(HOME)/share/usr/src/fusioncatcher/tools/STAR-2.5.2b/source/:$PATH
+#PATH=$(HOME)/share/usr/src/fusioncatcher/tools/liftover:$PATH
+#PATH=$(HOME)/share/usr/src/fusioncatcher/tools/1.2-r101c:$PATH
+#PATH=$(HOME)/share/usr/src/fusioncatcher/tools/sratoolkit/bin:$PATH
+#PATH=$(HOME)/share/usr/src/fusioncatcher/tools/velvet/:$PATH
+#PATH=$(HOME)/share/usr/src/fusioncatcher/tools/fatotwobit/:$PATH
+#PATH=$(HOME)/share/usr/src/fusioncatcher/tools/lzop/src/:$PATH
+#PATH=$(HOME)/share/usr/src/fusioncatcher/tools/coreutils/src/:$PATH
+#PATH=$(HOME)/share/usr/src/fusioncatcher/tools/pigz/:$PATH
+#PATH=$(HOME)/share/usr/src/fusioncatcher/tools/samtools/:$PATH
+#PATH=$(HOME)/share/usr/src/fusioncatcher/tools/BBMap_37.28/:$PATH
+#PATH=$(HOME)/share/usr/src/fusioncatcher/tools/picard/:$PATH
+#FUSION_CATCHER = $(HOME)/share/usr/fusioncatcher/bin/fusioncatcher
+#FUSIONCATCHER_OPTS = -d $(HOME)/share/usr/fusioncatcher/data/current --extract-buffer-size=35000000000
 
 fusion_catcher : $(foreach sample,$(SAMPLES),fusion_catcher/$(sample)/$(sample).1.fastq.gz) \
 		 		 $(foreach sample,$(SAMPLES),fusion_catcher/$(sample)/$(sample).2.fastq.gz)
