f33065403601bf2813439dc363e1b70fdc3ac916
David Brown <ndbrown6@users.noreply.github.com>
2019-10-31 16:21:03 -0400

    Update fusion_summary.mk

--- summary/fusion_summary.mk
+++ summary/fusion_summary.mk
@@ -3,17 +3,13 @@ include modules/Makefile.inc
 LOGDIR ?= log/fusion_summary.$(NOW)
 PHONY += fusion_summary
 
-fusion_summary : summary/tsv/fusion_summary.tsv \
-				 summary/fusion_summary.xlsx
+fusion_summary : summary/fusion_summary.tsv
 
-summary/tsv/fusion_summary.tsv : $(wildcard $(foreach sample,$(TUMOR_SAMPLES),defuse/$(sample).taskcomplete)) $(wildcard $(foreach sample,$(TUMOR_SAMPLES),fusion_catcher/$(sample)/out/taskcomplete)) $(wildcard $(foreach sample,$(TUMOR_SAMPLES),integrate_rnaseq/breakpoints/$(sample).breakpoints.tsv))
+summary/fusion_summary.tsv : integrate_rnaseq/summary.tsv fusion_catcher/summary.tsv defuse/summary.tsv
 	$(call RUN,-n 1 -s 4G -m 4G,"set -o pipefail && \
+								 mkdir -p summary && \
 								 do something here ...")
 				     			 
-summary/fusion_summary.xlsx : summary/tsv/fusion_summary.tsv
-	$(call RUN,-n 1 -s 4G -m 4G,"set -o pipefail && \
-								 do something here ...")
-
 .DELETE_ON_ERROR:
 .SECONDARY:
 .PHONY: $(PHONY)
