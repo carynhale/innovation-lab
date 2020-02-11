8ae392ef28506ffd31663b61027a95cbc1f8f537
David Brown <ndbrown6@gmail.com>
2020-02-11 13:48:25 -0500

    Update unmapped_fasta.mk

--- bam_tools/unmapped_fasta.mk
+++ bam_tools/unmapped_fasta.mk
@@ -5,7 +5,7 @@ LOGDIR ?= log/unmapped_fasta.$(NOW)
 unmapped_fasta : $(foreach sample,$(SAMPLES),unmapped_reads/$(sample).fasta)
 
 define extract-unmapped-reads
-unmapped_reads/%.fasta : unmapped_reads/%.bam
+unmapped_reads/%.fasta : bam/%.bam
 	$$(call RUN,-c -n 4 -s 4G -m 6G,"$(SAMTOOLS) fasta $$(<) > unmapped_reads/$$(*).fasta")
 endef
 $(foreach sample,$(SAMPLES),\
