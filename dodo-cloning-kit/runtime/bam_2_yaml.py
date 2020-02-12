855da23891e4b20184da4c77705db875089c15d9
David Brown <ndbrown6@gmail.com>
2020-02-11 20:15:05 -0500

    Update bam_2_yaml.py

--- dodo-cloning-kit/runtime/bam_2_yaml.py
+++ dodo-cloning-kit/runtime/bam_2_yaml.py
@@ -12,62 +12,18 @@ parser.add_argument('--sample_file', help='sample yaml output file', type=argpar
 args = parser.parse_args()
 
 bam_files = []
-for bams_dir in args.bam_dir:
-    bamFiles = glob2.glob(bam_dir + '*' + args.bam_suffix)
-    r1fastqs = [x for x in fastqFiles if re.search(r'_S\d+_R1_', x)]
-    for r1fastq in r1fastqs:
-       r2fastq = re.sub('_(S\d+)_R1_', '_\g<1>_R2_', r1fastq)
-       assert(any([r2fastq == x for x in fastqFiles]))
-       paired_fastqs.append([r1fastq, r2fastq])
+all_bams = glob2.glob(args.bam_dir + '*' + args.bam_suffix)
+for bam_file in all_bams:
+    if bam_file.startswith('bam/') and bam_file.endswith('.bam'):
+        bam_file = bam_file[4:]
+        bam_file = bam_file[:-4]
+        bam_files.append([bam_file])
+        
+all_samples = []
+for sample in bam_files:
+    all_samples.append({'name': sample, 'tumor': [sample]})
 
-sample_fastqs = {}
-for pair in paired_fastqs:
-    sample = re.search(r'Sample_([^/]+)', pair[0]).group(1)
-    sample = re.sub('_IGO.*', '', sample)
-    sample = re.sub('_', '-', sample)
-    if sample not in sample_fastqs:
-        sample_fastqs[sample] = []
-    sample_fastqs[sample].append(pair)
-
-yaml.dump(sample_fastqs, args.sample_fastq_file,
-    default_style=None,
-    default_flow_style=None,
-    encoding='utf-8',
-    explicit_start=None,
-    explicit_end=None,
-    version=None,
-    tags=None,
-    canonical=None,
-    indent=None,
-    width=None,
-    allow_unicode=None,
-    line_break=None)
-
-normals = set()
-tumors = set()
-for s in list(sample_fastqs.keys()):
-    if s.endswith('N'):
-        normals.add(s)
-    else:
-        tumors.add(s)
-
-samples = []
-unmatched_tumors = tumors
-for norm in normals:
-    name = re.sub(r'N$', '', norm)
-    tums = [x for x in tumors if x.startswith(name)]
-    if tums is not None:
-        for x in tums:
-            unmatched_tumors.discard(x)
-        samples.append({'name': name, 'normal': norm, 'tumor': tums})
-    else:
-        samples.append({'name': name, 'normal': norm})
-
-for unmatched_tum in unmatched_tumors:
-    name = re.sub(r'T$', '', unmatched_tum)
-    samples.append({'name': name, 'tumor': [unmatched_tum]})
-
-yaml.dump(samples, args.sample_file,
+yaml.dump(all_samples, sample_file,
     default_style=None,
     default_flow_style=None,
     encoding='utf-8',
