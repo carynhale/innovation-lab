522c562d87d55772eac0776d9c518352a691c9f1
David Brown <ndbrown6@gmail.com>
2020-02-12 13:45:51 -0500

    Collapse UMI

--- workflows/msk_access.mk
+++ workflows/msk_access.mk
@@ -15,7 +15,8 @@ LOGDIR ?= log/msk_access.$(NOW)
 
 msk_access : $(foreach sample,$(SAMPLES),marianas/$(sample)/$(sample)_R1.fastq.gz) \
 		   	 $(foreach sample,$(SAMPLES),marianas/$(sample)/$(sample)_R1_umi-clipped.fastq.gz) \
-		   	 $(foreach sample,$(SAMPLES),marianas/$(sample)/$(sample).standard.bam)
+		   	 $(foreach sample,$(SAMPLES),marianas/$(sample)/$(sample).standard.bam) \
+		   	 $(foreach sample,$(SAMPLES),marianas/$(sample)/second-pass-alt-alleles.txt)
 		   	 
 MARIANAS_UMI_LENGTH ?= 3
 MARIANAS_MIN_MAPQ ?= 1
@@ -146,8 +147,50 @@ marianas/$1/$1.standard.bam : marianas/$1/$1.recal.bam
 endef
 $(foreach sample,$(SAMPLES),\
 		$(eval $(call fastq-to-bam,$(sample))))
+		
+define genotype-and-collapse
+marianas/$1/$1.standard-pileup.txt : marianas/$1/$1.standard.bam
+	$$(call RUN,-c -n 1 -s 8G -m 12G,"set -o pipefail && \
+									  cd marianas/$1 && \
+									  $(JAVA) -server -Xms2G -Xmx8G -cp $(WALTZ) org.mskcc.juber.waltz.Waltz PileupMetrics $(WALTZ_MIN_MAPQ) $1.standard.bam $(REF_FASTA) $(WALTZ_BED_FILE) && \
+									  cd ../..")
+									  
+marianas/$1/first-pass.mate-position-sorted.txt : marianas/$1/$1.standard-pileup.txt
+	$$(call RUN,-c -n 1 -s 8G -m 12G,"set -o pipefail && \
+									  cd marianas/$1 && \
+									  $(JAVA) -server -Xms2G -Xmx8G -cp $(MARIANAS) org.mskcc.marianas.umi.duplex.DuplexUMIBamToCollapsedFastqFirstPass \
+									  $1.standard.bam \
+									  $1.standard-pileup.txt \
+									  $(MARIANAS_MIN_MAPQ) \
+									  $(MARIANAS_MIN_BAQ) \
+									  $(MARIANAS_MISMATCH) \
+									  $(MARIANAS_WOBBLE) \
+									  $(MARIANAS_MIN_CONSENSUS) \
+									  $(REF_FASTA) && \
+									  sort -n -s -S 6G -k 6 -k 8 first-pass.txt > first-pass.mate-position-sorted.txt && \
+									  cd ../..")
 
-# include modules/test/bam_tools/collapseumi.mk
+
+marianas/$1/second-pass-alt-alleles.txt : marianas/$1/first-pass.mate-position-sorted.txt
+	$$(call RUN,-c -n 1 -s 8G -m 12G,"set -o pipefail && \
+									  cd marianas/$1 && \
+									  $(JAVA) -server -Xms2G -Xmx8G -cp $(MARIANAS) org.mskcc.marianas.umi.duplex.DuplexUMIBamToCollapsedFastqSecondPass \
+									  $1.standard.bam \
+									  $1.standard-pileup.txt \
+									  $(MARIANAS_MIN_MAPQ) \
+									  $(MARIANAS_MIN_BAQ) \
+									  $(MARIANAS_MISMATCH) \
+									  $(MARIANAS_WOBBLE) \
+									  $(MARIANAS_MIN_CONSENSUS) \
+									  $(REF_FASTA) \
+									  first-pass.mate-position-sorted.txt && \
+									  cd ../..")
+									  									  
+endef
+$(foreach sample,$(SAMPLES),\
+	$(eval $(call genotype-and-collapse,$(sample))))
+
+
 # include modules/test/bam_tools/aligncollapsed.mk
 # include modules/test/bam_tools/copybam.mk
 # include modules/test/qc/intervalmetrics.mk
