abfd6ab78ca455621943b480a76e58840a63694c
David Brown <ndbrown6@gmail.com>
2020-02-11 20:25:58 -0500

    Update bam_2_yaml.py

--- dodo-cloning-kit/runtime/bam_2_yaml.py
+++ dodo-cloning-kit/runtime/bam_2_yaml.py
@@ -7,17 +7,17 @@ import re
 
 parser = argparse.ArgumentParser(prog='bam_2_yaml.py', description='Create samples.yaml from a bam directory')
 parser.add_argument('bam_dir', nargs = '+')
-parser.add_argument('--bam_suffix', default='bam')
+parser.add_argument('--bam_suffix', default='.bam')
 parser.add_argument('--sample_file', help='sample yaml output file', type=argparse.FileType('w'), nargs='?', default='samples.yaml')
 args = parser.parse_args()
 
 bam_files = []
 all_bams = glob2.glob(args.bam_dir + '*' + args.bam_suffix)
-for bam_file in all_bams:
-    if bam_file.startswith('bam/') and bam_file.endswith('.bam'):
-        #bam_file = bam_file[4:]
-        #bam_file = bam_file[:-4]
-        bam_files.append([bam_file])
+# for bam_file in all_bams:
+#     if bam_file.startswith('bam/') and bam_file.endswith('.bam'):
+#         bam_file = bam_file[4:]
+#         bam_file = bam_file[:-4]
+#         bam_files.append([bam_file])
         
 # all_samples = []
 # for sample in bam_files:
