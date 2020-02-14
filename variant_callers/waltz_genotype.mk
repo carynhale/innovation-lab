043913d9332fc6b1db006cdfe7de723b05003ff2
David Brown <ndbrown6@gmail.com>
2020-02-14 10:38:44 -0500

    Update waltz_genotype.mk

--- variant_callers/waltz_genotype.mk
+++ variant_callers/waltz_genotype.mk
@@ -8,7 +8,7 @@ waltz_genotype : $(foreach sample,$(SAMPLES),waltz/$(sample)-STANDARD-pileup.txt
 				 $(foreach sample,$(SAMPLES),waltz/$(sample)-COLLAPSED-pileup.txt.gz) \
 				 $(foreach sample,$(SAMPLES),waltz/$(sample)-SIMPLEX-pileup.txt.gz) \
 				 $(foreach sample,$(SAMPLES),waltz/$(sample)-DUPLEX-pileup.txt.gz) \
-				 $(foreach sample,$(SAMPLES),waltz/noise_metrics.txt)
+				 $(foreach sample,$(SAMPLES),waltz/noise_metrics_with_duplicates.txt)
 
 WALTZ_MIN_MAPQ ?= 15
 TARGETS_FILE_NOMSI ?= $(HOME)/share/lib/resource_files/MSK-ACCESS-v1_0-A-good-positions-noMSI.txt
