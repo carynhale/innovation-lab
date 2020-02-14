07a1b8c6ad0a029cef9529bfb489a3daa6773616
David Brown <ndbrown6@gmail.com>
2020-02-14 13:01:22 -0500

    Update waltz_genotype.mk

--- variant_callers/waltz_genotype.mk
+++ variant_callers/waltz_genotype.mk
@@ -4,10 +4,10 @@ include innovation-lab/genome_inc/b37.inc
 
 LOGDIR ?= log/waltz_genotype.$(NOW)
 
-waltz_genotype : $(foreach sample,$(SAMPLES),waltz/$(sample)-STANDARD-pileup.txt.gz) \
-				 $(foreach sample,$(SAMPLES),waltz/$(sample)-COLLAPSED-pileup.txt.gz) \
-				 $(foreach sample,$(SAMPLES),waltz/$(sample)-SIMPLEX-pileup.txt.gz) \
-				 $(foreach sample,$(SAMPLES),waltz/$(sample)-DUPLEX-pileup.txt.gz) \
+waltz_genotype : $(foreach sample,$(SAMPLES),waltz/$(sample)-pileup.txt.gz) \
+				 $(foreach sample,$(SAMPLES),waltz/$(sample)__aln_srt_IR_FX-pileup.txt.gz) \
+				 $(foreach sample,$(SAMPLES),waltz/$(sample)__aln_srt_IR_FX-simplex-pileup.txt.gz) \
+				 $(foreach sample,$(SAMPLES),waltz/$(sample)__aln_srt_IR_FX-duplex-pileup.txt.gz) \
 				 $(foreach sample,$(SAMPLES),waltz/noise_metrics_with_duplicates.txt) \
 				 $(foreach sample,$(SAMPLES),waltz/noise_metrics_without_duplicates.txt)
 
@@ -15,56 +15,56 @@ WALTZ_MIN_MAPQ ?= 15
 TARGETS_FILE_NOMSI ?= $(HOME)/share/lib/resource_files/MSK-ACCESS-v1_0-A-good-positions-noMSI.txt
 
 define waltz-genotype
-waltz/$1-STANDARD-pileup.txt.gz : bam/$1-STANDARD.bam
+waltz/$1-pileup.txt.gz : bam/$1.bam
 	$$(call RUN,-c -n 4 -s 4G -m 6G,"set -o pipefail && \
 									 mkdir -p waltz && \
 									 cd waltz && \
-									 ln -sf ../bam/$1-STANDARD.bam $1-STANDARD.bam && \
-									 ln -sf ../bam/$1-STANDARD.bai $1-STANDARD.bai && \
-									 $$(call WALTZ_CMD,2G,8G) org.mskcc.juber.waltz.Waltz PileupMetrics $(WALTZ_MIN_MAPQ) $1-STANDARD.bam $(DMP_FASTA) $(TARGETS_FILE) && \
-									 gzip $1-STANDARD-pileup.txt && \
-									 gzip $1-STANDARD-pileup-without-duplicates.txt && \
-									 gzip $1-STANDARD-intervals.txt && \
-									 gzip $1-STANDARD-intervals-without-duplicates.txt && \
+									 ln -sf ../bam/$1.bam $1.bam && \
+									 ln -sf ../bam/$1.bai $1.bai && \
+									 $$(call WALTZ_CMD,2G,8G) org.mskcc.juber.waltz.Waltz PileupMetrics $(WALTZ_MIN_MAPQ) $1.bam $(DMP_FASTA) $(TARGETS_FILE) && \
+									 gzip $1-pileup.txt && \
+									 gzip $1-pileup-without-duplicates.txt && \
+									 gzip $1-intervals.txt && \
+									 gzip $1-intervals-without-duplicates.txt && \
 									 cd ..")
 									 
-waltz/$1-COLLAPSED-pileup.txt.gz : bam/$1-COLLAPSED.bam
+waltz/$1__aln_srt_IR_FX-pileup.txt.gz : bam/$1__aln_srt_IR_FX.bam
 	$$(call RUN,-c -n 4 -s 4G -m 6G,"set -o pipefail && \
 									 mkdir -p waltz && \
 									 cd waltz && \
-									 ln -sf ../bam/$1-COLLAPSED.bam $1-COLLAPSED.bam && \
-									 ln -sf ../bam/$1-COLLAPSED.bai $1-COLLAPSED.bai && \
-									 $$(call WALTZ_CMD,2G,8G) org.mskcc.juber.waltz.Waltz PileupMetrics $(WALTZ_MIN_MAPQ) $1-COLLAPSED.bam $(DMP_FASTA) $(TARGETS_FILE) && \
-									 gzip $1-COLLAPSED-pileup.txt && \
-									 gzip $1-COLLAPSED-pileup-without-duplicates.txt && \
-									 gzip $1-COLLAPSED-intervals.txt && \
-									 gzip $1-COLLAPSED-intervals-without-duplicates.txt && \
+									 ln -sf ../bam/$1__aln_srt_IR_FX.bam $1__aln_srt_IR_FX.bam && \
+									 ln -sf ../bam/$1__aln_srt_IR_FX.bai $1__aln_srt_IR_FX.bai && \
+									 $$(call WALTZ_CMD,2G,8G) org.mskcc.juber.waltz.Waltz PileupMetrics $(WALTZ_MIN_MAPQ) $1__aln_srt_IR_FX.bam $(DMP_FASTA) $(TARGETS_FILE) && \
+									 gzip $1__aln_srt_IR_FX-pileup.txt && \
+									 gzip $1__aln_srt_IR_FX-pileup-without-duplicates.txt && \
+									 gzip $1__aln_srt_IR_FX-intervals.txt && \
+									 gzip $1__aln_srt_IR_FX-intervals-without-duplicates.txt && \
 									 cd ..")
 									 
-waltz/$1-SIMPLEX-pileup.txt.gz : bam/$1-SIMPLEX.bam
+waltz/$1__aln_srt_IR_FX-simplex-pileup.txt.gz : bam/$1__aln_srt_IR_FX-simplex.bam
 	$$(call RUN,-c -n 4 -s 4G -m 6G,"set -o pipefail && \
 									 mkdir -p waltz && \
 									 cd waltz && \
-									 ln -sf ../bam/$1-SIMPLEX.bam $1-SIMPLEX.bam && \
-									 ln -sf ../bam/$1-SIMPLEX.bai $1-SIMPLEX.bai && \
-									 $$(call WALTZ_CMD,2G,8G) org.mskcc.juber.waltz.Waltz PileupMetrics $(WALTZ_MIN_MAPQ) $1-SIMPLEX.bam $(DMP_FASTA) $(TARGETS_FILE) && \
-									 gzip $1-SIMPLEX-pileup.txt && \
-									 gzip $1-SIMPLEX-pileup-without-duplicates.txt && \
-									 gzip $1-SIMPLEX-intervals.txt && \
-									 gzip $1-SIMPLEX-intervals-without-duplicates.txt && \
+									 ln -sf ../bam/$1__aln_srt_IR_FX-simplex.bam $1__aln_srt_IR_FX-simplex.bam && \
+									 ln -sf ../bam/$1__aln_srt_IR_FX-simplex.bai $1__aln_srt_IR_FX-simplex.bai && \
+									 $$(call WALTZ_CMD,2G,8G) org.mskcc.juber.waltz.Waltz PileupMetrics $(WALTZ_MIN_MAPQ) $1__aln_srt_IR_FX-simplex.bam $(DMP_FASTA) $(TARGETS_FILE) && \
+									 gzip $1__aln_srt_IR_FX-simplex-pileup.txt && \
+									 gzip $1__aln_srt_IR_FX-simplex-pileup-without-duplicates.txt && \
+									 gzip $1__aln_srt_IR_FX-simplex-intervals.txt && \
+									 gzip $1__aln_srt_IR_FX-simplex-intervals-without-duplicates.txt && \
 									 cd ..")
 									 
-waltz/$1-DUPLEX-pileup.txt.gz : bam/$1-DUPLEX.bam
+waltz/$1__aln_srt_IR_FX-duplex-pileup.txt.gz : bam/$1__aln_srt_IR_FX-duplex.bam
 	$$(call RUN,-c -n 4 -s 4G -m 6G,"set -o pipefail && \
 									 mkdir -p waltz && \
 									 cd waltz && \
-									 ln -sf ../bam/$1-DUPLEX.bam $1-DUPLEX.bam && \
-									 ln -sf ../bam/$1-DUPLEX.bai $1-DUPLEX.bai && \
-									 $$(call WALTZ_CMD,2G,8G) org.mskcc.juber.waltz.Waltz PileupMetrics $(WALTZ_MIN_MAPQ) $1-DUPLEX.bam $(DMP_FASTA) $(TARGETS_FILE) && \
-									 gzip $1-DUPLEX-pileup.txt && \
-									 gzip $1-DUPLEX-pileup-without-duplicates.txt && \
-									 gzip $1-DUPLEX-intervals.txt && \
-									 gzip $1-DUPLEX-intervals-without-duplicates.txt && \
+									 ln -sf ../bam/$1__aln_srt_IR_FX-duplex.bam $1__aln_srt_IR_FX-duplex.bam && \
+									 ln -sf ../bam/$1__aln_srt_IR_FX-duplex.bai $1__aln_srt_IR_FX-duplex.bai && \
+									 $$(call WALTZ_CMD,2G,8G) org.mskcc.juber.waltz.Waltz PileupMetrics $(WALTZ_MIN_MAPQ) $1__aln_srt_IR_FX-duplex.bam $(DMP_FASTA) $(TARGETS_FILE) && \
+									 gzip $1__aln_srt_IR_FX-duplex-pileup.txt && \
+									 gzip $1__aln_srt_IR_FX-duplex-pileup-without-duplicates.txt && \
+									 gzip $1__aln_srt_IR_FX-duplex-intervals.txt && \
+									 gzip $1__aln_srt_IR_FX-duplex-intervals-without-duplicates.txt && \
 									 cd ..")
 
 
@@ -72,13 +72,13 @@ endef
 $(foreach sample,$(SAMPLES),\
 		$(eval $(call waltz-genotype,$(sample))))
 		
-waltz/noise_metrics_with_duplicates.txt : $(wildcard waltz/$(SAMPLES)-STANDARD-pileup.txt.gz) $(wildcard waltz/$(SAMPLES)-COLLAPSED-pileup.txt.gz) $(wildcard waltz/$(SAMPLES)-SIMPLEX-pileup.txt.gz) $(wildcard waltz/$(SAMPLES)-DUPLEX-pileup.txt.gz)
+waltz/noise_metrics_with_duplicates.txt : $(wildcard waltz/$(SAMPLES)-pileup.txt.gz) $(wildcard waltz/$(SAMPLES)__aln_srt_IR_FX-pileup.txt.gz) $(wildcard waltz/$(SAMPLES)__aln_srt_IR_FX-simplex-pileup.txt.gz) $(wildcard waltz/$(SAMPLES)__aln_srt_IR_FX-duplex-pileup.txt.gz)
 	$(call RUN, -c -n 1 -s 8G -m 12G,"set -o pipefail && \
 									  $(RSCRIPT) $(SCRIPTS_DIR)/qc/waltz_metrics.R --type 1 --target_file $(TARGETS_FILE_NOMSI) --sample_names '$(SAMPLES)'")
 
-waltz/noise_metrics_without_duplicates.txt : $(wildcard waltz/$(SAMPLES)-STANDARD-pileup.txt.gz) $(wildcard waltz/$(SAMPLES)-COLLAPSED-pileup.txt.gz) $(wildcard waltz/$(SAMPLES)-SIMPLEX-pileup.txt.gz) $(wildcard waltz/$(SAMPLES)-DUPLEX-pileup.txt.gz)
+waltz/noise_metrics_without_duplicates.txt : $(wildcard waltz/$(SAMPLES)-pileup.txt.gz) $(wildcard waltz/$(SAMPLES)__aln_srt_IR_FX-pileup.txt.gz) $(wildcard waltz/$(SAMPLES)__aln_srt_IR_FX-simplex-pileup.txt.gz) $(wildcard waltz/$(SAMPLES)__aln_srt_IR_FX-duplex-pileup.txt.gz)
 	$(call RUN, -c -n 1 -s 8G -m 12G,"set -o pipefail && \
-									  $(RSCRIPT) $(SCRIPTS_DIR)/qc/waltz_metrics.R --type 1 --target_file $(TARGETS_FILE_NOMSI) --sample_names '$(SAMPLES)'")
+									  $(RSCRIPT) $(SCRIPTS_DIR)/qc/waltz_metrics.R --type 2 --target_file $(TARGETS_FILE_NOMSI) --sample_names '$(SAMPLES)'")
 
 
 ..DUMMY := $(shell mkdir -p version; \
