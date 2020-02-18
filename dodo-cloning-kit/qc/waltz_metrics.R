f8183d6c1400cc5ce082862c8dd9ea8ad539c90c
David Brown <ndbrown6@gmail.com>
2020-02-18 16:43:33 -0500

    Update waltz_metrics.R

--- dodo-cloning-kit/qc/waltz_metrics.R
+++ dodo-cloning-kit/qc/waltz_metrics.R
@@ -389,37 +389,37 @@ if (as.numeric(opt$type)==1) {
 	sample_names = unlist(strsplit(x=as.character(opt$sample_names), split=" ", fixed=TRUE))
 	x = list()
 	for (i in 1:length(sample_names)) {
-		df = read_tsv(file=paste0("waltz/", sample_names[i], "-pileup.txt.gz"), col_names = FALSE, col_types = cols(.default = col_character())) %>%
-			 readr::type_convert() %>%
-			 dplyr::select(`chrom`	= X1,
-						   `pos`	= X2,
-						   `ref`	= X3,
-						   `total`	= X4,
-						   `a`		= X5,
-						   `c`		= X6,
-						   `g`		= X7,
-						   `t`		= X8) %>%
-			 dplyr::mutate(`total_n` = a+c+g+t,
-			 			   `uuid` = paste0(chrom, ":", pos)) %>%
-			 dplyr::filter(uuid %in% target_positions$uuid) %>%
-			 dplyr::mutate(af_a = 100*a/(total_n+epsilon),
-						   af_c = 100*c/(total_n+epsilon),
-						   af_g = 100*g/(total_n+epsilon),
-						   af_t = 100*t/(total_n+epsilon))
-		x[[i]] = dplyr::tibble(`chrom` = rep(df$chrom, 4),
-							   `pos` = rep(df$pos, 4),
-							   `ref` = rep(df$ref, 4),
-							   `alt` = rep(c("A", "C", "G", "T"), each=nrow(df)),
-							   `af` = c(df$af_a, df$af_c, df$af_g,df$af_t)) %>%
-				 dplyr::mutate(af = ifelse(af>cutoffAF*100, 0, af)) %>%
-				 dplyr::filter(chrom %in% chr,
-				 			   ref != alt) %>%
-				 dplyr::arrange(chrom, pos, ref, alt) %>%
-				 dplyr::rename_at("af", funs(sample_names[i]))
+		y = read_tsv(file=paste0("waltz/", sample_names[i], "-pileup.txt.gz"), col_names = FALSE, col_types = cols(.default = col_character())) %>%
+			readr::type_convert() %>%
+			dplyr::select(`chrom`	= X1,
+						  `pos`		= X2,
+						  `ref`		= X3,
+						  `total`	= X4,
+						  `a`		= X5,
+						  `c`		= X6,
+						  `g`		= X7,
+						  `t`		= X8) %>%
+			dplyr::mutate(`total_n` = a+c+g+t,
+			 			  `uuid` = paste0(chrom, ":", pos)) %>%
+			dplyr::filter(uuid %in% target_positions$uuid) %>%
+			dplyr::mutate(af_a = 100*a/(total_n+epsilon),
+						  af_c = 100*c/(total_n+epsilon),
+						  af_g = 100*g/(total_n+epsilon),
+						  af_t = 100*t/(total_n+epsilon))
+		z = dplyr::tibble(`chrom` = rep(df$chrom, 4),
+						  `pos` = rep(df$pos, 4),
+						  `ref` = rep(df$ref, 4),
+						  `alt` = rep(c("A", "C", "G", "T"), each=nrow(df)),
+						  `freq` = c(df$af_a, df$af_c, df$af_g,df$af_t)) %>%
+			dplyr::filter(chrom %in% chr,
+						  alt != ref)
+			dplyr::mutate(freq = ifelse(freq>(cutoffAF*100), 0, freq)) %>%
+			dplyr::arrange(chrom, pos, ref, alt) %>%
+			dplyr::rename_at("freq", funs(sample_names[i]))
 		if (i!=1) {
-			x[[i]] = x[[i]] %>%
-					 dplyr::select(-chrom, -pos, -ref, -alt)
+			z = z %>% dplyr::select(-chrom, -pos, -ref, -alt)
 		}
+		x[[i]] = z
 	}
 	x = do.call(cbind, x)
 	write_tsv(x, path="waltz/noise_by_position_standard_with_duplicates.txt", na = "NA", append = FALSE, col_names = TRUE)
@@ -433,7 +433,7 @@ if (as.numeric(opt$type)==1) {
 	sample_names = unlist(strsplit(x=as.character(opt$sample_names), split=" ", fixed=TRUE))
 	x = list()
 	for (i in 1:length(sample_names)) {
-		df = read_tsv(file=paste0("waltz/", sample_names[i], "-pileup-without-duplicates.txt.gz"), col_names = FALSE, col_types = cols(.default = col_character())) %>%
+		y = read_tsv(file=paste0("waltz/", sample_names[i], "-pileup-without-duplicates.txt.gz"), col_names = FALSE, col_types = cols(.default = col_character())) %>%
 			 readr::type_convert() %>%
 			 dplyr::select(`chrom`	= X1,
 						   `pos`	= X2,
@@ -476,37 +476,37 @@ if (as.numeric(opt$type)==1) {
 	sample_names = unlist(strsplit(x=as.character(opt$sample_names), split=" ", fixed=TRUE))
 	x = list()
 	for (i in 1:length(sample_names)) {
-		df = read_tsv(file=paste0("waltz/", sample_names[i], "__aln_srt_IR_FX-simplex-pileup-without-duplicates.txt.gz"), col_names = FALSE, col_types = cols(.default = col_character())) %>%
-			 readr::type_convert() %>%
-			 dplyr::select(`chrom`	= X1,
-						   `pos`	= X2,
-						   `ref`	= X3,
-						   `total`	= X4,
-						   `a`		= X5,
-						   `c`		= X6,
-						   `g`		= X7,
-						   `t`		= X8) %>%
-			 dplyr::mutate(`total_n` = a+c+g+t,
-			 			   `uuid` = paste0(chrom, ":", pos)) %>%
-			 dplyr::filter(uuid %in% target_positions$uuid) %>%
-			 dplyr::mutate(af_a = 100*a/(total_n+epsilon),
-						   af_c = 100*c/(total_n+epsilon),
-						   af_g = 100*g/(total_n+epsilon),
-						   af_t = 100*t/(total_n+epsilon))
-		x[[i]] = dplyr::tibble(`chrom` = rep(df$chrom, 4),
-							   `pos` = rep(df$pos, 4),
-							   `ref` = rep(df$ref, 4),
-							   `alt` = rep(c("A", "C", "G", "T"), each=nrow(df)),
-							   `af` = c(df$af_a, df$af_c, df$af_g,df$af_t)) %>%
-				 dplyr::mutate(af = ifelse(af>cutoffAF*100, 0, af)) %>%
-				 dplyr::filter(chrom %in% chr,
-				 			   ref != alt) %>%
-				 dplyr::arrange(chrom, pos, ref, alt) %>%
-				 dplyr::rename_at("af", funs(sample_names[i]))
+		y = read_tsv(file=paste0("waltz/", sample_names[i], "__aln_srt_IR_FX-simplex-pileup-without-duplicates.txt.gz"), col_names = FALSE, col_types = cols(.default = col_character())) %>%
+			readr::type_convert() %>%
+			dplyr::select(`chrom`	= X1,
+						  `pos`		= X2,
+						  `ref`		= X3,
+						  `total`	= X4,
+						  `a`		= X5,
+						  `c`		= X6,
+						  `g`		= X7,
+						  `t`		= X8) %>%
+			dplyr::mutate(`total_n` = a+c+g+t,
+			 			  `uuid` = paste0(chrom, ":", pos)) %>%
+			dplyr::filter(uuid %in% target_positions$uuid) %>%
+			dplyr::mutate(af_a = 100*a/(total_n+epsilon),
+						  af_c = 100*c/(total_n+epsilon),
+						  af_g = 100*g/(total_n+epsilon),
+						  af_t = 100*t/(total_n+epsilon))
+		z = dplyr::tibble(`chrom` = rep(df$chrom, 4),
+						  `pos` = rep(df$pos, 4),
+						  `ref` = rep(df$ref, 4),
+						  `alt` = rep(c("A", "C", "G", "T"), each=nrow(df)),
+						  `freq` = c(df$af_a, df$af_c, df$af_g,df$af_t)) %>%
+			dplyr::filter(chrom %in% chr,
+						  alt != ref)
+			dplyr::mutate(freq = ifelse(freq>(cutoffAF*100), 0, freq)) %>%
+			dplyr::arrange(chrom, pos, ref, alt) %>%
+			dplyr::rename_at("freq", funs(sample_names[i]))
 		if (i!=1) {
-			x[[i]] = x[[i]] %>%
-					 dplyr::select(-chrom, -pos, -ref, -alt)
+			z = z %>% dplyr::select(-chrom, -pos, -ref, -alt)
 		}
+		x[[i]] = z
 	}
 	x = do.call(cbind, x)
 	write_tsv(x, path="waltz/noise_by_position_simplex_without_duplicates.txt", na = "NA", append = FALSE, col_names = TRUE)
@@ -520,37 +520,37 @@ if (as.numeric(opt$type)==1) {
 	sample_names = unlist(strsplit(x=as.character(opt$sample_names), split=" ", fixed=TRUE))
 	x = list()
 	for (i in 1:length(sample_names)) {
-		df = read_tsv(file=paste0("waltz/", sample_names[i], "__aln_srt_IR_FX-duplex-pileup-without-duplicates.txt.gz"), col_names = FALSE, col_types = cols(.default = col_character())) %>%
-			 readr::type_convert() %>%
-			 dplyr::select(`chrom`	= X1,
-						   `pos`	= X2,
-						   `ref`	= X3,
-						   `total`	= X4,
-						   `a`		= X5,
-						   `c`		= X6,
-						   `g`		= X7,
-						   `t`		= X8) %>%
-			 dplyr::mutate(`total_n` = a+c+g+t,
-			 			   `uuid` = paste0(chrom, ":", pos)) %>%
-			 dplyr::filter(uuid %in% target_positions$uuid) %>%
-			 dplyr::mutate(af_a = 100*a/(total_n+epsilon),
-						   af_c = 100*c/(total_n+epsilon),
-						   af_g = 100*g/(total_n+epsilon),
-						   af_t = 100*t/(total_n+epsilon))
-		x[[i]] = dplyr::tibble(`chrom` = rep(df$chrom, 4),
-							   `pos` = rep(df$pos, 4),
-							   `ref` = rep(df$ref, 4),
-							   `alt` = rep(c("A", "C", "G", "T"), each=nrow(df)),
-							   `af` = c(df$af_a, df$af_c, df$af_g,df$af_t)) %>%
-				 dplyr::mutate(af = ifelse(af>cutoffAF*100, 0, af)) %>%
-				 dplyr::filter(chrom %in% chr,
-				 			   ref != alt) %>%
-				 dplyr::arrange(chrom, pos, ref, alt) %>%
-				 dplyr::rename_at("af", funs(sample_names[i]))
+		y = read_tsv(file=paste0("waltz/", sample_names[i], "__aln_srt_IR_FX-duplex-pileup-without-duplicates.txt.gz"), col_names = FALSE, col_types = cols(.default = col_character())) %>%
+			readr::type_convert() %>%
+			dplyr::select(`chrom`	= X1,
+						  `pos`		= X2,
+						  `ref`		= X3,
+						  `total`	= X4,
+						  `a`		= X5,
+						  `c`		= X6,
+						  `g`		= X7,
+						  `t`		= X8) %>%
+			dplyr::mutate(`total_n` = a+c+g+t,
+			 			  `uuid` = paste0(chrom, ":", pos)) %>%
+			dplyr::filter(uuid %in% target_positions$uuid) %>%
+			dplyr::mutate(af_a = 100*a/(total_n+epsilon),
+						  af_c = 100*c/(total_n+epsilon),
+						  af_g = 100*g/(total_n+epsilon),
+						  af_t = 100*t/(total_n+epsilon))
+		z = dplyr::tibble(`chrom` = rep(df$chrom, 4),
+						  `pos` = rep(df$pos, 4),
+						  `ref` = rep(df$ref, 4),
+						  `alt` = rep(c("A", "C", "G", "T"), each=nrow(df)),
+						  `freq` = c(df$af_a, df$af_c, df$af_g,df$af_t)) %>%
+			dplyr::filter(chrom %in% chr,
+						  alt != ref)
+			dplyr::mutate(freq = ifelse(freq>(cutoffAF*100), 0, freq)) %>%
+			dplyr::arrange(chrom, pos, ref, alt) %>%
+			dplyr::rename_at("freq", funs(sample_names[i]))
 		if (i!=1) {
-			x[[i]] = x[[i]] %>%
-					 dplyr::select(-chrom, -pos, -ref, -alt)
+			z = z %>% dplyr::select(-chrom, -pos, -ref, -alt)
 		}
+		x[[i]] = z
 	}
 	x = do.call(cbind, x)
 	write_tsv(x, path="waltz/noise_by_position_duplex_without_duplicates.txt", na = "NA", append = FALSE, col_names = TRUE)
