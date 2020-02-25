f055411409a2572cf6e3f089cca658ff587369a5
David Brown <ndbrown6@gmail.com>
2020-02-25 08:56:31 -0500

    Hides RData workspace

--- vcf_tools/annotate_vcf_context.mk
+++ vcf_tools/annotate_vcf_context.mk
@@ -3,17 +3,17 @@ include innovation-lab/genome_inc/b37.inc
 
 LOGDIR ?= log/annotate_vcf_context.$(NOW)
 
-annotate_vcf_context : vcf/external.txt
+annotate_vcf_context : vcf/target_vcf.txt
 
 ANNOTATION_ENV = $(HOME)/share/usr/env/r-bsgenome.hsapiens.ucsc.hg19-1.4.0
 
-vcf/external.txt : vcf/external.vcf
+vcf/target_vcf.txt : vcf/target_vcf.vcf
 	$(call RUN,-s 24G -m 48G -v $(ANNOTATION_ENV),"set -o pipefail && \
 							                       $(RSCRIPT) $(SCRIPTS_DIR)/vcf_tools/annotate_vcf_context.R \
-                                                   --file_in vcf/external.vcf \
-                                                   --file_out vcf/external.txt \
+                                                   --file_in $(<) \
+                                                   --file_out $(@) \
                                                    --genome_build 'hg19' \
-                                                   --ensembl_gene 'ensgene.RData'")
+                                                   --ensembl_gene $(ENSEMBLE)")
 							  
 	
 ..DUMMY := $(shell mkdir -p version; \
