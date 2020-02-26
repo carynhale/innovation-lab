f91a2deafd1c465ea2481e78465328e479ab8807
David Brown <ndbrown6@gmail.com>
2020-02-26 17:34:59 -0500

    Update star_align.mk

--- aligners/star_align.mk
+++ aligners/star_align.mk
@@ -47,16 +47,16 @@ $(foreach ss,$(SPLIT_SAMPLES),\
     
 define copy-bam
 bam/$1.bam : star/$1.Aligned.sortedByCoord.out.bam
-    $$(call RUN,-n 1 -s 2G -m 4G,"set -o pipefail && \
-                                  cp $$(<) $$(@)")
+    $(call RUN,-n 1 -s 2G -m 4G,"set -o pipefail && \
+                                 cp $(<) $(@)")
 
 bam/$1.bam.bai : bam/$1.bam
-    $$(call RUN,-n 1 -s 2G -m 4G,"set -o pipefail && \
-                                  $$(SAMTOOLS) index $$(<)")
+    $(call RUN,-n 1 -s 2G -m 4G,"set -o pipefail && \
+                                 $(SAMTOOLS) index $(<)")
 
 bam/$1.bai : bam/$1.bam.bai
-    $$(call RUN,-n 1 -s 2G -m 4G,"set -o pipefail && \
-                                  cp $$(<) $$(@)")
+    $(call RUN,-n 1 -s 2G -m 4G,"set -o pipefail && \
+                                 cp $(<) $(@)")
 
 endef
 $(foreach sample,$(SAMPLES),\
