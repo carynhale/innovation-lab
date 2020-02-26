abda9b7951843f2dc234aa990652858a108c44d5
David Brown <ndbrown6@gmail.com>
2020-02-26 17:23:45 -0500

    Update star_align.mk

--- aligners/star_align.mk
+++ aligners/star_align.mk
@@ -42,14 +42,15 @@ star/$1.Aligned.sortedByCoord.out.bam : $3
 bam/$1.bam : star/$1.Aligned.sortedByCoord.out.bam
     $$(call RUN,-n 1 -s 2G -m 4G, "set -o pipefail && \
                                    cp $$(<) $$(@)")
-                                    
+
 bam/$1.bam.bai : bam/$1.bam
     $$(call RUN,-n 1 -s 2G -m 4G, "set -o pipefail && \
-                                   $(SAMTOOLS) index $$(<)")
-                                    
+                                   $$(SAMTOOLS) index $$(<)")
+
 bam/$1.bai : bam/$1.bam.bai
     $$(call RUN,-n 1 -s 2G -m 4G, "set -o pipefail && \
                                    cp $$(<) $$(@)")
+
 
 endef
 $(foreach ss,$(SPLIT_SAMPLES),\
