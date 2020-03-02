81b3c70022da8f6fa7ad42cd92912da94a70b4bc
David Brown <ndbrown6@gmail.com>
2020-03-02 09:42:40 -0500

    RNAseq metrics

--- qc/rnaseq_metrics.mk
+++ qc/rnaseq_metrics.mk
@@ -1,32 +1,27 @@
 include modules/Makefile.inc
-include modules/config/gatk.inc
 
-VPATH ?= bam
 LOGDIR = log/rnaseq_metrics.$(NOW)
 
+REF_FLAT ?= $(HOME)/share/lib/resource_files/refFlat_hg19.txt
 
-PLOT_RNASEQ_METRICS = $(RSCRIPT) $(SCRIPTS_DIR)/qc/rnaseq_metrics.R
+rnaseq_metrics : $(foreach sample,$(SAMPLES),metrics/$(sample).txt)
 
-COLLECT_RNASEQ_METRICS = $(JAVA) -Xmx7G -jar $(JARDIR)/CollectRnaSeqMetrics.jar VALIDATION_STRINGENCY=LENIENT
-STRAND_SPECIFICITY ?= NONE
+define rnaseq-metrics
+metrics/$1.txt : bam/$1.bam
+	$$(call RUN,-c -s 6G -m 12G,"set -o pipefail && \
+                                $$(COLLECT_RNASEQ_METRICS) \
+                                INPUT=$$(<) \
+                                OUTPUT=$$(@) \
+                                REF_FLAT=$$(REF_FLAT) \
+                                CHART_OUTPUT=metrics/$1.pdf")
+									 
+endef
+$(foreach sample,$(SAMPLES),\
+		$(eval $(call rnaseq-metrics,$(sample))))
 
-all : $(foreach sample,$(SAMPLES),metrics/$(sample).rnaseq_metrics) metrics/all.rnaseq_metrics metrics/all.normalized_coverage.rnaseq_metrics report
-
-report : metrics/rnaseq_report/index.html
-
-
-metrics/%.rnaseq_metrics : bam/%.bam
-	$(call RUN,-c -s 8G -m 12G,"$(COLLECT_RNASEQ_METRICS) REF_FLAT=$(GENE_REF_FLAT) RIBOSOMAL_INTERVALS=$(RIBOSOMAL_INTERVALS) STRAND_SPECIFICITY=$(STRAND_SPECIFICITY) INPUT=$< REFERENCE_SEQUENCE=$(REF_FASTA) OUTPUT=$@ CHART_OUTPUT=$@.pdf VERBOSITY=ERROR")
-
-metrics/all.rnaseq_metrics : $(foreach sample,$(SAMPLES),metrics/$(sample).rnaseq_metrics)
-	grep '^PF_BASES' $< > $@ && for i in $^; do sample=`echo $$i | sed 's:.*/::; s/\..*//'`; grep -A1 '^PF_BASES' $$i | tail -1 | awk -v sample=$$sample 'BEGIN { OFS = "\t" } { $$23=sample; print $$0 }'  >> $@; done
-
-metrics/all.normalized_coverage.rnaseq_metrics : $(foreach sample,$(SAMPLES),metrics/$(sample).rnaseq_metrics)
-	grep -A101 '^normalized_position' $< | cut -f1 > $@ && for i in $^; do sample=`echo $$i | sed 's:.*/::; s/\..*//'`; grep -A101 '^normalized_position' $$i | cut -f2 | sed "s/All_Reads/$$sample/" | paste $@ - > $@.tmp && mv $@.tmp $@; done
-
-metrics/rnaseq_report/index.html : metrics/all.rnaseq_metrics metrics/all.normalized_coverage.rnaseq_metrics
-	$(PLOT_RNASEQ_METRICS) --outDir $(@D) $^
-
+..DUMMY := $(shell mkdir -p version; \
+			 echo "picard" >> version/rnaseq_metrics.txt; \
+			 $(PICARD) CollectRnaSeqMetrics --version &>> version/rnaseq_metrics.txt)
+.SECONDARY:
 .DELETE_ON_ERROR:
-.SECONDARY: 
-.PHONY: all report
+.PHONY:
