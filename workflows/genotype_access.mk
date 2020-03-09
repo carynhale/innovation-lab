56a1fb3523b09c6cd371e19ca79a0a26ef804570
David Brown <ndbrown6@gmail.com>
2020-03-09 17:00:00 -0400

    Update genotype_access.mk

--- workflows/genotype_access.mk
+++ workflows/genotype_access.mk
@@ -3,26 +3,38 @@ include innovation-lab/genome_inc/b37.inc
 
 LOGDIR ?= log/genotype_access.$(NOW)
 
-genotype_access : $(foreach sample,$(SAMPLES),)
+genotype_access : $(foreach sample,$(SAMPLES),genotype_variants/$(sample).taskcomplete)
 
 REF_FASTA = /ifs/depot/resources/dmp/data/pubdata/hg-fasta/VERSIONS/hg19/Homo_sapiens_assembly19.fasta
 GBCMS_PATH ?= $(HOME)/share/usr/bin/GetBaseCountsMultiSample
-FILTER_DUPLICATE ?= 0
+FILTER_DUPLICATES ?= 0
 FRAGMENT_COUNT ?= 1
 MAPPING_QUALITY ?= 20
 THREADS ?= 10
 VERBOSITY ?= INFO
 
-
 define genotype-access
-waltz/$1-pileup.txt.gz : bam/$1.bam
-	$$(call RUN,-c -n 10 -s 4G -m 6G,"set -o pipefail && \
-									  mkdir -p genotype_variants && \
-									 ")
+genotype_variants/$1.taskcomplete : bam/$1_cl_aln_srt_MD_IR_FX_BR.bam bam/$1_cl_aln_srt_MD_IR_FX_BR__aln_srt_IR_FX.bam bam/$1_cl_aln_srt_MD_IR_FX_BR__aln_srt_IR_FX-simplex.bam bam/$1_cl_aln_srt_MD_IR_FX_BR__aln_srt_IR_FX-duplex.bam
+	$$(call RUN,-c -n 10 -s 2G -m 4G -v $(GENOTYPE_VARIANTS_ENV),"set -o pipefail && \
+									  							  mkdir -p genotype_variants && \
+																  cd genotype_variants && \
+																  $$(GENOTYPE_VARIANTS) small_variants all \
+																  -i XXX \
+																  -r $$(REF_FASTA) \
+																  -p $1 \
+																  -b ../bam/$1_cl_aln_srt_MD_IR_FX_BR.bam \
+																  -d ../bam/$1_cl_aln_srt_MD_IR_FX_BR__aln_srt_IR_FX-duplex.bam \
+																  -s ../bam/$1_cl_aln_srt_MD_IR_FX_BR__aln_srt_IR_FX-simplex.bam \
+																  -g $$(GBCMS_PATH) \
+																  -fd $$(FILTER_DUPLICATES) \
+																  -fc $$(FRAGMENT_COUNT) \
+																  -mapq $$(MAPPING_QUALITY) \
+																  -t $$(THREADS) && \
+																  echo 'task completed!' > $1.taskcomplete")
 									 
 endef
 $(foreach sample,$(SAMPLES),\
-		$(eval $(call waltz-genotype,$(sample))))
+		$(eval $(call genotype-access,$(sample))))
 		
 ..DUMMY := $(shell mkdir -p version; \
 			 $(GBCMS_PATH) --help &> version/genotype_access.txt; \
