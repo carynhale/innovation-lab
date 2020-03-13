db12634f8ad7becf64d24c24a7e7843e5e28969b
David Brown <ndbrown6@gmail.com>
2020-03-12 20:47:33 -0400

    Update cluster_samples.mk

--- qc/cluster_samples.mk
+++ qc/cluster_samples.mk
@@ -2,32 +2,28 @@ include modules/Makefile.inc
 include modules/config/gatk.inc
 
 LOGDIR = log/cluster_samples.$(NOW)
-PHONE += metrics metrics/snps metrics/summary metrics/report
 
 cluster_samples : $(foreach sample,$(SAMPLES),metrics/snps/$(sample).vcf) \
 				  metrics/summary/snps_combined.vcf \
-				  metrics/summary/snps_filtered.vcf \
-				  metrics/summary/snps_filtered.tsv \
-				  metrics/report/snps_clustering.pdf
+				  metrics/summary/snps_filtered.vcf
 
-ifeq ($(EXOME),true)
-DBSNP_SUBSET ?= $(HOME)/share/reference/dbsnp_137_exome.bed
-else
-DBSNP_SUBSET = $(HOME)/share/reference/dbsnp_tseq_intersect.bed
-endif
-CLUSTER_VCF = $(RSCRIPT) $(SCRIPTS_DIR)/qc/cluster_samples.R
+#metrics/summary/snps_filtered.tsv
+#metrics/report/snps_clustering.pdf
+
+DBSNP_SUBSET = $(HOME)/share/lib/bed_files/dbsnp_137.b37_subset.bed
+CLUSTER_VCF = $(RSCRIPT) $(SCRIPTS_DIR)/qc/cluster_access.R
 
 define genotype-snps
 metrics/snps/$1.vcf : bam/$1.bam
 	$$(call RUN,-n 4 -s 2.5G -m 3G,"set -o pipefail && \
-									$(call GATK_MEM,8G) -T UnifiedGenotyper -nt 4 -R $(REF_FASTA) --dbsnp $(DBSNP) \
+									$$(call GATK_CMD,8G) -T UnifiedGenotyper -nt 4 \
 									-I $$(<) \
-									-L $(DBSNP_SUBSET) \
+									-R $$(REF_FASTA) \
 									-o $$(@) \
 									--output_mode EMIT_ALL_SITES \
 									--min_base_quality_score 30 \
 									--standard_min_confidence_threshold_for_calling 20 \
-									-rf BadCigar")
+									-L $$(DBSNP_SUBSET)")
 									
 endef
 $(foreach sample,$(SAMPLES),\
@@ -42,18 +38,21 @@ metrics/summary/snps_combined.vcf : $(foreach sample,$(SAMPLES),metrics/snps/$(s
 metrics/summary/snps_filtered.vcf : metrics/summary/snps_combined.vcf
 	$(INIT) grep '^#' $< > $@ && grep -e '0/1' -e '1/1' $< >> $@
 	
-metrics/summary/snps_filtered.tsv : metrics/summary/snps_filtered.vcf
-	$(INIT) $(CLUSTER_VCF) --switch 1
-	
-metrics/report/snps_clustering.pdf : metrics/summary/snps_filtered.tsv
-	$(call RUN, -c -n 1 -s 12G -m 16G -v $(SUPERHEAT_ENV),"set -o pipefail && \
-														   $(CLUSTER_VCF) --switch 2 && \
-									   					   gs -sDEVICE=pdfwrite -dNOPAUSE -dBATCH -dSAFER -dFirstPage=2 -dLastPage=2 -dAutoRotatePages=/None -sOutputFile=metrics/report/snps_clustering-2.pdf metrics/report/snps_clustering.pdf && \
-									   					   rm metrics/report/snps_clustering.pdf && \
-									   					   mv metrics/report/snps_clustering-2.pdf metrics/report/snps_clustering.pdf")
-		
-include modules/vcf_tools/vcftools.mk
+#metrics/summary/snps_filtered.tsv : metrics/summary/snps_filtered.vcf
+#	$(INIT) $(CLUSTER_VCF) --switch 1
+#	
+#metrics/report/snps_clustering.pdf : metrics/summary/snps_filtered.tsv
+#	$(call RUN, -c -n 1 -s 12G -m 16G -v $(SUPERHEAT_ENV),"set -o pipefail && \
+#														   $(CLUSTER_VCF) --switch 2 && \
+#									   					   gs -sDEVICE=pdfwrite -dNOPAUSE -dBATCH -dSAFER -dFirstPage=2 -dLastPage=2 -dAutoRotatePages=/None -sOutputFile=metrics/report/snps_clustering-2.pdf metrics/report/snps_clustering.pdf && \
+#									   					   rm metrics/report/snps_clustering.pdf && \
+#									   					   mv metrics/report/snps_clustering-2.pdf metrics/report/snps_clustering.pdf")
+#		
 
+..DUMMY := $(shell mkdir -p version; \
+			 echo "gatk3" > version/cluster_samples.txt; \
+			 $(GATK) --version >> version/cluster_samples.txt; \
+			 R --version >> version/cluster_samples.txt)
 .DELETE_ON_ERROR:
 .SECONDARY: 
-.PHONY: $(PHONY)
+.PHONY: cluster_samples
