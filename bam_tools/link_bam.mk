e11c7ab496683274bc2e2dbab929459fc6652b56
David Brown <ndbrown6@gmail.com>
2020-03-12 20:20:03 -0400

    Update link_bam.mk

--- bam_tools/link_bam.mk
+++ bam_tools/link_bam.mk
@@ -6,7 +6,8 @@ link_bam : $(foreach sample,$(SAMPLES),bam/$(sample).bam)
 
 define link-bam
 bam/%.bam : /ifs/dmpshare/share/irb12_245/%.bam
-	$$(call RUN,-c -n 1 -s 1G -m 2G,"ln -s $$(<) bam/$$(*).bam")
+	$$(call RUN,-c -n 1 -s 1G -m 2G,"ln -s /ifs/dmpshare/share/irb12_245/$$(*).bam bam/$$(*).bam && \
+									 ln -s /ifs/dmpshare/share/irb12_245/$$(*).bai bam/$$(*).bai")
 endef
 $(foreach sample,$(SAMPLES),\
 		$(eval $(call link-bam,$(sample))))
