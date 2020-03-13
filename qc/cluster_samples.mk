64c5052fbff534af4f689404a7b673a63d0aa6ff
David Brown <ndbrown6@gmail.com>
2020-03-12 22:08:23 -0400

    Update cluster_samples.mk

--- qc/cluster_samples.mk
+++ qc/cluster_samples.mk
@@ -6,9 +6,8 @@ LOGDIR = log/cluster_samples.$(NOW)
 cluster_samples : $(foreach sample,$(SAMPLES),metrics/snps/$(sample).vcf) \
 				  metrics/summary/snps_combined.vcf \
 				  metrics/summary/snps_filtered.vcf \
-				  metrics/summary/snps_filtered.tsv
-
-#metrics/report/snps_clustering.pdf
+				  metrics/summary/snps_filtered.tsv \
+				  metrics/report/snps_clustering.pdf
 
 DBSNP_SUBSET = $(HOME)/share/lib/bed_files/dbsnp_137.b37_subset.bed
 CLUSTER_VCF = $(RSCRIPT) $(SCRIPTS_DIR)/qc/cluster_samples.R
@@ -42,13 +41,13 @@ metrics/summary/snps_filtered.tsv : metrics/summary/snps_filtered.vcf
 	$(call RUN, -c -n 1 -s 8G -m 12G -v $(VARIANT_ANNOTATION_ENV),"set -o pipefail && \
 														   		   $(CLUSTER_VCF) --switch 1")
 	
-#metrics/report/snps_clustering.pdf : metrics/summary/snps_filtered.tsv
-#	$(call RUN, -c -n 1 -s 12G -m 16G -v $(SUPERHEAT_ENV),"set -o pipefail && \
-#														   $(CLUSTER_VCF) --switch 2 && \
-#									   					   gs -sDEVICE=pdfwrite -dNOPAUSE -dBATCH -dSAFER -dFirstPage=2 -dLastPage=2 -dAutoRotatePages=/None -sOutputFile=metrics/report/snps_clustering-2.pdf metrics/report/snps_clustering.pdf && \
-#									   					   rm metrics/report/snps_clustering.pdf && \
-#									   					   mv metrics/report/snps_clustering-2.pdf metrics/report/snps_clustering.pdf")
-#		
+metrics/report/snps_clustering.pdf : metrics/summary/snps_filtered.tsv
+	$(call RUN, -c -n 1 -s 12G -m 16G -v $(SUPERHEAT_ENV),"set -o pipefail && \
+														   $(CLUSTER_VCF) --switch 2 && \
+									   					   gs -sDEVICE=pdfwrite -dNOPAUSE -dBATCH -dSAFER -dFirstPage=2 -dLastPage=2 -dAutoRotatePages=/None -sOutputFile=metrics/report/snps_clustering-2.pdf metrics/report/snps_clustering.pdf && \
+									   					   rm metrics/report/snps_clustering.pdf && \
+									   					   mv metrics/report/snps_clustering-2.pdf metrics/report/snps_clustering.pdf")
+
 
 ..DUMMY := $(shell mkdir -p version; \
 			 echo "gatk3" > version/cluster_samples.txt; \
