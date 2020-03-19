2acf0074eaafbe06c2c24f328370b99d5c9d2d9b
David Brown <ndbrown6@gmail.com>
2020-03-18 22:10:53 -0400

    Update annotate_vcf_maf.mk

--- vcf_tools/annotate_vcf_maf.mk
+++ vcf_tools/annotate_vcf_maf.mk
@@ -3,25 +3,30 @@ include innovation-lab/genome_inc/b37.inc
 
 LOGDIR ?= log/annotate_vcf_maf.$(NOW)
 
-annotate_vcf_maf : vcf/target_vcf.maf
+annotate_vcf_maf : $(foreach sample,$(SAMPLES),vcf/$(sample).maf)
 
-vcf/target_vcf.maf : vcf/target_vcf.vcf
-	$(call RUN,-c -n 12 -s 2G -m 3G -v $(VCF2MAF_ENV) -w 72:00:00,"set -o pipefail && \
-													   			   $(VCF2MAF) \
-													   				--input-vcf $(<) \
-													   				--output-maf $(@) \
-																	--tmp-dir $(TMPDIR) \
-																	--tumor-id NA \
+define annotate-vcf-maf
+vcf/%.maf : vcf/%.vcf
+	$$(call RUN,-c -n 12 -s 2G -m 3G -v $(VCF2MAF_ENV) -w 72:00:00,"set -o pipefail && \
+													   			    $$(VCF2MAF) \
+													   				--input-vcf $$(<) \
+													   				--output-maf $$(@) \
+																	--tmp-dir $$(TMPDIR) \
+																	--tumor-id $$(*) \
 																	--normal-id NA \
-																	--vep-path $(VCF2MAF_ENV)/bin \
-																	--vep-data $(HOME)/share/lib/resource_files/VEP/GRCh37/ \
+																	--vep-path $$(VCF2MAF_ENV)/bin \
+																	--vep-data $$(HOME)/share/lib/resource_files/VEP/GRCh37/ \
 																	--vep-forks 12 \
-																	--ref-fasta $(HOME)/share/lib/resource_files/VEP/GRCh37/homo_sapiens/99_GRCh37/Homo_sapiens.GRCh37.75.dna.primary_assembly.fa.gz \
-																	--filter-vcf $(HOME)/share/lib/resource_files/VEP/GRCh37/homo_sapiens/99_GRCh37/ExAC_nonTCGA.r0.3.1.sites.vep.vcf.gz \
+																	--ref-fasta $$(HOME)/share/lib/resource_files/VEP/GRCh37/homo_sapiens/99_GRCh37/Homo_sapiens.GRCh37.75.dna.primary_assembly.fa.gz \
+																	--filter-vcf $$(HOME)/share/lib/resource_files/VEP/GRCh37/homo_sapiens/99_GRCh37/ExAC_nonTCGA.r0.3.1.sites.vep.vcf.gz \
 																	--species homo_sapiens \
 																	--ncbi-build GRCh37 \
 																	--maf-center MSKCC && \
-																	rm $(TMPDIR)/target_vcf.vep.vcf")
+																	rm $$(TMPDIR)/$$(*).vep.vcf")
+endef
+ $(foreach sample,$(SAMPLES),\
+		$(eval $(call annotate-vcf-maf,$(sample))))
+
 							  
 	
 ..DUMMY := $(shell mkdir -p version; \
