c6008cbee799dc59c72a773795ef2b0c8d6e412b
David Brown <ndbrown6@gmail.com>
2020-03-18 22:24:56 -0400

    Update annotate_vcf_context.mk

--- vcf_tools/annotate_vcf_context.mk
+++ vcf_tools/annotate_vcf_context.mk
@@ -3,16 +3,21 @@ include innovation-lab/genome_inc/b37.inc
 
 LOGDIR ?= log/annotate_vcf_context.$(NOW)
 
-annotate_vcf_context : vcf/target_vcf.txt
+annotate_vcf_context : $(foreach sample,$(SAMPLES),vcf/$(sample).txt)
 
-vcf/target_vcf.txt : vcf/target_vcf.vcf
-	$(call RUN,-s 24G -m 48G -v $(ANNOTATION_ENV),"set -o pipefail && \
-							                       $(RSCRIPT) $(SCRIPTS_DIR)/vcf_tools/annotate_vcf_context.R \
-                                                   --file_in $(<) \
-                                                   --file_out $(@) \
-                                                   --ensembl_gene $(ENSEMBL)")
-							  
-	
+define annotate-vcf-context
+vcf/%.txt : vcf/%.vcf
+	$$(call RUN,-c -s 4G -m 8G -v $(ANNOTATION_ENV),"set -o pipefail && \
+							                         $$(RSCRIPT) $$(SCRIPTS_DIR)/vcf_tools/annotate_vcf_context.R \
+                                                   	 --file_in $(<) \
+                                                   	 --file_out $(@) \
+                                                   	 --ensembl_gene $(ENSEMBL)")
+endef
+ $(foreach sample,$(SAMPLES),\
+		$(eval $(call annotate-vcf-context,$(sample))))
+
+
+
 ..DUMMY := $(shell mkdir -p version; \
 			 R --version >> version/annotate_vcf_context.txt)
 .DELETE_ON_ERROR:
