6b11e56c94cdfdcf5e1437c1e673194b336a7ccc
David Brown <ndbrown6@gmail.com>
2020-04-19 15:40:49 -0400

    Update genotype_access.mk

--- workflows/genotype_access.mk
+++ workflows/genotype_access.mk
@@ -22,9 +22,9 @@ genotype_variants/$1.taskcomplete : bam/$1_cl_aln_srt_MD_IR_FX_BR.bam bam/$1_cl_
 																  -i ../maf/$1.maf \
 																  -r $$(REF_FASTA) \
 																  -p $1 \
-																  -b ../bam/ \
-																  -d ../bam/ \
-																  -s ../bam/ \
+																  -b ../bam/$1_cl_aln_srt_MD_IR_FX_BR.bam \
+																  -d ../bam/$1_cl_aln_srt_MD_IR_FX_BR__aln_srt_IR_FX-duplex.bam \
+																  -s ../bam/bam/$1_cl_aln_srt_MD_IR_FX_BR__aln_srt_IR_FX-simplex.bam \
 																  -g $$(GBCMS_PATH) \
 																  -fd $$(FILTER_DUPLICATES) \
 																  -fc $$(FRAGMENT_COUNT) \
