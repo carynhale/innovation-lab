e2fad86cf34a76deb4800d64fb9beeeadf23c278
David Brown <ndbrown6@gmail.com>
2020-04-20 09:57:30 -0400

    defuse

--- structural_variants/defuse.mk
+++ structural_variants/defuse.mk
@@ -3,10 +3,12 @@ include innovation-lab/Makefile.inc
 LOGDIR ?= log/defuse.$(NOW)
 
 defuse : $(foreach sample,$(SAMPLES),defuse/$(sample).1.fastq) \
-		 $(foreach sample,$(SAMPLES),defuse/$(sample).2.fastq)
-#		 $(foreach sample,$(SAMPLES),defuse/$(sample).results.filtered.tsv) \
-#		 $(foreach sample,$(SAMPLES),defuse/$(sample).taskcomplete) \
-#		 defuse/summary.tsv
+		 $(foreach sample,$(SAMPLES),defuse/$(sample).2.fastq) \
+		 $(foreach sample,$(SAMPLES),defuse/$(sample).results.filtered.tsv) \
+		 $(foreach sample,$(SAMPLES),defuse/$(sample).taskcomplete)
+		 
+DEFUSE_CONFIG = innovation-lab/config/defuse.inc
+DEFUSE_E75 = 
 
 define merged-fastq
 defuse/$1.1.fastq : $$(foreach split,$2,$$(word 1, $$(fq.$$(split))))
@@ -17,21 +19,27 @@ endef
 $(foreach sample,$(SAMPLES),\
 		$(eval $(call merged-fastq,$(sample),$(split.$(sample)))))
 
-#defuse/%.results.filtered.tsv : defuse/%.1.fastq defuse/%.2.fastq
-#	$$(call RUN,-c -n 10 -s 3G -m 4G -w 540,"$$(PERL) $$(DEFUSE_SCRIPTS)/defuse_run.pl \
-#											 -c $$(CONFIG) -d $$(DEFUSE75) -o defuse/$$(*) \
-#											 --res defuse/$$(*).results.tsv \
-#											 -resfil defuse/$$(*).results.filtered.tsv \
-#											 -1 defuse/$$(*).1.fastq \
-#											 -2 defuse/$$(*).2.fastq -p 10 -s direct")
-#	
-#defuse/%.taskcomplete : defuse/%.results.filtered.tsv
-#	$$(call RUN,-c -s 1G -m 2G,"echo $$(*) > defuse/$$(*).taskcomplete")
-#
-#endef
-#
-#$(foreach sample,$(SAMPLES),\
-#		$(eval $(call defuse-single-sample,$(sample))))
+define run-defuse
+defuse/%.results.filtered.tsv : defuse/%.1.fastq defuse/%.2.fastq
+	$$(call RUN,-c -n 10 -s 2G -m 3G -w 72:00:00,"set -o pipefail && \
+												  mkdir -p defuse && \
+												  $$(DEFUSE) \
+											 	  -c $$(DEFUSE_CONFIG) \
+											 	  -d $$(DEFUSE_E75) \
+											 	  -o defuse/$$(*) \
+											 	  -r defuse/$$(*).results.tsv \
+											 	  -b defuse/$$(*).results.filtered.tsv \
+											 	  -1 defuse/$$(*).1.fastq \
+											 	  -2 defuse/$$(*).2.fastq \
+											 	  -s direct \
+											 	  -p 10")
+	
+defuse/%.taskcomplete : defuse/%.results.filtered.tsv
+	$$(call RUN,-c -s 1G -m 2G,"echo $$(*) > defuse/$$(*).taskcomplete")
+
+endef
+$(foreach sample,$(SAMPLES),\
+		$(eval $(call run-defuse,$(sample))))
 		
 # defuse/summary.tsv : $(wildcard $(foreach sample,$(TUMOR_SAMPLES),defuse/$(sample).taskcomplete))
 #	$(call RUN,-c -n 1 -s 6G -m 8G,"set -o pipefail && \
@@ -42,4 +50,3 @@ $(foreach sample,$(SAMPLES),\
 .SECONDARY:
 .DELETE_ON_ERROR:
 .PHONY: defuse
-
