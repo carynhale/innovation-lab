0044a1e76d380c522f07e31f4232fd996f9c5568
David Brown <ndbrown6@gmail.com>
2020-04-22 17:18:30 -0400

    defuse

--- structural_variants/defuse.mk
+++ structural_variants/defuse.mk
@@ -4,11 +4,9 @@ LOGDIR ?= log/defuse.$(NOW)
 
 defuse : $(foreach sample,$(SAMPLES),defuse/$(sample).1.fastq) \
 		 $(foreach sample,$(SAMPLES),defuse/$(sample).2.fastq) \
-		 $(foreach sample,$(SAMPLES),defuse/$(sample).results.filtered.tsv) \
 		 $(foreach sample,$(SAMPLES),defuse/$(sample).taskcomplete)
 		 
 DEFUSE_CONFIG = innovation-lab/config/defuse.inc
-DEFUSE_E75 = /home/brownd7/share/lib/resource_files/defuse/homo_sapiens/Ensembl/Grch37.p13/Sequence/defuse_e75/
 
 define merged-fastq
 defuse/$1.1.fastq : $$(foreach split,$2,$$(word 1, $$(fq.$$(split))))
@@ -20,24 +18,18 @@ $(foreach sample,$(SAMPLES),\
 		$(eval $(call merged-fastq,$(sample),$(split.$(sample)))))
 
 define run-defuse
-defuse/%.results.filtered.tsv : defuse/%.1.fastq defuse/%.2.fastq
-	$$(call RUN,-c -n 10 -s 2G -m 3G -w 72:00:00 -v $(DEFUSE_ENV),"set -o pipefail && \
-																   mkdir -p defuse && \
-																   $$(DEFUSE) \
-																   --config $$(DEFUSE_CONFIG) \
-																   --dataset $$(DEFUSE_E75) \
-																   --output defuse/$$(*) \
-																   --res defuse/$$(*).results.tsv \
-																   --rescla defuse/$$(*).results.classify.tsv \
-																   --resfil defuse/$$(*).results.filtered.tsv \
-																   -1 defuse/$$(*).1.fastq \
-																   -2 defuse/$$(*).2.fastq \
-																   -s direct \
-																   -p 10")
+defuse/%.taskcomplete : defuse/%.1.fastq defuse/%.2.fastq
+	$$(call RUN,-c -n 10 -s 2G -m 3G -w 72:00:00,"set -o pipefail && \
+												  mkdir -p defuse && \
+												  $$(DEFUSE) \
+												  --config $$(DEFUSE_CONFIG) \
+												  --output defuse/$$(*) \
+												  -1 defuse/$$(*).1.fastq \
+												  -2 defuse/$$(*).2.fastq \
+												  -s direct \
+												  -p 10 && \
+												  echo $$(*) > defuse/$$(*).taskcomplete")
 	
-defuse/%.taskcomplete : defuse/%.results.filtered.tsv
-	$$(call RUN,-c -s 1G -m 2G,"echo $$(*) > defuse/$$(*).taskcomplete")
-
 endef
 $(foreach sample,$(SAMPLES),\
 		$(eval $(call run-defuse,$(sample))))
