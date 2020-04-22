8c841a5d07453c207609f13c58d6b81ade5c5e2a
David Brown <ndbrown6@gmail.com>
2020-04-22 14:45:58 -0400

    Update fusioncatcher.mk

--- structural_variants/fusioncatcher.mk
+++ structural_variants/fusioncatcher.mk
@@ -39,7 +39,9 @@ $(foreach sample,$(SAMPLES),\
 		
 fusioncatcher/summary.txt : $(wildcard $(foreach sample,$(SAMPLES),fusioncatcher/$(sample)/out/taskcomplete))
 	set -o pipefail && \
-	for i in $(SAMPLES); do cat fusioncatcher/\"$$i\"/out/final-list_candidate-fusion-genes.hg19.txt >> fusioncatcher/summary.txt; done
+	for i in $(SAMPLES); do \
+		cat fusioncatcher/$$i/out/final-list_candidate-fusion-genes.hg19.txt >> fusioncatcher/summary.txt; \
+	done
 
 ..DUMMY := $(shell mkdir -p version; \
 			 $(PYTHON) --version &> version/fusioncatcher.txt)
