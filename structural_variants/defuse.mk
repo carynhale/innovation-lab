9852b69b49569cedd5e31231180bd29fec5f8d94
David Brown <ndbrown6@gmail.com>
2020-04-23 15:42:02 -0400

    Downgrade to defuse 0.8.0

--- structural_variants/defuse.mk
+++ structural_variants/defuse.mk
@@ -7,6 +7,7 @@ defuse : $(foreach sample,$(SAMPLES),defuse/$(sample).1.fastq) \
 		 $(foreach sample,$(SAMPLES),defuse/$(sample).taskcomplete)
 		 
 DEFUSE_CONFIG = innovation-lab/config/defuse.inc
+DEFUSE_E75 = /home/brownd7/share/lib/resource_files/defuse/homo_sapiens/Ensembl/Grch37.p13/Sequence/defuse_e75
 
 define merged-fastq
 defuse/$1.1.fastq : $$(foreach split,$2,$$(word 1, $$(fq.$$(split))))
@@ -18,17 +19,24 @@ $(foreach sample,$(SAMPLES),\
 		$(eval $(call merged-fastq,$(sample),$(split.$(sample)))))
 
 define run-defuse
-defuse/%.taskcomplete : defuse/%.1.fastq defuse/%.2.fastq
+defuse/%/results.candidate.tsv : defuse/%.1.fastq defuse/%.2.fastq
 	$$(call RUN,-c -n 10 -s 2G -m 3G -w 72:00:00,"set -o pipefail && \
 												  mkdir -p defuse && \
 												  $$(DEFUSE) \
 												  --config $$(DEFUSE_CONFIG) \
+												  --dataset $$(DEFUSE_E75) \
 												  --output defuse/$$(*) \
+												  --res defuse/$$(*)/results.candidate.tsv \
+												  --rescla defuse/$$(*)/results.classify.tsv \
+												  --resfil defuse/$$(*)/results.filtered.tsv \
 												  -1 defuse/$$(*).1.fastq \
 												  -2 defuse/$$(*).2.fastq \
 												  -s direct \
-												  -p 10 && \
-												  echo $$(*) > defuse/$$(*).taskcomplete")
+												  -p 10")
+												  
+defuse/%/taskcomplete : defuse/%/results.candidate.tsv
+	$$(call RUN,-c -n 1 -s 1G -m 2G,"set -o pipefail && \
+									 echo $$(*) > defuse/$$(*)/taskcomplete")
 	
 endef
 $(foreach sample,$(SAMPLES),\
