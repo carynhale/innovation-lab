b52a76e9962cb1117d7113b46ddfad1df111e16d
David Brown <ndbrown6@gmail.com>
2020-04-24 17:25:27 -0400

    Update starfusion.mk

--- structural_variants/starfusion.mk
+++ structural_variants/starfusion.mk
@@ -2,7 +2,7 @@ include innovation-lab/Makefile.inc
 
 LOGDIR ?= log/star_fusion.$(NOW)
 
-CTAT_LIB ?= $(HOME)/share/lib/ref_files/CTAT_GRCh37/GRCh37_gencode_v19_CTAT_lib_Apr032020/
+CTAT_LIB ?= $(HOME)/share/lib/ref_files/CTAT_GRCh37/GRCh37_gencode_v19_CTAT_lib_Apr032020/ctat_genome_lib_build_dir/
 
 star_fusion : $(foreach sample,$(SAMPLES),starfusion/$(sample)/$(sample).1.fastq) \
 			  $(foreach sample,$(SAMPLES),starfusion/$(sample)/$(sample).2.fastq) \
