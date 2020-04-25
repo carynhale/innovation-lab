8d78c1a68ae80606e99c99f13d31798528cebc19
David Brown <ndbrown6@gmail.com>
2020-04-25 17:20:01 -0400

    Update absolute.mk

--- clonality/absolute.mk
+++ clonality/absolute.mk
@@ -5,7 +5,7 @@ LOGDIR ?= log/absolute.$(NOW)
 absolute : $(foreach set,$(SAMPLE_SETS),absolute/$(set)/$(set).vcf)
 
 define run-absolute
-absolute/%/%.vcf : summary/mutation_summary.txt
+absolute/$1/$1.vcf : summary/mutation_summary.txt
 	$$(call RUN,-c -s 8G -m 12G -v $(ABSOLUTE_ENV),"set -o pipefail && \
 												   	mkdir -p absolute && \
 												   	mkdir -p absolute/$$(*) && \
