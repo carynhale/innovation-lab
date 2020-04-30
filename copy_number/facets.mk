2678d1262756c37f3e0faf64dcf328911b77950c
David Brown <ndbrown6@gmail.com>
2020-04-30 01:24:40 -0400

    FACETS

--- copy_number/facets.mk
+++ copy_number/facets.mk
@@ -1,9 +1,11 @@
 include innovation-lab/Makefile.inc
+include innovation-lab/config/facets.inc
 
 LOGDIR ?= log/facets.$(NOW)
 
 facets : facets/targets/targets.vcf \
-		 $(foreach pair,$(SAMPLE_PAIRS),facets/pileup/$(pair).txt.gz)
+		 $(foreach pair,$(SAMPLE_PAIRS),facets/pileup/$(pair).txt.gz) \
+		 $(foreach pair,$(SAMPLE_PAIRS),facets/cncf/$(pair).RData)
 
 facets/targets/targets.vcf : $(TARGETS_FILE)
 	$(INIT) $(BEDTOOLS) intersect -header -u -a $(DBSNP) -b $< > $@
@@ -22,13 +24,27 @@ facets/pileup/$1_$2.txt.gz : bam/$1.bam bam/$2.bam facets/targets/targets.vcf
 													$$@ \
 													$$(<<) \
 													$$(<)")
+													
 endef
 $(foreach pair,$(SAMPLE_PAIRS),$(eval $(call snp-pileup-tumor-normal,$(tumor.$(pair)),$(normal.$(pair)))))
 
 
+define facets-tumor-normal
+facets/cncf/$1_$2.RData : facets/pileup/$1_$2.txt.gz
+	$$(call RUN,-c -s 8G -m 16G -v $(ABSOLUTE_ENV),"set -o pipefail && \
+													$(RSCRIPT) $(SCRIPTS_DIR)/copy_number/facets.R \
+													--option 1 \
+													--sample_name $1_$2 \
+													$(call FACETS_OPTS,$*)")
+
+endef
+$(foreach pair,$(SAMPLE_PAIRS),$(eval $(call facets-tumor-normal,$(tumor.$(pair)),$(normal.$(pair)))))
+
+
 ..DUMMY := $(shell mkdir -p version; \
 			 $(BEDTOOLS) --version > version/facets.txt; \
-			 ~/share/usr/env/cntu-0.0.1/bin/snp-pileup --help >> version/facets.txt)
+			 ~/share/usr/env/cntu-0.0.1/bin/snp-pileup --help >> version/facets.txt; \
+			 ~/share/usr/env/cntu-0.0.1/bin/R --version >> version/facets.txt)
 .SECONDARY:
 .DELETE_ON_ERROR:
 .PHONY: facets
