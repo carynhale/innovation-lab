1c886ccb7505b4bc9411a2b64aa6e4ea537504b8
David Brown <ndbrown6@gmail.com>
2020-05-11 17:29:49 -0400

    fgbio

--- workflows/fgbio_access.mk
+++ workflows/fgbio_access.mk
@@ -1,17 +1,22 @@
 include innovation-lab/Makefile.inc
 
+LOGDIR ?= log/msk_access.$(NOW)
+
+fgbio_access : $(foreach sample,$(SAMPLES),fgbio/$(sample)/$(sample)_R1.fastq.gz)
+
+define copy-fastq
+fgbio/$1/$1_R1.fastq.gz : $3
+	$$(call RUN,-c -n 1 -s 2G -m 4G,"set -o pipefail && \
+									 mkdir -p fgbio/$1 && \
+								     $(RSCRIPT) $(SCRIPTS_DIR)/fastq_tools/copy_fastq.R --sample_name $1 --directory_name 'fgbio' --fastq_files '$$^'")
+
+endef
+$(foreach ss,$(SPLIT_SAMPLES),\
+	$(if $(fq.$(ss)),$(eval $(call copy-fastq,$(split.$(ss)),$(ss),$(fq.$(ss))))))
 
 
 
-..DUMMY := $(shell mkdir -p version; \
-			 $(BWA) &> version/tmp.txt; \
-			 head -3 version/tmp.txt | tail -2 > version/msk_access.txt; \
-			 rm version/tmp.txt; \
-			 $(SAMTOOLS) --version >> version/msk_access.txt; \
-			 echo "gatk3" >> version/msk_access.txt; \
-			 $(GATK) --version >> version/msk_access.txt; \
-			 echo "picard" >> version/msk_access.txt; \
-			 R --version >> version.txt)
+..DUMMY := $(shell mkdir -p version)
 .DELETE_ON_ERROR:
 .SECONDARY:
-.PHONY: msk_access
+.PHONY: fgbio_access
