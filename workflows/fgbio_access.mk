b1a56941054f5a30296cdef120adcd70536967a7
David Brown <ndbrown6@gmail.com>
2020-05-11 17:43:40 -0400

    fgbio

--- workflows/fgbio_access.mk
+++ workflows/fgbio_access.mk
@@ -1,6 +1,9 @@
 include innovation-lab/Makefile.inc
+include innovation-lab/config/fgbio.inc
+include innovation-lab/config/gatk.inc
+include innovation-lab/genome_inc/b37.inc
 
-LOGDIR ?= log/msk_access.$(NOW)
+LOGDIR ?= log/fgbio_access.$(NOW)
 
 fgbio_access : $(foreach sample,$(SAMPLES),fgbio/$(sample)/$(sample)_R1.fastq.gz)
 
@@ -14,6 +17,30 @@ endef
 $(foreach ss,$(SPLIT_SAMPLES),\
 	$(if $(fq.$(ss)),$(eval $(call copy-fastq,$(split.$(ss)),$(ss),$(fq.$(ss))))))
 
+define fastq-2bam
+fgbio/$1/ : fgbio/$1/$1_R1.fastq.gz
+	$$(call RUN,-c -n 1 -s 8G -m 16G,"set -o pipefail && \
+									  $$(call FGBIO_CMD,2G,8G) \
+									  FastqToBam \
+									  --input fgbio/$1/$1_R1.fastq.gz fgbio/$1/$1_R2.fastq.gz \
+									  --read-structures 3M2S+T 3M2S+T \
+									  --output ${OUTPUT_FILE} \
+									  --sample ${SAMPLE_NAME} \
+									  --library ${SAMPLE_NAME} \
+									  --platform ${sequencing_platform} \
+									  --platform-unit ${sequencing_platform_unit} \
+									  --sequencing-center ${sequencing_center} \
+									  --read-group-id ${READ_GROUP}")
+
+endef
+$(foreach sample,$(SAMPLES),\
+	$(eval $(call clip-umi,$(sample))))
+	
+COMMAND="$COMMAND \"$JAVA -Djava.io.tmpdir=/scratch/ -Xmx${MEM}g -jar
+${FGBIO} FastqToBam
+--input ${R1_FASTQ} ${R2_FASTQ}
+
+\""
 
 
 ..DUMMY := $(shell mkdir -p version)
