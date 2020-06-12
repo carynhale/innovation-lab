60cb0f896b39b5ea1849d7f2032ca2437820f4cf
David Brown <ndbrown6@gmail.com>
2020-06-11 23:01:20 -0400

    Create em_seq.mk

---
+++ workflows/em_seq.mk
@@ -0,0 +1,24 @@
+include innovation-lab/Makefile.inc
+
+LOGDIR ?= log/em_seq.$(NOW)
+
+em_seq : $(foreach sample,$(SAMPLES),emseq/$(sample)/$(sample)_R1.fastq.gz) \
+
+define copy-fastq
+emseq/$1/$1_R1.fastq.gz : $3
+	$$(call RUN,-c -n 1 -s 2G -m 4G,"set -o pipefail && \
+									 mkdir -p emseq/$1 && \
+								     $(RSCRIPT) $(SCRIPTS_DIR)/fastq_tools/copy_fastq2.R \
+								     --sample_name $1 \
+								     --directory_name marianas \
+								     --fastq_files '$$^'")
+
+endef
+$(foreach ss,$(SPLIT_SAMPLES),\
+	$(if $(fq.$(ss)),$(eval $(call copy-fastq,$(split.$(ss)),$(ss),$(fq.$(ss))))))
+ 
+..DUMMY := $(shell mkdir -p version; \
+			 R --version >> version.txt)
+.DELETE_ON_ERROR:
+.SECONDARY:
+.PHONY: em_seq
