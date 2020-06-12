b153e0b900cccb65b73a67c83bdf3646b4f96114
David Brown <ndbrown6@gmail.com>
2020-06-12 15:46:00 -0400

    Update em_seq.mk

--- workflows/em_seq.mk
+++ workflows/em_seq.mk
@@ -2,12 +2,8 @@ include innovation-lab/Makefile.inc
 
 LOGDIR ?= log/em_seq.$(NOW)
 
-em_seq : $(foreach sample,$(SAMPLES),emseq/$(sample)/$(sample)_R1.fastq.gz) \
-		 $(foreach sample,$(SAMPLES),emseq/$(sample)/$(sample).fastq) \
-		 $(foreach sample,$(SAMPLES),emseq/$(sample)/$(sample).txt) \
-		 $(foreach sample,$(SAMPLES),emseq/$(sample)/qual.txt) \
-		 $(foreach sample,$(SAMPLES),emseq/$(sample)/seq.txt)
-
+em_seq : $(foreach sample,$(SAMPLES),emseq/$(sample)/$(sample)_R1.fastq.gz)
+		 
 define copy-fastq
 emseq/$1/$1_R1.fastq.gz : $3
 	$$(call RUN,-c -n 1 -s 2G -m 4G,"set -o pipefail && \
@@ -26,18 +22,6 @@ emseq/$1/$1.fastq : emseq/$1/$1_R1.fastq.gz
 	$$(call RUN,-c -n 1 -s 4G -m 6G,"set -o pipefail && \
 									 zcat $$(<) > $$(@)")
 									 
-emseq/$1/$1.txt : emseq/$1/$1.fastq
-	$$(call RUN,-c -n 1 -s 4G -m 6G,"set -o pipefail && \
-									 $(SCRIPTS_DIR)/fastq_tools/extract_fastq.sh 1 $$(<) $$(@)")
-									 
-emseq/$1/qual.txt : emseq/$1/$1.txt
-	$$(call RUN,-c -n 1 -s 4G -m 6G,"set -o pipefail && \
-									 $(SCRIPTS_DIR)/fastq_tools/extract_fastq.sh 1 $$(<) $$(@)")
-
-emseq/$1/seq.txt : emseq/$1/$1.txt
-	$$(call RUN,-c -n 1 -s 4G -m 6G,"set -o pipefail && \
-									 $(SCRIPTS_DIR)/fastq_tools/extract_fastq.sh 2 $$(<) $$(@)")
-
 endef
 $(foreach sample,$(SAMPLES),\
 		$(eval $(call extract-fastq,$(sample))))
