1a6987927862ecdfe9975b6459be3b8053fed3d1
David Brown <ndbrown6@gmail.com>
2020-06-12 16:19:37 -0400

    Update em_seq.mk

--- workflows/em_seq.mk
+++ workflows/em_seq.mk
@@ -4,13 +4,17 @@ include innovation-lab/genome_inc/b37.inc
 LOGDIR ?= log/em_seq.$(NOW)
 
 em_seq : $(foreach sample,$(SAMPLES),emseq/$(sample)/$(sample)_R1.fastq.gz) \
-		 $(foreach sample,$(SAMPLES),emseq/$(sample)/$(sample).bam)
+		 $(foreach sample,$(SAMPLES),emseq/$(sample)/$(sample)_aln.bam) \
+		 $(foreach sample,$(SAMPLES),emseq/$(sample)/$(sample)_aln_srt.bam)
 
 REF_FASTA = $(REF_DIR)/IDT_oligo/idt_oligo.fasta
 
 BWA_ALN_OPTS ?= -M
 BWAMEM_THREADS = 12
 BWAMEM_MEM_PER_THREAD = 2G
+
+SAMTOOLS_THREADS = 8
+SAMTOOLS_MEM_THREAD = 2G
 		 
 define copy-fastq
 emseq/$1/$1_R1.fastq.gz : $3
@@ -26,10 +30,16 @@ $(foreach ss,$(SPLIT_SAMPLES),\
 	$(if $(fq.$(ss)),$(eval $(call copy-fastq,$(split.$(ss)),$(ss),$(fq.$(ss))))))
 	
 define fastq-to-bam
-emseq/$1/$1.bam : emseq/$1/$1_R1.fastq.gz
+emseq/$1/$1_aln.bam : emseq/$1/$1_R1.fastq.gz
 	$$(call RUN,-c -n $(BWAMEM_THREADS) -s 1G -m $(BWAMEM_MEM_PER_THREAD),"set -o pipefail && \
 																		   $$(BWA) mem -t $$(BWAMEM_THREADS) $$(BWA_ALN_OPTS) \
 																		   -R \"@RG\tID:$1\tLB:$1\tPL:$$(SEQ_PLATFORM)\tSM:$1\" $$(REF_FASTA) emseq/$1/$1_R1.fastq.gz | $$(SAMTOOLS) view -bhS - > $$(@)")
+
+emseq/$1/$1_aln_srt.bam : emseq/$1/$1_aln.bam
+	$$(call RUN,-c -n $(SAMTOOLS_THREADS) -s 1G -m $(SAMTOOLS_MEM_THREAD),"set -o pipefail && \
+									  									   $$(SAMTOOLS) sort -@ $$(SAMTOOLS_THREADS) -m $$(SAMTOOLS_MEM_THREAD) $$(^) -o $$(@) -T $$(TMPDIR) && \
+									  									   $$(SAMTOOLS) index $$(@) && \
+									  									   cp emseq/$1/$1_aln_srt.bam.bai emseq/$1/$1_aln_srt.bai")
 																		   
 endef
 $(foreach sample,$(SAMPLES),\
