3e8cc8717b9640f53969dc224b75d5f113c1d537
David Brown <ndbrown6@gmail.com>
2020-06-18 13:23:49 -0400

    Update em_seq.mk

--- workflows/em_seq.mk
+++ workflows/em_seq.mk
@@ -6,7 +6,9 @@ LOGDIR ?= log/em_seq.$(NOW)
 
 em_seq : $(foreach sample,$(SAMPLES),bismark/$(sample)/$(sample)_R1.fastq.gz) \
 		 $(foreach sample,$(SAMPLES),bismark/$(sample)/$(sample)_aln.bam) \
-		 $(foreach sample,$(SAMPLES),bismark/$(sample)/$(sample)_aln_srt.bam)
+		 $(foreach sample,$(SAMPLES),bismark/$(sample)/$(sample)_aln_srt.bam) \
+		 $(foreach sample,$(SAMPLES),bismark/$(sample)/$(sample)_aln_srt__F1R1R2.bam) \
+		 $(foreach sample,$(SAMPLES),bismark/$(sample)/$(sample)_aln_srt__F2R1R2.bam)
 
 REF_FASTA = $(REF_DIR)/IDT_oligo/idt_oligo.fasta
 GENOME_FOLDER = $(REF_DIR)/IDT_oligo/
@@ -48,6 +50,30 @@ bismark/$1/$1_aln_srt.bam : bismark/$1/$1_aln.bam
 endef
 $(foreach sample,$(SAMPLES),\
 		$(eval $(call fastq-to-bam,$(sample))))
+		
+define filter-bam
+bismark/$1/$1_aln_srt__F1R1R2.bam : bismark/$1/$1_aln_srt.bam
+	$$(call RUN,-c -n $(SAMTOOLS_THREADS) -s 1G -m $(SAMTOOLS_MEM_THREAD),"set -o pipefail && \
+																		   $$(SAMTOOLS) view -@ $$(SAMTOOLS_THREADS) -m $$(SAMTOOLS_MEM_THREAD) -b -f 144 $$(<) > bismak/$1/$1_aln_srt__F1R1.bam && \
+																		   $$(SAMTOOLS) index bismark/$1/$1_aln_srt__F1R1.bam && \
+																		   $$(SAMTOOLS) view -@ $$(SAMTOOLS_THREADS) -m $$(SAMTOOLS_MEM_THREAD) -b -f 64 -F 16 $$(<) > bismark/$1/$1_aln_srt__F1R2.bam && \
+																		   $$(SAMTOOLS) index bismark/$1/$1_aln_srt__F1R2.bam && \
+																		   $$(SAMTOOLS) merge -@ $$(SAMTOOLS_THREADS) -m $$(SAMTOOLS_MEM_THREAD) -f $$(@) bismark/$1/$1_aln_srt__F1R1.bam bismark/$1/$1_aln_srt__F1R2.bam && \
+																		   $$(SAMTOOLS) index $$(@)")
+
+bismark/$1/$1_aln_srt__F2R1R2.bam : bismark/$1/$1_aln_srt.bam
+	$$(call RUN,-c -n $(SAMTOOLS_THREADS) -s 1G -m $(SAMTOOLS_MEM_THREAD),"set -o pipefail && \
+																		   $$(SAMTOOLS) view -@ $$(SAMTOOLS_THREADS) -m $$(SAMTOOLS_MEM_THREAD) -b -f 128 -F 16 $$(<) > bismark/$1/$1_aln_srt__F2R1.bam && \
+																		   $$(SAMTOOLS) index bismark/$1/$1_aln_srt__F2R1.bam && \
+																		   $$(SAMTOOLS) view -@ $$(SAMTOOLS_THREADS) -m $$(SAMTOOLS_MEM_THREAD) -b -f 80 $$(<) > bismark/$1/$1_aln_srt__F2R2.bam && \
+																		   $$(SAMTOOLS) index bismark/$1/$1_aln_srt__F2R2.bam && \
+																		   $$(SAMTOOLS) merge -@ $$(SAMTOOLS_THREADS) -m $$(SAMTOOLS_MEM_THREAD) -f $$(@) bismark/$1/$1_aln_srt__F2R1.bam bismark/$1/$1_aln_srt__F2R2.bam && \
+																		   $$(SAMTOOLS) index $$(@)")
+
+endef
+$(foreach sample,$(SAMPLES),\
+		$(eval $(call filter-bam,$(sample))))
+
 		
 ..DUMMY := $(shell mkdir -p version; \
 			 $(HOME)/share/usr/env/bismark-0.22.1/bin/bismark --version > version/em_seq.txt; \
