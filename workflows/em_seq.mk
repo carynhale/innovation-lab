04cea6b1602a74fc58f2f0db86fc6160c573bc43
David Brown <ndbrown6@gmail.com>
2020-07-20 15:04:29 -0400

    Update em_seq.mk

--- workflows/em_seq.mk
+++ workflows/em_seq.mk
@@ -69,7 +69,6 @@ bismark/$1/$1_aln_srt.bam : bismark/$1/$1_aln.bam
 
 bismark/$1/$1_aln_srt_fx.bam : bismark/$1/$1_aln_srt.bam
     $$(call RUN,-c -n 1 -s 12G -m 16G,"set -o pipefail && \
-#									   $$(FIX_MATE) \
 									   INPUT=$$(<) \
 									   OUTPUT=$$(@) \
 									   SORT_ORDER=coordinate \
