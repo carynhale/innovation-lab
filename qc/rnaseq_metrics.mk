0c8387b746878cbc6b46047d9de121d042c80e89
David Brown <ndbrown6@gmail.com>
2020-09-16 14:16:57 -0400

    Update rnaseq_metrics.mk

--- qc/rnaseq_metrics.mk
+++ qc/rnaseq_metrics.mk
@@ -6,11 +6,13 @@ REF_FLAT ?= $(HOME)/share/lib/resource_files/refFlat_ensembl.v75.txt
 RIBOSOMAL_INTERVALS ?= $(HOME)/share/lib/resource_files/Homo_sapiens.GRCh37.75.rRNA.interval_list
 STRAND_SPECIFICITY ?= NONE
 
-rnaseq_metrics : $(foreach sample,$(SAMPLES),metrics/$(sample).txt) \
-                 metrics/collect_rnaseq_metrics.txt
+rnaseq_metrics : $(foreach sample,$(SAMPLES),metrics/$(sample)_rnaseq_metrics.txt) \
+				 $(foreach sample,$(SAMPLES),metrics/$(sample)_alignment_metrics.txt) \
+                 summary/rnaseq_metrics.txt \
+				 summary/alignment_metrics.txt
 
 define rnaseq-metrics
-metrics/$1.txt : bam/$1.bam
+metrics/$1_rnaseq_metrics.txt : bam/$1.bam
 	$$(call RUN,-c -s 6G -m 12G,"set -o pipefail && \
                                  $$(COLLECT_RNASEQ_METRICS) \
                                  INPUT=$$(<) \
@@ -24,9 +26,13 @@ endef
 $(foreach sample,$(SAMPLES),\
 		$(eval $(call rnaseq-metrics,$(sample))))
         
-metrics/collect_rnaseq_metrics.txt : $(foreach sample,$(SAMPLES),metrics/$(sample).txt)
+summary/rnaseq_metrics.txt : $(foreach sample,$(SAMPLES),metrics/$(sample)_rnaseq_metrics.txt)
 	$(call RUN, -c -n 1 -s 4G -m 6G,"set -o pipefail && \
-                                     $(RSCRIPT) $(SCRIPTS_DIR)/qc/rnaseq_metrics.R --sample_names '$(SAMPLES)'")
+                                     $(RSCRIPT) $(SCRIPTS_DIR)/qc/rnaseq_metrics.R --option 1 --sample_names '$(SAMPLES)'")
+
+summary/alignment_metrics.txt : $(foreach sample,$(SAMPLES),metrics/$(sample)_alignment_metrics.txt)
+	$(call RUN, -c -n 1 -s 4G -m 6G,"set -o pipefail && \
+                                     $(RSCRIPT) $(SCRIPTS_DIR)/qc/rnaseq_metrics.R --option 2 --sample_names '$(SAMPLES)'")
 
 ..DUMMY := $(shell mkdir -p version; \
 			 echo "picard" >> version/rnaseq_metrics.txt; \
