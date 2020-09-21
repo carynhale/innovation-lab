da2d342d89c83da19ce26bf11827e5c8723d45ed
David Brown <ndbrown6@gmail.com>
2020-09-21 08:43:36 -0400

    Update umi_tools.mk

--- rna_seq/umi_tools.mk
+++ rna_seq/umi_tools.mk
@@ -31,7 +31,6 @@ STAR_OPTS = --genomeDir $(STAR_REF) \
 
 umi_tools : $(foreach sample,$(SAMPLES),umi_tools/$(sample)/$(sample)_R1.fastq.gz) \
 			$(foreach sample,$(SAMPLES),umi_tools/$(sample)/$(sample)_R1_cl.fastq.gz) \
-			$(foreach sample,$(SAMPLES),umi_tools/$(sample)/$(sample)_R2_cl.fastq.gz) \
 			$(foreach sample,$(SAMPLES),star/$(sample).Aligned.sortedByCoord.out.bam) \
 			$(foreach sample,$(SAMPLES),star/$(sample).Aligned.sortedByCoord.out.bam.bai) \
 			$(foreach sample,$(SAMPLES),bam/$(sample).bam) \
@@ -54,31 +53,26 @@ define clip-fastq
 umi_tools/$1/$1_R1_cl.fastq.gz : umi_tools/$1/$1_R1.fastq.gz
 	$$(call RUN,-c -n 1 -s 8G -m 16G -v $(UMITOOLS_ENV),"set -o pipefail && \
 														 umi_tools extract \
-														 --bc-pattern=$$(UMI_PATTERN) \
+														 -p $$(UMI_PATTERN) \
 														 -I umi_tools/$1/$1_R1.fastq.gz \
-														 --stdout $$(@) \
-														 --log=umi_tools/$1/$1_R1_cl.log")
-
-umi_tools/$1/$1_R2_cl.fastq.gz : umi_tools/$1/$1_R1.fastq.gz
-	$$(call RUN,-c -n 1 -s 8G -m 16G -v $(UMITOOLS_ENV),"set -o pipefail && \
-														 umi_tools extract \
-														 --bc-pattern=$$(UMI_PATTERN) \
-														 -I umi_tools/$1/$1_R2.fastq.gz \
-														 --stdout $$(@) \
-														 --log=umi_tools/$1/$1_R2_cl.log")
+														 -S umi_tools/$1/$1_R1_cl.fastq.gz \
+														 --bc-pattern2=$$(UMI_PATTERN) \
+														 --read2-in=umi_tools/$1/$1_R2.fastq.gz \
+														 --read2-out=umi_tools/$1/$1_R2.fastq.gz \
+														 --log=umi_tools/$1/$1_cl.log")
 
 endef
 $(foreach sample,$(SAMPLES),\
 	$(eval $(call clip-fastq,$(sample))))
 
 define align-fastq
-star/$1.Aligned.sortedByCoord.out.bam : umi_tools/$1/$1_R1_cl.fastq.gz umi_tools/$1/$1_R2_cl.fastq.gz
+star/$1.Aligned.sortedByCoord.out.bam : umi_tools/$1/$1_R1_cl.fastq.gz
 	$$(call RUN,-n 4 -s 6G -m 10G -w 1440,"set -o pipefail && \
 										   STAR $$(STAR_OPTS) \
 										   --outFileNamePrefix star/$1. \
 										   --runThreadN 4 \
 										   --outSAMattrRGline \"ID:$1\" \"LB:$1\" \"SM:$1\" \"PL:$${SEQ_PLATFORM}\" \
-										   --readFilesIn $$(<) $$(<<) \
+										   --readFilesIn umi_tools/$1/$1_R1_cl.fastq.gz umi_tools/$1/$1_R2_cl.fastq.gz \
 										   --readFilesCommand zcat")
                                    
 star/$1.Aligned.sortedByCoord.out.bam.bai : star/$1.Aligned.sortedByCoord.out.bam
