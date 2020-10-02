c249a69f45970537c28385b8c45bb028ab3cefde
David Brown <ndbrown6@gmail.com>
2020-10-02 18:08:45 -0400

    Update defuse_umi.mk

--- workflows/defuse_umi.mk
+++ workflows/defuse_umi.mk
@@ -10,8 +10,9 @@ defuse_umi : $(foreach sample,$(SAMPLES),defuse/$(sample).dedup.1.fastq) \
 DEFUSE_CONFIG = innovation-lab/config/defuse.inc
 DEFUSE_E75 = /home/brownd7/share/lib/resource_files/defuse/homo_sapiens/Ensembl/Grch37.p13/Sequence/defuse_e75
 
-defuse/%.dedup.1.fastq defuse/%.dedup.2.fastq : %.bam
-	$(call RUN,-n 4 -s 4G -m 9G,"$(SAMTOOLS) sort -T $(<D)/$* -O bam -n -@ 4 -m 6G $< | $(SAMTOOLS) fastq -f 1 -1 defuse/$*.dedup.1.fastq -2 defuse/$*.dedup.2.fastq")
+defuse/%.dedup.1.fastq : %.bam
+	$$(call RUN,-n 4 -s 4G -m 9G,"set -o pipefail && \
+								  $(SAMTOOLS) sort -T $(<D)/$* -O bam -n -@ 4 -m 6G $< | $(SAMTOOLS) fastq -f 1 -1 defuse/$*.dedup.1.fastq -2 defuse/$*.dedup.2.fastq")
 
 
 define run-defuse
