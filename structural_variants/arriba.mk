d01cd47b3290e24fb8f31c13fd02ab493d31bc4e
David Brown <ndbrown6@gmail.com>
2020-11-27 18:23:54 -0500

    Update arriba.mk

--- structural_variants/arriba.mk
+++ structural_variants/arriba.mk
@@ -4,11 +4,30 @@ LOGDIR ?= log/arriba.$(NOW)
 
 arriba : $(foreach sample,$(SAMPLES),arriba/$(sample)/$(sample).fusion.tsv) \
 		 arriba/summary.txt
+		 
+DBSNP_SUBSET = $(HOME)/share/lib/bed_files/dbsnp_137.b37_subset.bed
+
+STAR_INDEX_DIR = $(STAR_REF)
+ASSEMBLY_FA = $(REF_FASTA)
+ANNOTATION_GTF = $(HOME)/share/lib/resource_files/GENCODE19.gtf
+BLACKLIST_TSV = $(HOME)/share/usr/arriba_v2.0.0/database/blacklist_hg19_hs37d5_GRCh37_v2.0.0.tsv
+KNOWN_FUSIONS_TSV = $(HOME)/share/usr/arriba_v2.0.0/database/known_fusions_hg19_hs37d5_GRCh37_v2.0.0.tsv
+PROTEIN_DOMAINS_GFF3 = $(HOME)/share/usr/arriba_v2.0.0/database/protein_domains_hg19_hs37d5_GRCh37_v2.0.0.gff3
+ARRIBA = $(HOME)/share/usr/arriba_v2.0.0/arriba
 
 define run-arriba
 arriba/$1/$1.fusion.tsv : bam/$1.bam
-	$$(call RUN,-c -n 8 -s 1G -m 2G -v $(STARFUSION_ENV) -w 36:00:00,"set -o pipefail && \
-																	  ")
+	$$(call RUN,-c -n 1 -s 8G -m 12G -v $(STARFUSION_ENV) -w 36:00:00,"set -o pipefail && \
+																	  tee bam/$1.bam | \
+																	  /home/brownd7/share/usr/arriba_v2.0.0/arriba \
+																	  -x /dev/stdin \
+																	  -o arriba/$1/$1.fusion.tsv -O arriba/$1/$1.discarded.tsv \
+																	  -a $$(ASSEMBLY_FA) \
+																	  -g $$(ANNOTATION_GTF) \
+																	  -b $$(BLACKLIST_TSV) \
+																	  -k $$(KNOWN_FUSIONS_TSV) \
+																	  -t $$(KNOWN_FUSIONS_TSV) \
+																	  -p $$(PROTEIN_DOMAINS_GFF3)")
 
 endef
 $(foreach sample,$(SAMPLES),\
@@ -21,7 +40,7 @@ arriba/summary.txt : $(foreach sample,$(SAMPLES),arriba/$(sample)/$(sample).fusi
 	done
 
 ..DUMMY := $(shell mkdir -p version; \
-			 share/usr/arriba_v2.0.0/arriba -h &> version/arriba.txt)
+			 $(ARRIBA) -h > version/arriba.txt)
 .SECONDARY:
 .DELETE_ON_ERROR:
 .PHONY: arriba
