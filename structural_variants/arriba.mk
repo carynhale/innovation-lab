d55ac1f7d090afbcb8ff1f1ec3ff8d998142d06c
David Brown <ndbrown6@gmail.com>
2020-11-28 20:52:21 -0500

    Update arriba.mk

--- structural_variants/arriba.mk
+++ structural_variants/arriba.mk
@@ -2,41 +2,61 @@ include innovation-lab/Makefile.inc
 
 LOGDIR ?= log/arriba.$(NOW)
 
-arriba : $(foreach sample,$(SAMPLES),arriba/$(sample)/$(sample).fusion.tsv) \
-		 arriba/summary.txt
-		 
-DBSNP_SUBSET = $(HOME)/share/lib/bed_files/dbsnp_137.b37_subset.bed
+arriba : $(foreach sample,$(SAMPLES),arriba/$(sample)-nodedup/$(sample)-nodedup.tsv)
 
-STAR_INDEX_DIR = $(STAR_REF)
-ASSEMBLY_FA = $(REF_FASTA)
-ANNOTATION_GTF = $(HOME)/share/lib/resource_files/GENCODE19.gtf
-BLACKLIST_TSV = $(HOME)/share/usr/arriba_v2.0.0/database/blacklist_hg19_hs37d5_GRCh37_v2.0.0.tsv
-KNOWN_FUSIONS_TSV = $(HOME)/share/usr/arriba_v2.0.0/database/known_fusions_hg19_hs37d5_GRCh37_v2.0.0.tsv
-PROTEIN_DOMAINS_GFF3 = $(HOME)/share/usr/arriba_v2.0.0/database/protein_domains_hg19_hs37d5_GRCh37_v2.0.0.gff3
-ARRIBA = $(HOME)/share/usr/arriba_v2.0.0/arriba
+ARRIBA_ENV = $(HOME)/share/usr/env/arriba-2.0.0
+ARRIBA_EXE = $(HOME)/share/usr/env/arriba-2.0.0/src/arriba_v2.0.0/arriba
+STAR_INDEX_DIR = $(HOME)/share/usr/env/arriba-2.0.0/src/arriba_v2.0.0/STAR_index_GRCh37viral_GENCODE19
+ANNOTATION_GTF = $(HOME)/share/usr/env/arriba-2.0.0/src/arriba_v2.0.0/GENCODE19.gtf
+ASSEMBLY_FA = $(HOME)/share/usr/env/arriba-2.0.0/src/arriba_v2.0.0/GRCh37viral.fa
+BLACKLIST_TSV = $(HOME)/share/usr/env/arriba-2.0.0/src/arriba_v2.0.0/database/blacklist_hg19_hs37d5_GRCh37_v2.0.0.tsv.gz
+KNOWN_FUSIONS_TSV = $(HOME)/share/usr/env/arriba-2.0.0/src/arriba_v2.0.0/database/known_fusions_hg19_hs37d5_GRCh37_v2.0.0.tsv.gz
+PROTEIN_DOMAINS_GFF3 = $(HOME)/share/usr/env/arriba-2.0.0/src/arriba_v2.0.0/database/protein_domains_hg19_hs37d5_GRCh37_v2.0.0.gff3
+THREADS = 16
 
-define run-arriba
-arriba/$1/$1.fusion.tsv : bam/$1.bam
-	$$(call RUN,-c -n 1 -s 8G -m 12G -v $(STARFUSION_ENV) -w 36:00:00,"set -o pipefail && \
-																	  /home/brownd7/share/usr/arriba_v2.0.0/arriba \
-																	  -x bam/$1.bam \
-																	  -o arriba/$1/$1.fusion.tsv -O arriba/$1/$1.discarded.tsv \
-																	  -a $$(ASSEMBLY_FA) \
-																	  -g $$(ANNOTATION_GTF) \
-																	  -b $$(BLACKLIST_TSV) \
-																	  -k $$(KNOWN_FUSIONS_TSV) \
-																	  -t $$(KNOWN_FUSIONS_TSV) \
-																	  -p $$(PROTEIN_DOMAINS_GFF3)")
+define arriba-nodedup
+arriba/$1-nodedup/$1.Aligned.out.bam : umi_tools/$1/$1_R1_cl.fastq.gz
+	$$(call RUN,-c -n $(THREADS) -s 1G -m 2G -v $(ARRIBA_ENV) -w 72:00:00,"set -o pipefail && \
+																		   STAR \
+																		   --runThreadN $$(THREADS) \
+																		   --genomeDir $$(STAR_INDEX_DIR) \
+																		   --genomeLoad NoSharedMemory \
+																		   --readFilesIn umi_tools/$1/$1_R1_cl.fastq.gz umi_tools/$1/$1_R2_cl.fastq.gz \
+																		   --readFilesCommand zcat \
+																		   --outStd BAM_Unsorted \
+																		   --outSAMtype BAM Unsorted \
+																		   --outSAMunmapped Within \
+																		   --outBAMcompression 0 \
+																		   --outFilterMultimapNmax 50 \
+																		   --peOverlapNbasesMin 10 \
+																		   --alignSplicedMateMapLminOverLmate 0.5 \
+																		   --alignSJstitchMismatchNmax 5 -1 5 5 \
+																		   --chimSegmentMin 10 \
+																		   --chimOutType WithinBAM HardClip \
+																		   --chimJunctionOverhangMin 10 \
+																		   --chimScoreDropMax 30 \
+																		   --chimScoreJunctionNonGTAG 0 \
+																		   --chimScoreSeparation 1 \
+																		   --chimSegmentReadGapMax 3 \
+																		   --chimMultimapNmax 50 \
+																		   --outFileNamePrefix arriba/$1-nodedup/$1. > arriba/$1-nodedup/$1.Aligned.out.bam")
+
+
+arriba/$1-nodedup/$1-nodedup.tsv : arriba/$1-nodedup/$1.Aligned.out.bam
+	$$(call RUN,-c -n 1 -s 4G -m 8G -v $(ARRIBA_ENV),"set -o pipefail && \
+													  $$(ARRIBA_EXE) -x arriba/$1-nodedup/$1.Aligned.out.bam \
+													  -o arriba/$1-nodedup/$1-nodedup.tsv -O arriba/$1-nodedup/$1-nodedup.discarded.tsv \
+													  -a $$(ASSEMBLY_FA) \
+													  -g $$(ANNOTATION_GTF) \
+													  -b $$(BLACKLIST_TSV) \
+													  -k $$(KNOWN_FUSIONS_TSV) \
+													  -t $$(KNOWN_FUSIONS_TSV) \
+													  -p $$(PROTEIN_DOMAINS_GFF3)")
 
 endef
 $(foreach sample,$(SAMPLES),\
-		$(eval $(call run-arriba,$(sample))))
-		
-arriba/summary.txt : $(foreach sample,$(SAMPLES),arriba/$(sample)/$(sample).fusion.tsv)
-	echo "" > arriba/summary.txt; \
-	for i in $(SAMPLES); do \
-		sed -e "1d" arriba/$$i/$$i.fusion.tsv | sed "s/$$/\t$$i/" >> arriba/summary.txt; \
-	done
+		$(eval $(call arriba-nodedup,$(sample))))
+
 
 ..DUMMY := $(shell mkdir -p version; \
 			 $(ARRIBA) -h > version/arriba.txt)
