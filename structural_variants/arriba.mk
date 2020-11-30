a3035c48a731919553a6b9b546c4f54bf49cc1e2
David Brown <ndbrown6@gmail.com>
2020-11-30 09:53:57 -0500

    Update arriba.mk

--- structural_variants/arriba.mk
+++ structural_variants/arriba.mk
@@ -2,8 +2,7 @@ include innovation-lab/Makefile.inc
 
 LOGDIR ?= log/arriba.$(NOW)
 
-arriba : $(foreach sample,$(SAMPLES),arriba/$(sample)-nodedup/$(sample)-nodedup.tsv) \
-	 	 $(foreach sample,$(SAMPLES),arriba/$(sample)-dedup/$(sample)-dedup.tsv)
+arriba : $(foreach sample,$(SAMPLES),arriba/$(sample)/fusion.tsv)
 
 ARRIBA_ENV = $(HOME)/share/usr/env/arriba-2.0.0
 ARRIBA_EXE = $(HOME)/share/usr/env/arriba-2.0.0/src/arriba_v2.0.0/arriba
@@ -15,7 +14,7 @@ KNOWN_FUSIONS_TSV = $(HOME)/share/usr/env/arriba-2.0.0/src/arriba_v2.0.0/databas
 PROTEIN_DOMAINS_GFF3 = $(HOME)/share/usr/env/arriba-2.0.0/src/arriba_v2.0.0/database/protein_domains_hg19_hs37d5_GRCh37_v2.0.0.gff3
 THREADS = 16
 
-define arriba-nodedup
+define run-arriba
 arriba/$1-nodedup/$1.Aligned.out.bam : umi_tools/$1/$1_R1_cl.fastq.gz
 	$$(call RUN,-c -n $(THREADS) -s 1G -m 2G -v $(ARRIBA_ENV) -w 72:00:00,"set -o pipefail && \
 										STAR \
@@ -56,57 +55,7 @@ arriba/$1-nodedup/$1-nodedup.tsv : arriba/$1-nodedup/$1.Aligned.out.bam
 
 endef
 $(foreach sample,$(SAMPLES),\
-		$(eval $(call arriba-nodedup,$(sample))))
-
-define arriba-dedup
-arriba/$1-dedup/$1.1.fastq.gz : bam/$1.bam
-	$$(call RUN,-n 4 -s 4G -m 9G,"set -o pipefail && \
-					$$(SAMTOOLS) sort -T bam/$1 -O bam -n -@ 4 -m 6G $$(<) | \
-					bedtools bamtofastq -i - -fq >(gzip -c > arriba/$1-dedup/$1.1.fastq.gz) -fq2 >(gzip -c > arriba/$1-dedup/$1.2.fastq.gz)")
-
-arriba/$1-dedup/$1.Aligned.out.bam : arriba/$1-dedup/$1.1.fastq.gz
-	$$(call RUN,-c -n $(THREADS) -s 1G -m 2G -v $(ARRIBA_ENV) -w 72:00:00,"set -o pipefail && \
-										STAR \
-										--runThreadN $$(THREADS) \
-										--genomeDir $$(STAR_INDEX_DIR) \
-										--genomeLoad NoSharedMemory \
-										--readFilesIn arriba/$1-dedup/$1.1.fastq.gz arriba/$1-dedup/$1.2.fastq.gz \
-										--readFilesCommand zcat \
-										--outStd BAM_Unsorted \
-										--outSAMtype BAM Unsorted \
-										--outSAMunmapped Within \
-										--outBAMcompression 0 \
-										--outFilterMultimapNmax 50 \
-										--peOverlapNbasesMin 10 \
-										--alignSplicedMateMapLminOverLmate 0.5 \
-										--alignSJstitchMismatchNmax 5 -1 5 5 \
-										--chimSegmentMin 10 \
-										--chimOutType WithinBAM HardClip \
-										--chimJunctionOverhangMin 10 \
-										--chimScoreDropMax 30 \
-										--chimScoreJunctionNonGTAG 0 \
-										--chimScoreSeparation 1 \
-										--chimSegmentReadGapMax 3 \
-										--chimMultimapNmax 50 \
-										--outFileNamePrefix arriba/$1-dedup/$1. > arriba/$1-dedup/$1.Aligned.out.bam")
-
-
-arriba/$1-dedup/$1-dedup.tsv : arriba/$1-dedup/$1.Aligned.out.bam
-	$$(call RUN,-c -n 1 -s 24G -m 36G -v $(ARRIBA_ENV),"set -o pipefail && \
-								$$(ARRIBA_EXE) -x arriba/$1-dedup/$1.Aligned.out.bam \
-								-o arriba/$1-dedup/$1-dedup.tsv -O arriba/$1-dedup/$1-dedup.discarded.tsv \
-								-a $$(ASSEMBLY_FA) \
-								-g $$(ANNOTATION_GTF) \
-								-b $$(BLACKLIST_TSV) \
-								-k $$(KNOWN_FUSIONS_TSV) \
-								-t $$(KNOWN_FUSIONS_TSV) \
-								-p $$(PROTEIN_DOMAINS_GFF3)")
-
-endef
-$(foreach sample,$(SAMPLES),\
-                $(eval $(call arriba-dedup,$(sample))))
-
-
+		$(eval $(call run-arriba,$(sample))))
 
 ..DUMMY := $(shell mkdir -p version; \
 			 $(ARRIBA_EXE) -h > version/arriba.txt)
