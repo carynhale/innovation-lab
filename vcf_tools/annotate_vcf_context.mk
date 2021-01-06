d5b669174f7e867aec2fac0cd17a81cca99b31f4
David Brown <ndbrown6@gmail.com>
2021-01-06 10:02:48 -0500

    Update annotate_vcf_context.mk

--- vcf_tools/annotate_vcf_context.mk
+++ vcf_tools/annotate_vcf_context.mk
@@ -8,18 +8,17 @@ annotate_vcf_context : $(foreach sample,$(SAMPLES),vcf/$(sample).txt)
 define annotate-vcf-context
 vcf/%.txt : vcf/%.vcf
 	$$(call RUN,-c -s 4G -m 8G -v $(ANNOTATION_ENV),"set -o pipefail && \
-							                         $$(RSCRIPT) $$(SCRIPTS_DIR)/vcf_tools/annotate_vcf_context.R \
+							 $$(RSCRIPT) $$(SCRIPTS_DIR)/vcf_tools/annotate_vcf_context.R \
                                                    	 --file_in $$(<) \
                                                    	 --file_out $$(@) \
                                                    	 --ensembl_gene $$(ENSEMBL)")
+
 endef
  $(foreach sample,$(SAMPLES),\
 		$(eval $(call annotate-vcf-context,$(sample))))
 
-
-
 ..DUMMY := $(shell mkdir -p version; \
-			 R --version >> version/annotate_vcf_context.txt)
+	     R --version >> version/annotate_vcf_context.txt)
 .DELETE_ON_ERROR:
 .SECONDARY: 
 .PHONY: annotate_vcf_context
