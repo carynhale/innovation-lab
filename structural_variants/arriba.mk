443fa6b415221ac1a429cfde9259ea653cfb0633
David Brown <ndbrown6@gmail.com>
2021-01-06 10:05:33 -0500

    Update arriba.mk

--- structural_variants/arriba.mk
+++ structural_variants/arriba.mk
@@ -4,21 +4,23 @@ include innovation-lab/config/arriba.inc
 LOGDIR ?= log/arriba.$(NOW)
 
 arriba : $(foreach sample,$(SAMPLES),arriba/$(sample)/$(sample).1.fastq.gz) \
-		 $(foreach sample,$(SAMPLES),arriba/$(sample)/$(sample).2.fastq.gz) \
-		 $(foreach sample,$(SAMPLES),arriba/$(sample)/$(sample).Aligned.out.bam) \
-		 $(foreach sample,$(SAMPLES),arriba/$(sample)/fusions.tsv) \
-		 arriba/summary.txt
+	 $(foreach sample,$(SAMPLES),arriba/$(sample)/$(sample).2.fastq.gz) \
+	 $(foreach sample,$(SAMPLES),arriba/$(sample)/$(sample).Aligned.out.bam) \
+	 $(foreach sample,$(SAMPLES),arriba/$(sample)/fusions.tsv) \
+	 arriba/summary.txt
 
 
 define merged-fastq
 arriba/$1/$1.1.fastq.gz : $$(foreach split,$2,$$(word 1, $$(fq.$$(split))))
 	$$(call RUN,-c -n 1 -s 2G -m 4G,"set -o pipefail && \
-									 mkdir -p arriba/$1 && \
-									 cp $$(^) $$(@)")
+					 mkdir -p arriba/$1 && \
+					 cp $$(^) $$(@)")
+					 
 arriba/$1/$1.2.fastq.gz : $$(foreach split,$2,$$(word 2, $$(fq.$$(split))))
 	$$(call RUN,-c -n 1 -s 2G -m 4G,"set -o pipefail && \
-									 mkdir -p arriba/$1 && \
-									 cp $$(^) $$(@)")
+					 mkdir -p arriba/$1 && \
+					 cp $$(^) $$(@)")
+
 endef
 $(foreach sample,$(SAMPLES),\
 		$(eval $(call merged-fastq,$(sample),$(split.$(sample)))))
@@ -26,29 +28,29 @@ $(foreach sample,$(SAMPLES),\
 define star-align
 arriba/$1/$1.Aligned.out.bam : arriba/$1/$1.1.fastq.gz arriba/$1/$1.2.fastq.gz
 	$$(call RUN,-c -n $(STAR_THREADS) -s 1G -m 2G -v $(ARRIBA_ENV),"set -o pipefail && \
-																	STAR \
-																	--runThreadN $$(STAR_THREADS) \
-																	--genomeDir $$(STAR_INDEX_DIR) \
-																	--genomeLoad NoSharedMemory \
-																	--readFilesIn $$(<) $$(<<) \
-																	--readFilesCommand zcat \
-																	--outStd BAM_Unsorted \
-																	--outSAMtype BAM Unsorted \
-																	--outSAMunmapped Within \
-																	--outBAMcompression 0 \
-																	--outFilterMultimapNmax 50 \
-																	--peOverlapNbasesMin 10 \
-																	--alignSplicedMateMapLminOverLmate 0.5 \
-																	--alignSJstitchMismatchNmax 5 -1 5 5 \
-																	--chimSegmentMin 10 \
-																	--chimOutType WithinBAM HardClip \
-																	--chimJunctionOverhangMin 10 \
-																	--chimScoreDropMax 30 \
-																	--chimScoreJunctionNonGTAG 0 \
-																	--chimScoreSeparation 1 \
-																	--chimSegmentReadGapMax 3 \
-																	--chimMultimapNmax 50 \
-																	--outFileNamePrefix arriba/$1/$1. > $$(*)")
+									STAR \
+									--runThreadN $$(STAR_THREADS) \
+									--genomeDir $$(STAR_INDEX_DIR) \
+									--genomeLoad NoSharedMemory \
+									--readFilesIn $$(<) $$(<<) \
+									--readFilesCommand zcat \
+									--outStd BAM_Unsorted \
+									--outSAMtype BAM Unsorted \
+									--outSAMunmapped Within \
+									--outBAMcompression 0 \
+									--outFilterMultimapNmax 50 \
+									--peOverlapNbasesMin 10 \
+									--alignSplicedMateMapLminOverLmate 0.5 \
+									--alignSJstitchMismatchNmax 5 -1 5 5 \
+									--chimSegmentMin 10 \
+									--chimOutType WithinBAM HardClip \
+									--chimJunctionOverhangMin 10 \
+									--chimScoreDropMax 30 \
+									--chimScoreJunctionNonGTAG 0 \
+									--chimScoreSeparation 1 \
+									--chimSegmentReadGapMax 3 \
+									--chimMultimapNmax 50 \
+									--outFileNamePrefix arriba/$1/$1. > $$(*)")
 
 endef
 $(foreach sample,$(SAMPLES),\
@@ -57,14 +59,14 @@ $(foreach sample,$(SAMPLES),\
 define run-arriba
 arriba/$1/fusions.tsv : arriba/$1/$1.Aligned.out.bam
 	$$(call RUN,-c -n 1 -s 24G -m 36G -v $(ARRIBA_ENV),"set -o pipefail && \
-														$$(ARRIBA_EXE) -x $$(<) \
-														-o arriba/$1/fusions.tsv -O arriba/$1/discarded.tsv \
-														-a $$(ASSEMBLY_FA) \
-														-g $$(ANNOTATION_GTF) \
-														-b $$(BLACKLIST_TSV) \
-														-k $$(KNOWN_FUSIONS_TSV) \
-														-t $$(KNOWN_FUSIONS_TSV) \
-														-p $$(PROTEIN_DOMAINS_GFF3)")
+							    $$(ARRIBA_EXE) -x $$(<) \
+							    -o arriba/$1/fusions.tsv -O arriba/$1/discarded.tsv \
+							    -a $$(ASSEMBLY_FA) \
+							    -g $$(ANNOTATION_GTF) \
+							    -b $$(BLACKLIST_TSV) \
+							    -k $$(KNOWN_FUSIONS_TSV) \
+							    -t $$(KNOWN_FUSIONS_TSV) \
+							    -p $$(PROTEIN_DOMAINS_GFF3)")
 
 endef
 $(foreach sample,$(SAMPLES),\
@@ -72,11 +74,11 @@ $(foreach sample,$(SAMPLES),\
 		
 arriba/summary.txt : $(foreach sample,$(SAMPLES),arriba/$(sample)/fusions.tsv)
 	$(call RUN, -c -n 1 -s 12G -m 16G,"set -o pipefail && \
-									   $(RSCRIPT) $(SCRIPTS_DIR)/rna_seq/summarize_arriba.R --sample_names '$(SAMPLES)'")
+					   $(RSCRIPT) $(SCRIPTS_DIR)/rna_seq/summarize_arriba.R --sample_names '$(SAMPLES)'")
 
 
 ..DUMMY := $(shell mkdir -p version; \
-			 $(ARRIBA_EXE) -h > version/arriba.txt)
+	     $(ARRIBA_EXE) -h > version/arriba.txt)
 .SECONDARY:
 .DELETE_ON_ERROR:
 .PHONY: arriba
