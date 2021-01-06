3d3fd19ebc6b63f462f2bb3fd7a0e1b0261a6dbb
David Brown <ndbrown6@gmail.com>
2021-01-06 10:35:21 -0500

    Update kallisto.mk

--- rna_seq/kallisto.mk
+++ rna_seq/kallisto.mk
@@ -3,17 +3,17 @@ include innovation-lab/Makefile.inc
 LOGDIR = log/kallisto.$(NOW)
 
 kallisto : $(foreach sample,$(SAMPLES),kallisto/$(sample)/$(sample).1.fastq) \
-		   $(foreach sample,$(SAMPLES),kallisto/$(sample)/abundance.tsv) \
-		   kallisto/tpm_bygene.txt
+	   $(foreach sample,$(SAMPLES),kallisto/$(sample)/abundance.tsv) \
+	   kallisto/tpm_bygene.txt
 
 SLEUTH_ANNOT ?= $(HOME)/share/lib/resource_files/Hugo_ENST_ensembl75_fixed.txt
 
 define bam-to-fastq
 kallisto/$1/$1.1.fastq : bam/$1.bam
 	$$(call RUN,-n 4 -s 4G -m 9G,"set -o pipefail && \
-								  mkdir -p kallisto/$1 && \
-								  $$(SAMTOOLS) sort -T kallisto/$1/$1 -O bam -n -@ 4 -m 6G $$(<) | \
-								  bedtools bamtofastq -i - -fq kallisto/$1/$1.1.fastq -fq2 kallisto/$1/$1.2.fastq")
+				      mkdir -p kallisto/$1 && \
+				      $$(SAMTOOLS) sort -T kallisto/$1/$1 -O bam -n -@ 4 -m 6G $$(<) | \
+				      bedtools bamtofastq -i - -fq kallisto/$1/$1.1.fastq -fq2 kallisto/$1/$1.2.fastq")
 
 endef
 $(foreach sample,$(SAMPLES),\
@@ -22,11 +22,11 @@ $(foreach sample,$(SAMPLES),\
 define fastq-to-kallisto
 kallisto/$1/abundance.tsv : kallisto/$1/$1.1.fastq
 	$$(call RUN,-c -n 12 -s 2G -m 3G -v $(KALLISTO_ENV),"set -o pipefail && \
-														 kallisto quant \
-														 -i $$(KALLISTO_INDEX) \
-														 -o kallisto/$1 \
-														 --bias -b 100 -t 12 -rf-stranded \
-														 --fusion kallisto/$1/$1.1.fastq kallisto/$1/$1.2.fastq")
+							     kallisto quant \
+							     -i $$(KALLISTO_INDEX) \
+							     -o kallisto/$1 \
+							     --bias -b 100 -t 12 -rf-stranded \
+							     --fusion kallisto/$1/$1.1.fastq kallisto/$1/$1.2.fastq")
 
 endef
 $(foreach sample,$(SAMPLES),\
@@ -34,13 +34,12 @@ $(foreach sample,$(SAMPLES),\
 		
 kallisto/tpm_bygene.txt : $(foreach sample,$(SAMPLES),kallisto/$(sample)/abundance.tsv)
 	$(call RUN, -c -n 24 -s 1G -m 2G -v $(KALLISTO_ENV),"set -o pipefail && \
-														$(RSCRIPT) $(SCRIPTS_DIR)/rna_seq/summarize_sleuth.R --annotation $(SLEUTH_ANNOT) --samples '$(SAMPLES)'")
-
+							     $(RSCRIPT) $(SCRIPTS_DIR)/rna_seq/summarize_sleuth.R --annotation $(SLEUTH_ANNOT) --samples '$(SAMPLES)'")
 
 ..DUMMY := $(shell mkdir -p version; \
-			 $(SAMTOOLS) --version > version/kallisto.txt; \
-			 ~/share/usr/env/kallisto-0.46.2/bin/kallisto version >> version/kallisto.txt; \
-			 ~/share/usr/env/kallisto-0.46.2/bin/R --version >> version/kallisto.txt;)
+	     $(SAMTOOLS) --version > version/kallisto.txt; \
+	     ~/share/usr/env/kallisto-0.46.2/bin/kallisto version >> version/kallisto.txt; \
+	     ~/share/usr/env/kallisto-0.46.2/bin/R --version >> version/kallisto.txt;)
 .SECONDARY:
 .DELETE_ON_ERROR:
 .PHONY: kallisto
