0b3f4a19b0223f308bbc5801c3135fb53acc4f6e
David Brown <ndbrown6@gmail.com>
2021-01-14 01:25:23 -0500

    Update rnaseq_metrics.mk

--- qc/rnaseq_metrics.mk
+++ qc/rnaseq_metrics.mk
@@ -8,44 +8,44 @@ STRAND_SPECIFICITY ?= NONE
 TARGETS_LIST ?= $(HOME)/share/lib/resource_files/IDT_exome_research_targets.list
 
 rnaseq_metrics : $(foreach sample,$(SAMPLES),metrics/$(sample)_aln_rnaseq_metrics.txt) \
-				 $(foreach sample,$(SAMPLES),metrics/$(sample)_aln_alignment_metrics.txt) \
-				 $(foreach sample,$(SAMPLES),metrics/$(sample)_aln_insert_metrics.txt) \
-				 $(foreach sample,$(SAMPLES),metrics/$(sample)_aln_hs_metrics.txt) \
-				 $(foreach sample,$(SAMPLES),metrics/$(sample)_aln_dd_rnaseq_metrics.txt) \
-				 $(foreach sample,$(SAMPLES),metrics/$(sample)_aln_dd_alignment_metrics.txt) \
-				 $(foreach sample,$(SAMPLES),metrics/$(sample)_aln_dd_insert_metrics.txt) \
-				 $(foreach sample,$(SAMPLES),metrics/$(sample)_aln_dd_hs_metrics.txt) \
-				 summary/rnaseq_aln_metrics.txt \
-				 summary/alignment_aln_metrics.txt \
-				 summary/insert_aln_metrics.txt \
-				 summary/dv200_aln_metrics.txt \
-				 summary/rnaseq_aln_dd_metrics.txt \
-				 summary/alignment_aln_dd_metrics.txt \
-				 summary/insert_aln_dd_metrics.txt \
-				 summary/dv200_aln_dd_metrics.txt \
-				 summary/aln_hs_metrics.txt \
-				 summary/aln_dd_hs_metrics.txt
+		 $(foreach sample,$(SAMPLES),metrics/$(sample)_aln_alignment_metrics.txt) \
+		 $(foreach sample,$(SAMPLES),metrics/$(sample)_aln_insert_metrics.txt) \
+		 $(foreach sample,$(SAMPLES),metrics/$(sample)_aln_hs_metrics.txt) \
+		 $(foreach sample,$(SAMPLES),metrics/$(sample)_aln_dd_rnaseq_metrics.txt) \
+		 $(foreach sample,$(SAMPLES),metrics/$(sample)_aln_dd_alignment_metrics.txt) \
+		 $(foreach sample,$(SAMPLES),metrics/$(sample)_aln_dd_insert_metrics.txt) \
+		 $(foreach sample,$(SAMPLES),metrics/$(sample)_aln_dd_hs_metrics.txt) \
+		 summary/rnaseq_aln_metrics.txt \
+		 summary/alignment_aln_metrics.txt \
+		 summary/insert_aln_metrics.txt \
+		 summary/dv200_aln_metrics.txt \
+		 summary/rnaseq_aln_dd_metrics.txt \
+		 summary/alignment_aln_dd_metrics.txt \
+		 summary/insert_aln_dd_metrics.txt \
+		 summary/dv200_aln_dd_metrics.txt \
+		 summary/aln_hs_metrics.txt \
+		 summary/aln_dd_hs_metrics.txt
 
 define rnaseq-metrics
 metrics/$1_aln_rnaseq_metrics.txt : star/$1.Aligned.sortedByCoord.out.bam
 	$$(call RUN,-c -s 6G -m 12G,"set -o pipefail && \
-                                 $$(COLLECT_RNASEQ_METRICS) \
-                                 INPUT=$$(<) \
-                                 OUTPUT=$$(@) \
-                                 REF_FLAT=$$(REF_FLAT) \
-                                 RIBOSOMAL_INTERVALS=$$(RIBOSOMAL_INTERVALS) \
-                                 CHART_OUTPUT=metrics/$1_aln.pdf \
-                                 STRAND_SPECIFICITY=$$(STRAND_SPECIFICITY)")
+				     $$(COLLECT_RNASEQ_METRICS) \
+				     INPUT=$$(<) \
+				     OUTPUT=$$(@) \
+				     REF_FLAT=$$(REF_FLAT) \
+				     RIBOSOMAL_INTERVALS=$$(RIBOSOMAL_INTERVALS) \
+				     CHART_OUTPUT=metrics/$1_aln.pdf \
+				     STRAND_SPECIFICITY=$$(STRAND_SPECIFICITY)")
 
 metrics/$1_aln_dd_rnaseq_metrics.txt : bam/$1.bam
 	$$(call RUN,-c -s 6G -m 12G,"set -o pipefail && \
-                                 $$(COLLECT_RNASEQ_METRICS) \
-                                 INPUT=$$(<) \
-                                 OUTPUT=$$(@) \
-                                 REF_FLAT=$$(REF_FLAT) \
-                                 RIBOSOMAL_INTERVALS=$$(RIBOSOMAL_INTERVALS) \
-                                 CHART_OUTPUT=metrics/$1.pdf \
-                                 STRAND_SPECIFICITY=$$(STRAND_SPECIFICITY)")
+				     $$(COLLECT_RNASEQ_METRICS) \
+				     INPUT=$$(<) \
+				     OUTPUT=$$(@) \
+				     REF_FLAT=$$(REF_FLAT) \
+				     RIBOSOMAL_INTERVALS=$$(RIBOSOMAL_INTERVALS) \
+				     CHART_OUTPUT=metrics/$1.pdf \
+				     STRAND_SPECIFICITY=$$(STRAND_SPECIFICITY)")
 
 endef
 $(foreach sample,$(SAMPLES),\
@@ -54,17 +54,17 @@ $(foreach sample,$(SAMPLES),\
 define alignment-metrics
 metrics/$1_aln_alignment_metrics.txt : star/$1.Aligned.sortedByCoord.out.bam
 	$$(call RUN, -c -n 1 -s 6G -m 12G,"set -o pipefail && \
-									   $$(COLLECT_ALIGNMENT_METRICS) \
-									   REFERENCE_SEQUENCE=$$(REF_FASTA) \
-									   INPUT=$$(<) \
-									   OUTPUT=$$(@)")
+					   $$(COLLECT_ALIGNMENT_METRICS) \
+					   REFERENCE_SEQUENCE=$$(REF_FASTA) \
+					   INPUT=$$(<) \
+					   OUTPUT=$$(@)")
 
 metrics/$1_aln_dd_alignment_metrics.txt : bam/$1.bam
 	$$(call RUN, -c -n 1 -s 6G -m 12G,"set -o pipefail && \
-									   $$(COLLECT_ALIGNMENT_METRICS) \
-									   REFERENCE_SEQUENCE=$$(REF_FASTA) \
-									   INPUT=$$(<) \
-									   OUTPUT=$$(@)")
+					   $$(COLLECT_ALIGNMENT_METRICS) \
+					   REFERENCE_SEQUENCE=$$(REF_FASTA) \
+					   INPUT=$$(<) \
+					   OUTPUT=$$(@)")
 
 endef
 $(foreach sample,$(SAMPLES),\
@@ -73,17 +73,17 @@ $(foreach sample,$(SAMPLES),\
 define insert-size-metrics
 metrics/$1_aln_insert_metrics.txt : star/$1.Aligned.sortedByCoord.out.bam
 	$$(call RUN,-c -s 6G -m 12G,"set -o pipefail && \
-								 $$(COLLECT_INSERT_METRICS) \
- 								 INPUT=$$(<) \
-								 OUTPUT=$$(@) \
-								 HISTOGRAM_FILE=metrics/$1_aln.insert_metrics.pdf")
+				     $$(COLLECT_INSERT_METRICS) \
+				     INPUT=$$(<) \
+				     OUTPUT=$$(@) \
+				     HISTOGRAM_FILE=metrics/$1_aln.insert_metrics.pdf")
 
 metrics/$1_aln_dd_insert_metrics.txt : bam/$1.bam
 	$$(call RUN,-c -s 6G -m 12G,"set -o pipefail && \
-								 $$(COLLECT_INSERT_METRICS) \
- 								 INPUT=$$(<) \
-								 OUTPUT=$$(@) \
-								 HISTOGRAM_FILE=metrics/$1_aln_dd.insert_metrics.pdf")
+				     $$(COLLECT_INSERT_METRICS) \
+				     INPUT=$$(<) \
+				     OUTPUT=$$(@) \
+				     HISTOGRAM_FILE=metrics/$1_aln_dd.insert_metrics.pdf")
 
 endef
 $(foreach sample,$(SAMPLES),\
@@ -92,23 +92,23 @@ $(foreach sample,$(SAMPLES),\
 define hs-metrics
 metrics/$1_aln_hs_metrics.txt : star/$1.Aligned.sortedByCoord.out.bam
 	$$(call RUN,-c -n 1 -s 6G -m 12G,"set -o pipefail && \
-									  $$(CALC_HS_METRICS) \
-									  R=$(REF_FASTA) \
-									  I=$$(<) \
-									  O=$$(@) \
-									  BAIT_INTERVALS=$(TARGETS_LIST) \
-									  TARGET_INTERVALS=$(TARGETS_LIST) \
-									  TMP_DIR=$(TMPDIR)")
+					  $$(CALC_HS_METRICS) \
+					  R=$(REF_FASTA) \
+					  I=$$(<) \
+					  O=$$(@) \
+					  BAIT_INTERVALS=$(TARGETS_LIST) \
+					  TARGET_INTERVALS=$(TARGETS_LIST) \
+					  TMP_DIR=$(TMPDIR)")
 
 metrics/$1_aln_dd_hs_metrics.txt : bam/$1.bam
 	$$(call RUN,-c -n 1 -s 6G -m 12G,"set -o pipefail && \
-								 	  $$(CALC_HS_METRICS) \
-									  R=$(REF_FASTA) \
-									  I=$$(<) \
-									  O=$$(@) \
-									  BAIT_INTERVALS=$(TARGETS_LIST) \
-									  TARGET_INTERVALS=$(TARGETS_LIST) \
-									  TMP_DIR=$(TMPDIR)")
+					  $$(CALC_HS_METRICS) \
+					  R=$(REF_FASTA) \
+					  I=$$(<) \
+					  O=$$(@) \
+					  BAIT_INTERVALS=$(TARGETS_LIST) \
+					  TARGET_INTERVALS=$(TARGETS_LIST) \
+					  TMP_DIR=$(TMPDIR)")
 
 endef
 $(foreach sample,$(SAMPLES),\
@@ -117,47 +117,47 @@ $(foreach sample,$(SAMPLES),\
 
 summary/rnaseq_aln_metrics.txt : $(foreach sample,$(SAMPLES),metrics/$(sample)_aln_rnaseq_metrics.txt)
 	$(call RUN, -c -n 1 -s 4G -m 6G,"set -o pipefail && \
-                                     $(RSCRIPT) $(SCRIPTS_DIR)/qc/rnaseq_metrics.R --option 1 --sample_names '$(SAMPLES)'")
+					 $(RSCRIPT) $(SCRIPTS_DIR)/qc/rnaseq_metrics.R --option 1 --sample_names '$(SAMPLES)'")
 
 summary/alignment_aln_metrics.txt : $(foreach sample,$(SAMPLES),metrics/$(sample)_aln_alignment_metrics.txt)
 	$(call RUN, -c -n 1 -s 4G -m 6G,"set -o pipefail && \
-                                     $(RSCRIPT) $(SCRIPTS_DIR)/qc/rnaseq_metrics.R --option 2 --sample_names '$(SAMPLES)'")
+					 $(RSCRIPT) $(SCRIPTS_DIR)/qc/rnaseq_metrics.R --option 2 --sample_names '$(SAMPLES)'")
 									 
 summary/rnaseq_aln_dd_metrics.txt : $(foreach sample,$(SAMPLES),metrics/$(sample)_aln_dd_rnaseq_metrics.txt)
 	$(call RUN, -c -n 1 -s 4G -m 6G,"set -o pipefail && \
-                                     $(RSCRIPT) $(SCRIPTS_DIR)/qc/rnaseq_metrics.R --option 3 --sample_names '$(SAMPLES)'")
+					 $(RSCRIPT) $(SCRIPTS_DIR)/qc/rnaseq_metrics.R --option 3 --sample_names '$(SAMPLES)'")
 
 summary/alignment_aln_dd_metrics.txt : $(foreach sample,$(SAMPLES),metrics/$(sample)_aln_dd_alignment_metrics.txt)
 	$(call RUN, -c -n 1 -s 4G -m 6G,"set -o pipefail && \
-                                     $(RSCRIPT) $(SCRIPTS_DIR)/qc/rnaseq_metrics.R --option 4 --sample_names '$(SAMPLES)'")
+					 $(RSCRIPT) $(SCRIPTS_DIR)/qc/rnaseq_metrics.R --option 4 --sample_names '$(SAMPLES)'")
 									 
 summary/insert_aln_metrics.txt : $(foreach sample,$(SAMPLES),metrics/$(sample)_aln_insert_metrics.txt)
 	$(call RUN, -c -n 1 -s 4G -m 6G,"set -o pipefail && \
-                                     $(RSCRIPT) $(SCRIPTS_DIR)/qc/rnaseq_metrics.R --option 5 --sample_names '$(SAMPLES)'")
+					 $(RSCRIPT) $(SCRIPTS_DIR)/qc/rnaseq_metrics.R --option 5 --sample_names '$(SAMPLES)'")
 
 summary/insert_aln_dd_metrics.txt : $(foreach sample,$(SAMPLES),metrics/$(sample)_aln_dd_insert_metrics.txt)
 	$(call RUN, -c -n 1 -s 4G -m 6G,"set -o pipefail && \
-                                     $(RSCRIPT) $(SCRIPTS_DIR)/qc/rnaseq_metrics.R --option 6 --sample_names '$(SAMPLES)'")
+					 $(RSCRIPT) $(SCRIPTS_DIR)/qc/rnaseq_metrics.R --option 6 --sample_names '$(SAMPLES)'")
 									 
 summary/dv200_aln_metrics.txt : $(foreach sample,$(SAMPLES),metrics/$(sample)_aln_insert_metrics.txt)
 	$(call RUN, -c -n 1 -s 4G -m 6G,"set -o pipefail && \
-                                     $(RSCRIPT) $(SCRIPTS_DIR)/qc/rnaseq_metrics.R --option 7 --sample_names '$(SAMPLES)'")
+					 $(RSCRIPT) $(SCRIPTS_DIR)/qc/rnaseq_metrics.R --option 7 --sample_names '$(SAMPLES)'")
 
 summary/dv200_aln_dd_metrics.txt : $(foreach sample,$(SAMPLES),metrics/$(sample)_aln_dd_insert_metrics.txt)
 	$(call RUN, -c -n 1 -s 4G -m 6G,"set -o pipefail && \
-                                     $(RSCRIPT) $(SCRIPTS_DIR)/qc/rnaseq_metrics.R --option 8 --sample_names '$(SAMPLES)'")
+					 $(RSCRIPT) $(SCRIPTS_DIR)/qc/rnaseq_metrics.R --option 8 --sample_names '$(SAMPLES)'")
                                      
 summary/aln_hs_metrics.txt : $(foreach sample,$(SAMPLES),metrics/$(sample)_aln_hs_metrics.txt)
 	$(call RUN, -c -n 1 -s 4G -m 6G,"set -o pipefail && \
-                                     $(RSCRIPT) $(SCRIPTS_DIR)/qc/rnaseq_metrics.R --option 9 --sample_names '$(SAMPLES)'")
+					 $(RSCRIPT) $(SCRIPTS_DIR)/qc/rnaseq_metrics.R --option 9 --sample_names '$(SAMPLES)'")
 
 summary/aln_dd_hs_metrics.txt : $(foreach sample,$(SAMPLES),metrics/$(sample)_aln_dd_hs_metrics.txt)
 	$(call RUN, -c -n 1 -s 4G -m 6G,"set -o pipefail && \
-                                     $(RSCRIPT) $(SCRIPTS_DIR)/qc/rnaseq_metrics.R --option 10 --sample_names '$(SAMPLES)'")
+					 $(RSCRIPT) $(SCRIPTS_DIR)/qc/rnaseq_metrics.R --option 10 --sample_names '$(SAMPLES)'")
 
 ..DUMMY := $(shell mkdir -p version; \
-			 echo "picard" >> version/rnaseq_metrics.txt; \
-			 $(PICARD) CollectRnaSeqMetrics --version &>> version/rnaseq_metrics.txt; \
+	     echo "picard" >> version/rnaseq_metrics.txt; \
+	     $(PICARD) CollectRnaSeqMetrics --version &>> version/rnaseq_metrics.txt; \
              R --version >> version/rnaseq_metrics.txt)
 .SECONDARY:
 .DELETE_ON_ERROR:
