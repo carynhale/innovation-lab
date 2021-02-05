3d7d2b4384353c61c4762785052441d1149672e3
David Brown <ndbrown6@gmail.com>
2021-02-05 13:10:59 -0500

    Create star_align.mk

---
+++ workflows/star_align.mk
@@ -0,0 +1,74 @@
+include innovation-lab/Makefile.inc
+include innovation-lab/config/align.inc
+
+LOGDIR = log/star_align.$(NOW)
+
+ALIGNER := star
+BAM_NO_REALN = true
+BAM_NO_RECAL = true
+BAM_NO_FILTER = true
+BAM_DUP_TYPE = none
+SEQ_PLATFROM = illumina
+
+STAR_OPTS = --genomeDir $(STAR_REF) \
+            --outSAMtype BAM SortedByCoordinate \
+			--twopassMode Basic \
+            --outReadsUnmapped None \
+            --chimSegmentMin 12 \
+            --chimJunctionOverhangMin 12 \
+            --alignSJDBoverhangMin 10 \
+            --alignMatesGapMax 200000 \
+            --alignIntronMax 200000 \
+            --chimSegmentReadGapMax parameter 3 \
+            --alignSJstitchMismatchNmax 5 -1 5 5 \
+            --chimOutType WithinBAM \
+			--quantMode GeneCounts
+
+star : $(foreach sample,$(SAMPLES),star/$(sample).Aligned.sortedByCoord.out.bam \
+                                   star/$(sample).Aligned.sortedByCoord.out.bam.bai \
+                                   bam/$(sample).bam \
+                                   bam/$(sample).bam.bai \
+                                   bam/$(sample).bai)
+
+define align-split-fastq
+star/$1.Aligned.sortedByCoord.out.bam : $3
+	$$(call RUN,-n 4 -s 6G -m 10G,"set -o pipefail && \
+                                   STAR $$(STAR_OPTS) \
+                                   --outFileNamePrefix star/$1. \
+                                   --runThreadN 4 \
+                                   --outSAMattrRGline \"ID:$1\" \"LB:$1\" \"SM:$1\" \"PL:$${SEQ_PLATFORM}\" \
+                                   --readFilesIn $$^ \
+                                   --readFilesCommand zcat")
+                                   
+star/$1.Aligned.sortedByCoord.out.bam.bai : star/$1.Aligned.sortedByCoord.out.bam
+	$$(call RUN,-n 1 -s 2G -m 4G,"set -o pipefail && \
+                                  $$(SAMTOOLS) index $$(<)")
+
+bam/$1.bam : star/$1.Aligned.sortedByCoord.out.bam
+	$$(call RUN,-n 1 -s 2G -m 4G,"set -o pipefail && \
+                                  cp $$(<) $$(@)")
+                                   
+bam/$1.bam.bai : star/$1.Aligned.sortedByCoord.out.bam.bai
+	$$(call RUN,-n 1 -s 2G -m 4G,"set -o pipefail && \
+                                  cp $$(<) $$(@)")
+                                   
+bam/$1.bai : star/$1.Aligned.sortedByCoord.out.bam.bai
+	$$(call RUN,-n 1 -s 2G -m 4G,"set -o pipefail && \
+                                  cp $$(<) $$(@)")
+
+endef
+$(foreach ss,$(SPLIT_SAMPLES), \
+	$(if $(fq.$(ss)), \
+	$(eval $(call align-split-fastq,$(split.$(ss)),$(ss),$(fq.$(ss))))))
+    
+..DUMMY := $(shell mkdir -p version; \
+             echo "STAR" > version/star_align.txt; \
+			 STAR --version >> version/star_align.txt; \
+             $(SAMTOOLS) --version >> version/star_align.txt)
+.SECONDARY: 
+.DELETE_ON_ERROR:
+.PHONY: star
+
+include innovation-lab/fastq_tools/fastq.mk
+include innovation-lab/bam_tools/process_bam.mk
+include innovation-lab/aligners/align.mk
