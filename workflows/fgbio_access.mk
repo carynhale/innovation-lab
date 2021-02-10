f7df1cbd9c51c9fdffc1fc6a0c67d7f40155fa6f
David Brown <ndbrown6@gmail.com>
2021-02-09 19:16:43 -0500

    Update fgbio_access.mk

--- workflows/fgbio_access.mk
+++ workflows/fgbio_access.mk
@@ -495,13 +495,6 @@ endef
 $(foreach sample,$(SAMPLES),\
  		$(eval $(call picard-metrics-duplex,$(sample))))
 		
-	       summary/idx_metrics.txt \
-	       summary/aln_metrics.txt \
-	       summary/insert_metrics.txt \
-	       summary/oxog_metrics.txt \
-	       summary/hs_metrics.txt
-
-		
 summary/idx_metrics.txt : $(foreach sample,$(SAMPLES),metrics/$(sample)_cl_aln_srt_MD_IR_FX.idx_stats.txt) $(foreach sample,$(SAMPLES),metrics/$(sample)_cl_aln_srt_MD_IR_FX__grp_DC_MA_RG_IR_FX.idx_stats.txt) $(foreach sample,$(SAMPLES),metrics/$(sample)_cl_aln_srt_MD_IR_FX__grp_DC_MA_RG_IR_FX_SIMPLEX.idx_stats.txt) $(foreach sample,$(SAMPLES),metrics/$(sample)_cl_aln_srt_MD_IR_FX__grp_DC_MA_RG_IR_FX_DUPLEX.idx_stats.txt)
 	$(call RUN, -c -n 1 -s 8G -m 12G,"set -o pipefail && \
 					  $(RSCRIPT) $(SCRIPTS_DIR)/qc/fgbio_access.R --option 1 --sample_names '$(SAMPLES)'")
