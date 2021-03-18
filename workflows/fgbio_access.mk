642b3f7361c1db0ee686ff48da59a5914b74b5d4
David Brown <ndbrown6@gmail.com>
2021-03-18 13:08:47 -0400

    Update fgbio_access.mk

--- workflows/fgbio_access.mk
+++ workflows/fgbio_access.mk
@@ -497,37 +497,6 @@ endef
 $(foreach sample,$(SAMPLES),\
  		$(eval $(call picard-metrics-duplex,$(sample))))
 		
-define pileup-metric
-waltz/$1_cl_aln_srt_MD_IR_FX-pileup.txt : fgbio/$1/$1_cl_aln_srt_MD_IR_FX.bam
-	$$(call RUN,-c -s 6G -m 12G,"set -o pipefail && \
-				     cd waltz && \
-				     ln -sf ../fgbio/$1/$1_cl_aln_srt_MD_IR_FX.bam $1_cl_aln_srt_MD_IR_FX.bam && \
-				     ln -sf ../fgbio/$1/$1_cl_aln_srt_MD_IR_FX.bai $1_cl_aln_srt_MD_IR_FX.bai && \
-				     ln -sf ../fgbio/$1/$1_cl_aln_srt_MD_IR_FX.bai $1_cl_aln_srt_MD_IR_FX.bam.bai && \
-				     if [[ ! -f '.bed' ]]; then cp $$(WALTZ_BED_FILE) .bed; fi && \
-				     $$(call WALTZ_CMD,2G,8G) org.mskcc.juber.waltz.Waltz PileupMetrics $$(WALTZ_MIN_MAPQ) $1_cl_aln_srt_MD_IR_FX.bam $$(REF_FASTA) .bed && \
-				     unlink $1_cl_aln_srt_MD_IR_FX.bam && \
-				     unlink $1_cl_aln_srt_MD_IR_FX.bai && \
-				     unlink $1_cl_aln_srt_MD_IR_FX.bam.bai && \
-				     cd ../..")
-									 
-waltz/$1_cl_aln_srt_MD_IR_FX__grp_DC_MA_RG_IR_FX-pileup.txt : fgbio/$1/$1_cl_aln_srt_MD_IR_FX__grp_DC_MA_RG_IR_FX.bam
-	$$(call RUN,-c -s 6G -m 12G,"set -o pipefail && \
-				     cd waltz && \
-				     ln -sf ../fgbio/$1/$1_cl_aln_srt_MD_IR_FX__grp_DC_MA_RG_IR_FX.bam $1_cl_aln_srt_MD_IR_FX__grp_DC_MA_RG_IR_FX.bam && \
-				     ln -sf ../fgbio/$1/$1_cl_aln_srt_MD_IR_FX__grp_DC_MA_RG_IR_FX.bai $1_cl_aln_srt_MD_IR_FX__grp_DC_MA_RG_IR_FX.bai && \
-				     ln -sf ../fgbio/$1/$1_cl_aln_srt_MD_IR_FX__grp_DC_MA_RG_IR_FX.bam.bai $1_cl_aln_srt_MD_IR_FX__grp_DC_MA_RG_IR_FX.bam.bai && \
-				     if [[ ! -f '.bed' ]]; then cp $$(WALTZ_BED_FILE) .bed; fi && \
-				     $$(call WALTZ_CMD,2G,8G) org.mskcc.juber.waltz.Waltz PileupMetrics $$(WALTZ_MIN_MAPQ) $1_cl_aln_srt_MD_IR_FX__grp_DC_MA_RG_IR_FX.bam $$(REF_FASTA) .bed && \
-				     unlink $1_cl_aln_srt_MD_IR_FX__grp_DC_MA_RG_IR_FX.bam && \
-				     unlink $1_cl_aln_srt_MD_IR_FX__grp_DC_MA_RG_IR_FX.bai && \
-				     unlink $1_cl_aln_srt_MD_IR_FX__grp_DC_MA_RG_IR_FX.bam.bai && \
-				     cd ../..")
-								 
-endef
-$(foreach sample,$(SAMPLES),\
-		$(eval $(call pileup-metric,$(sample))))
-		
 summary/idx_metrics.txt : $(foreach sample,$(SAMPLES),metrics/$(sample)_cl_aln_srt_MD_IR_FX.idx_stats.txt) $(foreach sample,$(SAMPLES),metrics/$(sample)_cl_aln_srt_MD_IR_FX__grp_DC_MA_RG_IR_FX.idx_stats.txt) $(foreach sample,$(SAMPLES),metrics/$(sample)_cl_aln_srt_MD_IR_FX__grp_DC_MA_RG_IR_FX_SIMPLEX.idx_stats.txt) $(foreach sample,$(SAMPLES),metrics/$(sample)_cl_aln_srt_MD_IR_FX__grp_DC_MA_RG_IR_FX_DUPLEX.idx_stats.txt)
 	$(call RUN, -c -n 1 -s 8G -m 12G,"set -o pipefail && \
 					  $(RSCRIPT) $(SCRIPTS_DIR)/qc/fgbio_access.R --option 1 --sample_names '$(SAMPLES)'")
