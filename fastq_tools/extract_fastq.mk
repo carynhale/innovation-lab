e1e54de89f2d458969e4a2f6c725eeef49af4819
David Brown <ndbrown6@gmail.com>
2021-03-22 11:54:21 -0400

    Update extract_fastq.mk

--- fastq_tools/extract_fastq.mk
+++ fastq_tools/extract_fastq.mk
@@ -2,13 +2,14 @@ include modules/Makefile.inc
 
 LOGDIR ?= log/extract_fastq.$(NOW)
 
-VPATH = rawdata unprocessed_bam
-
 extract_fastq : $(foreach sample,$(SAMPLES),fastq/$(sample).1.fastq.gz)
 
 fastq/%.1.fastq.gz fastq/%.2.fastq.gz : %.bam
-	$(call RUN,-n 4 -s 4G -m 9G,"$(SAMTOOLS2) sort -T $(<D)/$* -O bam -n -@ 4 -m 6G $< | $(SAMTOOLS2) fastq -f 1 -1 >(gzip -c > fastq/$*.1.fastq.gz) -2 >(gzip -c > fastq/$*.2.fastq.gz) -")
+	$(call RUN,-n 4 -s 4G -m 9G,"$(SAMTOOLS) sort -T $(<D)/$* -O bam -n -@ 4 -m 6G $< | \
+				     $(SAMTOOLS) fastq -f 1 -1 >(gzip -c > fastq/$*.1.fastq.gz) -2 >(gzip -c > fastq/$*.2.fastq.gz) -")
 
-.DELETE_ON_ERROR:
+..DUMMY := $(shell mkdir -p version; \
+	     $(SAMTOOLS) --version > version/extract_fastq.txt)
 .SECONDARY:
+.DELETE_ON_ERROR: 
 .PHONY: extract_fastq
