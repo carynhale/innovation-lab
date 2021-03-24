47bf5e969acb052215ccf81ff570a6dae539c76f
David Brown <ndbrown6@gmail.com>
2021-03-24 18:21:03 -0400

    Update subsample_fastq.mk

--- fastq_tools/subsample_fastq.mk
+++ fastq_tools/subsample_fastq.mk
@@ -4,6 +4,7 @@ LOGDIR ?= log/subsample_fastq.$(NOW)
 
 subsample_fastq : $(foreach sample,$(SAMPLES),FASTQ_DOWNSAMPLE/fastq/$(sample).taskcomplete)
 
+THREADS = 6
 SEED = 1
 TARGETREADS = 1000000 \
 	      2000000 \
@@ -33,8 +34,8 @@ $(foreach ss,$(SPLIT_SAMPLES),\
 
 define sample-fastq
 FASTQ_DOWNSAMPLE/fastq/$1.taskcomplete : FASTQ_DOWNSAMPLE/fastq/$1_R1--0.fastq.gz
-	$$(call RUN, -c -n 1 -s 6G -m 12G -v $(SEQTK_ENV),"set -o pipefail && \
-							   for x in $(TARGETREADS); do $$(SEQTK) sample -s $(SEED) FASTQ_DOWNSAMPLE/fastq/$1_R1--0.fastq.gz '$x' > FASTQ_DOWNSAMPLE/fastq/$1_R1--\$x.fastq.gz; done && \
+	$$(call RUN, -c -n $(THREADS) -s 4G -m 8G -v $(SEQTK_ENV),"set -o pipefail && \
+							   echo $(TARGETREADS) | parallel -j $(THREADS) '$$(SEQTK) sample -s $(SEED) FASTQ_DOWNSAMPLE/fastq/$1_R1--0.fastq.gz {} > FASTQ_DOWNSAMPLE/fastq/$1_R1--{}.fastq.gz' && \
 							   echo $1 >  FASTQ_DOWNSAMPLE/fastq/$1.taskcomplete")
 
 endef
