c6a2f6f091aa4ee7867a9c0974540af0035b3b26
David Brown <ndbrown6@gmail.com>
2021-03-25 10:04:05 -0400

    Update subsample_fastq.mk

--- fastq_tools/subsample_fastq.mk
+++ fastq_tools/subsample_fastq.mk
@@ -3,47 +3,47 @@ include innovation-lab/Makefile.inc
 LOGDIR ?= log/subsample_fastq.$(NOW)
 
 SEED = 1
-TARGET_READS = 1000000 \
-	       2000000 \
-	       3000000 \
-	       4000000 \
-	       5000000 \
-	       7000000 \
-	       10000000 \
-	       15000000 \
-	       20000000 \
-	       25000000 \
-	       30000000 \
-	       40000000 \
-	       50000000
+READS = 1000000 \
+	2000000 \
+	3000000 \
+	4000000 \
+	5000000 \
+	7000000 \
+	10000000 \
+	15000000 \
+	20000000 \
+	25000000 \
+	30000000 \
+	40000000 \
+	50000000
 
-subsample_fastq : $(foreach sample,$(SAMPLES),FASTQ_DOWNSAMPLE/$(sample)/$(sample)_R1.fastq.gz) \
-		  $(foreach sample,$(SAMPLES), \
-		  	$(foreach targetreads,$(TARGET_READS),FASTQ_DOWNSAMPLE/$(sample)/$(sample)_R1--$(targetreads).fastq.gz))
+subsample_fastq : $(foreach sample,$(SAMPLES),FASTQ_DOWNSAMPLE/fastq/$(sample)/$(sample)_R1.fastq.gz) #\
+#		  $(foreach sample,$(SAMPLES), \
+#		  	$(foreach reads,$(READS),FASTQ_DOWNSAMPLE/fastq/$(sample)/$(sample)_R1--$(reads).fastq.gz))
 
 define copy-fastq
-FASTQ_DOWNSAMPLE/$1/$1_R1.fastq.gz : $3
+FASTQ_DOWNSAMPLE/fastq/$1/$1_R1.fastq.gz : $3
 	$$(call RUN,-c -n 1 -s 2G -m 4G,"set -o pipefail && \
 					 mkdir -p FASTQ_DOWNSAMPLE/$1 && \
 					 $(RSCRIPT) $(SCRIPTS_DIR)/fastq_tools/copy_fastq.R \
 					 --sample_name $1 \
-					 --directory_name FASTQ_DOWNSAMPLE \
+					 --directory_name 'FASTQ_DOWNSAMPLE/fastq' \
 					 --fastq_files '$$^'")
 
 endef
 $(foreach ss,$(SPLIT_SAMPLES),\
 	$(if $(fq.$(ss)),$(eval $(call copy-fastq,$(split.$(ss)),$(ss),$(fq.$(ss))))))
 
-define sample-fastq
-FASTQ_DOWNSAMPLE/$1/$1_R1--$2.fastq.gz : FASTQ_DOWNSAMPLE/$1/$1_R1.fastq.gz
-	$$(call RUN, -c -s 12G -m 24G -v $(SEQTK_ENV),"set -o pipefail && \
-						       $$(SEQTK) sample -s $(SEED) FASTQ_DOWNSAMPLE/$1/$1_R1.fastq.gz $2 > FASTQ_DOWNSAMPLE/$1/$1_R1--$2.fastq.gz && \
-						       $$(SEQTK) sample -s $(SEED) FASTQ_DOWNSAMPLE/$1/$1_R2.fastq.gz $2 > FASTQ_DOWNSAMPLE/$1/$1_R2--$2.fastq.gz")
-
-endef
-$(foreach sample,$(SAMPLES), \
-	$(foreach targetreads,$(TARGET_READS), \
-		$(eval $(call sample-fastq,$(sample),$(targetreads)))))
+#define sample-fastq
+#FASTQ_DOWNSAMPLE/$1/$1_R1--$2.fastq.gz : FASTQ_DOWNSAMPLE/$1/$1_R1.fastq.gz
+#	$$(call RUN, -c -s 12G -m 24G -v $(SEQTK_ENV),"set -o pipefail && \
+#						       $$(SEQTK) sample -s $(SEED) FASTQ_DOWNSAMPLE/$1/$1_R1.fastq.gz $2 > FASTQ_DOWNSAMPLE/$1/$1_R1--$2.fastq.gz && \
+#						       $$(SEQTK) sample -s $(SEED) FASTQ_DOWNSAMPLE/$1/$1_R2.fastq.gz $2 > FASTQ_DOWNSAMPLE/$1/$1_R2--$2.fastq.gz")
+#
+#endef
+#$(foreach sample,$(SAMPLES), \
+#	$(foreach targetreads,$(TARGET_READS), \
+#		$(eval $(call sample-fastq,$(sample),$(targetreads)))))
 
 
 ..DUMMY := $(shell mkdir -p version; \
