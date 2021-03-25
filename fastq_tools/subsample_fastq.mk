fa88af8a4f424eb518817b9cd8f020c6a2890f7a
David Brown <ndbrown6@gmail.com>
2021-03-25 10:49:41 -0400

    Update subsample_fastq.mk

--- fastq_tools/subsample_fastq.mk
+++ fastq_tools/subsample_fastq.mk
@@ -4,7 +4,13 @@ LOGDIR ?= log/subsample_fastq.$(NOW)
 
 SEED = 1
 N = 1 2 3 4 5
-
+READS:
+    1: 1000000
+    2: 2000000
+    3: 3000000
+    4: 4000000
+    5: 5000000
+    
 subsample_fastq : $(foreach sample,$(SAMPLES),FASTQ_DOWNSAMPLE/fastq/$(sample)/$(sample)_R1.fastq.gz) \
 		  $(foreach sample,$(SAMPLES), \
 		  	$(foreach n,$(N),FASTQ_DOWNSAMPLE/fastq/$(sample)/$(sample)_R1--$(n).fastq.gz))
