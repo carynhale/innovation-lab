ea901befd64bf22849999a8390efdb91302a3b73
David Brown <ndbrown6@gmail.com>
2021-03-30 15:47:24 -0400

    Update salmon.mk

--- rna_seq/salmon.mk
+++ rna_seq/salmon.mk
@@ -3,7 +3,7 @@ include innovation-lab/Makefile.inc
 LOGDIR = log/salmon.$(NOW)
 
 salmon : $(foreach sample,$(SAMPLES),salmon/$(sample)/$(sample).1.fastq.gz) \
-	 $(foreach sample,$(SAMPLES),kallisto/$(sample)/quant.sf)
+	 $(foreach sample,$(SAMPLES),salmon/$(sample)/quant.sf)
 
 define bam-to-fastq
 salmon/$1/$1.1.fastq.gz : bam/$1.bam
