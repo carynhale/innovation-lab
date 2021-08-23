11dfd4a6ad6accea4fd4ec4dd6ebdf93bac63987
David Brown <ndbrown6@gmail.com>
2021-08-23 16:29:25 -0400

    Update em_seq.mk

--- workflows/em_seq.mk
+++ workflows/em_seq.mk
@@ -21,8 +21,9 @@ em_seq : $(foreach sample,$(SAMPLES),bismark/$(sample)/$(sample)_R1.fastq.gz) \
 	 $(foreach sample,$(SAMPLES),metrics/$(sample)_aln_srt_IR_FX_BR.aln_metrics) \
 	 $(foreach sample,$(SAMPLES),metrics/$(sample)_aln_srt_IR_FX_BR__F1R2.aln_metrics) \
 	 $(foreach sample,$(SAMPLES),metrics/$(sample)_aln_srt_IR_FX_BR__F2R1.aln_metrics)
-	 #summary/rrbs_metrics.txt \
-	 #summary/alignment_metrics.txt
+	 
+#summary/rrbs_metrics.txt \
+#summary/alignment_metrics.txt
 
 REF_FASTA = $(REF_DIR)/IDT_oligo/idt_oligo.fasta
 GENOME_FOLDER = $(REF_DIR)/IDT_oligo/
