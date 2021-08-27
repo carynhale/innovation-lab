fad5f6be84fc699171b2e2b840e966bcf39581e3
David Brown <ndbrown6@gmail.com>
2021-08-27 17:26:17 -0400

    Update beadlink_prism.mk

--- workflows/beadlink_prism.mk
+++ workflows/beadlink_prism.mk
@@ -63,8 +63,8 @@ beadlink_prism : $(foreach sample,$(SAMPLES),fgbio/$(sample)/$(sample)_R1.fastq.
 		 summary/hs_metrics.txt \
 		 summary/gc_metrics.txt \
 		 summary/umi_counts.txt \
-		 summary/umi_duplex_counts.txt
-#		 summary/umi_fixed_counts.txt \
+		 summary/umi_duplex_counts.txt \
+		 summary/umi_fixed_counts.txt
 #		 summary/umi_fixed_duplex_counts.txt \
 #		 summary/all_family_sizes.txt \
 #		 summary/duplex_family_sizes.txt \
