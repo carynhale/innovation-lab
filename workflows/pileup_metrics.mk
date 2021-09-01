d53edc86d51773ad5318ad33822aee8c7e957f11
David Brown <ndbrown6@gmail.com>
2021-09-01 13:43:43 -0400

    Update pileup_metrics.mk

--- workflows/pileup_metrics.mk
+++ workflows/pileup_metrics.mk
@@ -89,60 +89,60 @@ waltz/$1_cl_aln_srt_MD_IR_FX_BR-pileup.txt.gz : bam/$1_cl_aln_srt_MD_IR_FX_BR.ba
 	$$(call RUN,-c -n 4 -s 4G -m 6G,"set -o pipefail && \
 					 mkdir -p waltz && \
 					 cd waltz && \
-					 ln -sf ../bam/$1_cl_aln_srt_MD_IR_FX2.bam $1_cl_aln_srt_MD_IR_FX2.bam && \
-					 ln -sf ../bam/$1_cl_aln_srt_MD_IR_FX2.bai $1_cl_aln_srt_MD_IR_FX2.bai && \
-					 $$(call WALTZ_CMD,2G,8G) org.mskcc.juber.waltz.Waltz PileupMetrics $$(WALTZ_MIN_MAPQ) $1_cl_aln_srt_MD_IR_FX2.bam $$(REF_FASTA) $$(WALTZ_BED_FILE) && \
-					 gzip $1_cl_aln_srt_MD_IR_FX2-pileup.txt && \
-					 gzip $1_cl_aln_srt_MD_IR_FX2-pileup-without-duplicates.txt && \
-					 gzip $1_cl_aln_srt_MD_IR_FX2-intervals.txt && \
-					 gzip $1_cl_aln_srt_MD_IR_FX2-intervals-without-duplicates.txt && \
-					 unlink $1_cl_aln_srt_MD_IR_FX2.bam && \
-					 unlink $1_cl_aln_srt_MD_IR_FX2.bai && \
+					 ln -sf ../bam/$1_cl_aln_srt_MD_IR_FX_BR.bam $1_cl_aln_srt_MD_IR_FX_BR.bam && \
+					 ln -sf ../bam/$1_cl_aln_srt_MD_IR_FX_BR.bai $1_cl_aln_srt_MD_IR_FX_BR.bai && \
+					 $$(call WALTZ_CMD,2G,8G) org.mskcc.juber.waltz.Waltz PileupMetrics $$(WALTZ_MIN_MAPQ) $1_cl_aln_srt_MD_IR_FX_BR.bam $$(REF_FASTA) $$(WALTZ_BED_FILE) && \
+					 gzip $1_cl_aln_srt_MD_IR_FX_BR-pileup.txt && \
+					 gzip $1_cl_aln_srt_MD_IR_FX_BR-pileup-without-duplicates.txt && \
+					 gzip $1_cl_aln_srt_MD_IR_FX_BR-intervals.txt && \
+					 gzip $1_cl_aln_srt_MD_IR_FX_BR-intervals-without-duplicates.txt && \
+					 unlink $1_cl_aln_srt_MD_IR_FX_BR.bam && \
+					 unlink $1_cl_aln_srt_MD_IR_FX_BR.bai && \
 					 cd ..")
 
 waltz/$1_cl_aln_srt_MD_IR_FX_BR__grp_DC_MA_RG_IR_FX-pileup.txt.gz : bam/$1_cl_aln_srt_MD_IR_FX_BR__grp_DC_MA_RG_IR_FX.bam
 	$$(call RUN,-c -n 4 -s 4G -m 6G,"set -o pipefail && \
 					 mkdir -p waltz && \
 					 cd waltz && \
-					 ln -sf ../bam/$1_cl_aln_srt_MD_IR_FX2__grp_DC_MA_RG_IR_FX.bam $1_cl_aln_srt_MD_IR_FX2__grp_DC_MA_RG_IR_FX.bam && \
-					 ln -sf ../bam/$1_cl_aln_srt_MD_IR_FX2__grp_DC_MA_RG_IR_FX.bai $1_cl_aln_srt_MD_IR_FX2__grp_DC_MA_RG_IR_FX.bai && \
-					 $$(call WALTZ_CMD,2G,8G) org.mskcc.juber.waltz.Waltz PileupMetrics $$(WALTZ_MIN_MAPQ) $1_cl_aln_srt_MD_IR_FX2__grp_DC_MA_RG_IR_FX.bam $$(REF_FASTA) $$(WALTZ_BED_FILE) && \
-					 gzip $1_cl_aln_srt_MD_IR_FX2__grp_DC_MA_RG_IR_FX-pileup.txt && \
-					 gzip $1_cl_aln_srt_MD_IR_FX2__grp_DC_MA_RG_IR_FX-pileup-without-duplicates.txt && \
-					 gzip $1_cl_aln_srt_MD_IR_FX2__grp_DC_MA_RG_IR_FX-intervals.txt && \
-					 gzip $1_cl_aln_srt_MD_IR_FX2__grp_DC_MA_RG_IR_FX-intervals-without-duplicates.txt && \
-					 unlink $1_cl_aln_srt_MD_IR_FX2__grp_DC_MA_RG_IR_FX.bam && \
-					 unlink $1_cl_aln_srt_MD_IR_FX2__grp_DC_MA_RG_IR_FX.bai && \
+					 ln -sf ../bam/$1_cl_aln_srt_MD_IR_FX_BR__grp_DC_MA_RG_IR_FX.bam $1_cl_aln_srt_MD_IR_FX_BR__grp_DC_MA_RG_IR_FX.bam && \
+					 ln -sf ../bam/$1_cl_aln_srt_MD_IR_FX_BR__grp_DC_MA_RG_IR_FX.bai $1_cl_aln_srt_MD_IR_FX_BR__grp_DC_MA_RG_IR_FX.bai && \
+					 $$(call WALTZ_CMD,2G,8G) org.mskcc.juber.waltz.Waltz PileupMetrics $$(WALTZ_MIN_MAPQ) $1_cl_aln_srt_MD_IR_FX_BR__grp_DC_MA_RG_IR_FX.bam $$(REF_FASTA) $$(WALTZ_BED_FILE) && \
+					 gzip $1_cl_aln_srt_MD_IR_FX_BR__grp_DC_MA_RG_IR_FX-pileup.txt && \
+					 gzip $1_cl_aln_srt_MD_IR_FX_BR__grp_DC_MA_RG_IR_FX-pileup-without-duplicates.txt && \
+					 gzip $1_cl_aln_srt_MD_IR_FX_BR__grp_DC_MA_RG_IR_FX-intervals.txt && \
+					 gzip $1_cl_aln_srt_MD_IR_FX_BR__grp_DC_MA_RG_IR_FX-intervals-without-duplicates.txt && \
+					 unlink $1_cl_aln_srt_MD_IR_FX_BR__grp_DC_MA_RG_IR_FX.bam && \
+					 unlink $1_cl_aln_srt_MD_IR_FX_BR__grp_DC_MA_RG_IR_FX.bai && \
 					 cd ..")
 
 waltz/$1_cl_aln_srt_MD_IR_FX_BR__grp_DC_MA_RG_IR_FX_SIMPLEX-pileup.txt.gz : bam/$1_cl_aln_srt_MD_IR_FX_BR__grp_DC_MA_RG_IR_FX_SIMPLEX.bam
 	$$(call RUN,-c -n 4 -s 4G -m 6G,"set -o pipefail && \
 					 mkdir -p waltz && \
 					 cd waltz && \
-					 ln -sf ../bam/$1_cl_aln_srt_MD_IR_FX2__grp_DC_MA_RG_IR_FX_SIMPLEX.bam $1_cl_aln_srt_MD_IR_FX2__grp_DC_MA_RG_IR_FX_SIMPLEX.bam && \
-					 ln -sf ../bam/$1_cl_aln_srt_MD_IR_FX2__grp_DC_MA_RG_IR_FX_SIMPLEX.bai $1_cl_aln_srt_MD_IR_FX2__grp_DC_MA_RG_IR_FX_SIMPLEX.bai && \
-					 $$(call WALTZ_CMD,2G,8G) org.mskcc.juber.waltz.Waltz PileupMetrics $$(WALTZ_MIN_MAPQ) $1_cl_aln_srt_MD_IR_FX2__grp_DC_MA_RG_IR_FX_SIMPLEX.bam $$(REF_FASTA) $$(WALTZ_BED_FILE) && \
-					 gzip $1_cl_aln_srt_MD_IR_FX2__grp_DC_MA_RG_IR_FX_SIMPLEX-pileup.txt && \
-					 gzip $1_cl_aln_srt_MD_IR_FX2__grp_DC_MA_RG_IR_FX_SIMPLEX-pileup-without-duplicates.txt && \
-					 gzip $1_cl_aln_srt_MD_IR_FX2__grp_DC_MA_RG_IR_FX_SIMPLEX-intervals.txt && \
-					 gzip $1_cl_aln_srt_MD_IR_FX2__grp_DC_MA_RG_IR_FX_SIMPLEX-intervals-without-duplicates.txt && \
-					 unlink $1_cl_aln_srt_MD_IR_FX2__grp_DC_MA_RG_IR_FX_SIMPLEX.bam && \
-					 unlink $1_cl_aln_srt_MD_IR_FX2__grp_DC_MA_RG_IR_FX_SIMPLEX.bai && \
+					 ln -sf ../bam/$1_cl_aln_srt_MD_IR_FX_BR__grp_DC_MA_RG_IR_FX_SIMPLEX.bam $1_cl_aln_srt_MD_IR_FX_BR__grp_DC_MA_RG_IR_FX_SIMPLEX.bam && \
+					 ln -sf ../bam/$1_cl_aln_srt_MD_IR_FX_BR__grp_DC_MA_RG_IR_FX_SIMPLEX.bai $1_cl_aln_srt_MD_IR_FX_BR__grp_DC_MA_RG_IR_FX_SIMPLEX.bai && \
+					 $$(call WALTZ_CMD,2G,8G) org.mskcc.juber.waltz.Waltz PileupMetrics $$(WALTZ_MIN_MAPQ) $1_cl_aln_srt_MD_IR_FX_BR__grp_DC_MA_RG_IR_FX_SIMPLEX.bam $$(REF_FASTA) $$(WALTZ_BED_FILE) && \
+					 gzip $1_cl_aln_srt_MD_IR_FX_BR__grp_DC_MA_RG_IR_FX_SIMPLEX-pileup.txt && \
+					 gzip $1_cl_aln_srt_MD_IR_FX_BR__grp_DC_MA_RG_IR_FX_SIMPLEX-pileup-without-duplicates.txt && \
+					 gzip $1_cl_aln_srt_MD_IR_FX_BR__grp_DC_MA_RG_IR_FX_SIMPLEX-intervals.txt && \
+					 gzip $1_cl_aln_srt_MD_IR_FX_BR__grp_DC_MA_RG_IR_FX_SIMPLEX-intervals-without-duplicates.txt && \
+					 unlink $1_cl_aln_srt_MD_IR_FX_BR__grp_DC_MA_RG_IR_FX_SIMPLEX.bam && \
+					 unlink $1_cl_aln_srt_MD_IR_FX_BR__grp_DC_MA_RG_IR_FX_SIMPLEX.bai && \
 					 cd ..")
 
 waltz/$1_cl_aln_srt_MD_IR_FX_BR__grp_DC_MA_RG_IR_FX_DUPLEX-pileup.txt.gz : bam/$1_cl_aln_srt_MD_IR_FX_BR__grp_DC_MA_RG_IR_FX_DUPLEX.bam
 	$$(call RUN,-c -n 4 -s 4G -m 6G,"set -o pipefail && \
 					 mkdir -p waltz && \
 					 cd waltz && \
-					 ln -sf ../bam/$1_cl_aln_srt_MD_IR_FX2__grp_DC_MA_RG_IR_FX_DUPLEX.bam $1_cl_aln_srt_MD_IR_FX2__grp_DC_MA_RG_IR_FX_DUPLEX.bam && \
-					 ln -sf ../bam/$1_cl_aln_srt_MD_IR_FX2__grp_DC_MA_RG_IR_FX_DUPLEX.bai $1_cl_aln_srt_MD_IR_FX2__grp_DC_MA_RG_IR_FX_DUPLEX.bai && \
-					 $$(call WALTZ_CMD,2G,8G) org.mskcc.juber.waltz.Waltz PileupMetrics $$(WALTZ_MIN_MAPQ) $1_cl_aln_srt_MD_IR_FX2__grp_DC_MA_RG_IR_FX_DUPLEX.bam $$(REF_FASTA) $$(WALTZ_BED_FILE) && \
-					 gzip $1_cl_aln_srt_MD_IR_FX2__grp_DC_MA_RG_IR_FX_DUPLEX-pileup.txt && \
-					 gzip $1_cl_aln_srt_MD_IR_FX2__grp_DC_MA_RG_IR_FX_DUPLEX-pileup-without-duplicates.txt && \
-					 gzip $1_cl_aln_srt_MD_IR_FX2__grp_DC_MA_RG_IR_FX_DUPLEX-intervals.txt && \
-					 gzip $1_cl_aln_srt_MD_IR_FX2__grp_DC_MA_RG_IR_FX_DUPLEX-intervals-without-duplicates.txt && \
-					 unlink $1_cl_aln_srt_MD_IR_FX2__grp_DC_MA_RG_IR_FX_DUPLEX.bam && \
-					 unlink $1_cl_aln_srt_MD_IR_FX2__grp_DC_MA_RG_IR_FX_DUPLEX.bai && \
+					 ln -sf ../bam/$1_cl_aln_srt_MD_IR_FX_BR__grp_DC_MA_RG_IR_FX_DUPLEX.bam $1_cl_aln_srt_MD_IR_FX_BR__grp_DC_MA_RG_IR_FX_DUPLEX.bam && \
+					 ln -sf ../bam/$1_cl_aln_srt_MD_IR_FX_BR__grp_DC_MA_RG_IR_FX_DUPLEX.bai $1_cl_aln_srt_MD_IR_FX_BR__grp_DC_MA_RG_IR_FX_DUPLEX.bai && \
+					 $$(call WALTZ_CMD,2G,8G) org.mskcc.juber.waltz.Waltz PileupMetrics $$(WALTZ_MIN_MAPQ) $1_cl_aln_srt_MD_IR_FX_BR__grp_DC_MA_RG_IR_FX_DUPLEX.bam $$(REF_FASTA) $$(WALTZ_BED_FILE) && \
+					 gzip $1_cl_aln_srt_MD_IR_FX_BR__grp_DC_MA_RG_IR_FX_DUPLEX-pileup.txt && \
+					 gzip $1_cl_aln_srt_MD_IR_FX_BR__grp_DC_MA_RG_IR_FX_DUPLEX-pileup-without-duplicates.txt && \
+					 gzip $1_cl_aln_srt_MD_IR_FX_BR__grp_DC_MA_RG_IR_FX_DUPLEX-intervals.txt && \
+					 gzip $1_cl_aln_srt_MD_IR_FX_BR__grp_DC_MA_RG_IR_FX_DUPLEX-intervals-without-duplicates.txt && \
+					 unlink $1_cl_aln_srt_MD_IR_FX_BR__grp_DC_MA_RG_IR_FX_DUPLEX.bam && \
+					 unlink $1_cl_aln_srt_MD_IR_FX_BR__grp_DC_MA_RG_IR_FX_DUPLEX.bai && \
 					 cd ..")
 
 endef
