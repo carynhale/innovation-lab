f625060230495baa1de8bb058066596833ccf5e6
David Brown <ndbrown6@gmail.com>
2021-09-27 17:18:18 -0400

    Update extract_fastq.mk

--- fastq_tools/extract_fastq.mk
+++ fastq_tools/extract_fastq.mk
@@ -4,7 +4,7 @@ LOGDIR ?= log/extract_fastq.$(NOW)
 
 extract_fastq : $(foreach sample,$(SAMPLES),fastq/$(sample).1.fastq.gz)
 
-fastq/%.1.fastq.gz fastq/%.2.fastq.gz : %.bam
+fastq/%.1.fastq.gz fastq/%.2.fastq.gz : bam/%.bam
 	$(call RUN,-n 4 -s 4G -m 9G,"$(SAMTOOLS) sort -T $(<D)/$* -O bam -n -@ 4 -m 6G $< | \
 				     $(SAMTOOLS) fastq -f 1 -1 >(gzip -c > fastq/$*.1.fastq.gz) -2 >(gzip -c > fastq/$*.2.fastq.gz) -")
 
