0d907e358ba344aaf2fb2f5917a80aeea25c36c4
David Brown <ndbrown6@gmail.com>
2022-03-04 12:44:30 -0500

    Update sum_reads.mk

--- rna_seq/sum_reads.mk
+++ rna_seq/sum_reads.mk
@@ -2,46 +2,46 @@ include innovation-lab/Makefile.inc
 
 LOGDIR = log/sum_reads.$(NOW)
 
-SUM_READS_RSCRIPT = $(SCRIPTS_DIR)/rna_seq/summarize_rnaseqreads.R
-SUM_EXONS_RSCRIPT = $(SCRIPTS_DIR)/rna_seq/summarize_rnaseqreads_byexon.R
+sum_reads : $(foreach sample,$(SAMPLES),sumreads/$(sample)_bygene.txt)
+#	    $(foreach sample,$(SAMPLES),sumreads/$(sample)_byexon.txt)
+#	    sumreads/rpkm_bygene.txt \
+#	    sumreads/counts_bygene.txt \
+#	    sumreads/rpkm_byexon.txt \
+#           sumreads/counts_byexon.txt
 
-TARGETS_FILE = $(HOME)/share/lib/resource_files/IDT_exome_research_targets.txt
+define sum-reads
+sumreads/$1_bygene.txt : bam/$1.bam bam/$1.bam.bai
+	$$(call RUN,-n 1 -s 24G -m 48G -v $(SUMREADS_ENV),"$$(RSCRIPT) $$(SCRIPTS_DIR)/rna_seq/summarize_sumreads.R \
+							   --option 1 \
+							   --in_file $$(<) \
+							   --out_file $$(@)")
 
-sum_reads : $(foreach sample,$(SAMPLES),sumreads/$(sample).sumreads_bygene.txt) \
-	    sumreads/rpkm_bygene.txt \
-	    sumreads/counts_bygene.txt \
-            $(foreach sample,$(SAMPLES),sumreads/$(sample).sumreads_byexon.txt) \
-	    sumreads/rpkm_byexon.txt \
-            sumreads/counts_byexon.txt
+#sumreads/$1_byexon.txt : bam/$1.bam bam/$1.bam.bai
+#	$$(call RUN,-n 1 -s 24G -m 48G -v $(SUMREADS_ENV),"$$(RSCRIPT) $$(SCRIPTS_DIR)/rna_seq/summarize_sumreads.R \
+#							   --option 2 \
+#							   --in_file $$(<) \
+#							   --out_file $$(@)")
+							  
+endef
+$(foreach sample,$(SAMPLES),\
+		$(eval $(call sum-reads,$(sample))))
 
-sumreads/%.sumreads_bygene.txt : bam/%.bam bam/%.bam.bai
-	$(call RUN,-n 1 -s 24G -m 48G -v $(SUMREADS_ENV),"$(RSCRIPT) $(SUM_READS_RSCRIPT) \
-							  --genome $(REF) \
-							  --in_file $(<) \
-							  --out_file $(@)")
 
-sumreads/%.sumreads_byexon.txt : bam/%.bam bam/%.bam.bai
-	$(call RUN,-n 1 -s 24G -m 48G -v $(SUMREADS_ENV),"$(RSCRIPT) $(SUM_EXONS_RSCRIPT) \
-							  --genome $(REF) \
-							  --in_file $(<) \
-							  --out_file $(@) \
-							  --target_file $(TARGETS_FILE)")
-
-sumreads/rpkm_bygene.txt : $(foreach sample,$(SAMPLES),sumreads/$(sample).sumreads_bygene.txt)
-	cut -f 2 $< > $@; \
-	for x in $^; do sample=`echo $$x | sed 's/.*\///; s/\..*//'`; cut -f 7 $$x | sed "s/exonRPKM/$$sample/" | paste $@ - > $@.tmp; mv $@.tmp $@; done
-
-sumreads/rpkm_byexon.txt : $(foreach sample,$(SAMPLES),sumreads/$(sample).sumreads_byexon.txt)
-	cut -f 1-2 $< > $@; \
-	for x in $^; do sample=`echo $$x | sed 's/.*\///; s/\..*//'`; cut -f 6 $$x | sed "s/exonRPKM/$$sample/" | paste $@ - > $@.tmp; mv $@.tmp $@; done
-
-sumreads/counts_bygene.txt : $(foreach sample,$(SAMPLES),sumreads/$(sample).sumreads_bygene.txt)
-	cut -f 2 $< > $@; \
-	for x in $^; do sample=`echo $$x | sed 's/.*\///; s/\..*//'`; cut -f 3 $$x | sed "s/countsByGene/$$sample/" | paste $@ - > $@.tmp; mv $@.tmp $@; done
-
-sumreads/counts_byexon.txt : $(foreach sample,$(SAMPLES),sumreads/$(sample).sumreads_byexon.txt)
-	cut -f 1-2 $< > $@; \
-	for x in $^; do sample=`echo $$x | sed 's/.*\///; s/\..*//'`; cut -f 4 $$x | sed "s/exonCount/$$sample/" | paste $@ - > $@.tmp; mv $@.tmp $@; done
+#sumreads/rpkm_bygene.txt : $(foreach sample,$(SAMPLES),sumreads/$(sample)_bygene.txt)
+#	cut -f 2 $< > $@; \
+#	for x in $^; do sample=`echo $$x | sed 's/.*\///; s/\..*//'`; cut -f 7 $$x | sed "s/exonRPKM/$$sample/" | paste $@ - > $@.tmp; mv $@.tmp $@; done
+#
+#sumreads/rpkm_byexon.txt : $(foreach sample,$(SAMPLES),sumreads/$(sample).sumreads_byexon.txt)
+#	cut -f 1-2 $< > $@; \
+#	for x in $^; do sample=`echo $$x | sed 's/.*\///; s/\..*//'`; cut -f 6 $$x | sed "s/exonRPKM/$$sample/" | paste $@ - > $@.tmp; mv $@.tmp $@; done
+#
+#sumreads/counts_bygene.txt : $(foreach sample,$(SAMPLES),sumreads/$(sample).sumreads_bygene.txt)
+#	cut -f 2 $< > $@; \
+#	for x in $^; do sample=`echo $$x | sed 's/.*\///; s/\..*//'`; cut -f 3 $$x | sed "s/countsByGene/$$sample/" | paste $@ - > $@.tmp; mv $@.tmp $@; done
+#
+#sumreads/counts_byexon.txt : $(foreach sample,$(SAMPLES),sumreads/$(sample).sumreads_byexon.txt)
+#	cut -f 1-2 $< > $@; \
+#	for x in $^; do sample=`echo $$x | sed 's/.*\///; s/\..*//'`; cut -f 4 $$x | sed "s/exonCount/$$sample/" | paste $@ - > $@.tmp; mv $@.tmp $@; done
 
 
 ..DUMMY := $(shell mkdir -p version; \
