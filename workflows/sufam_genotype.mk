3f4c12ff8f59b9b5ee709292c6392083f8a7f8f3
David Brown <ndbrown6@gmail.com>
2022-04-11 10:44:53 -0400

    SUFAM

--- workflows/sufam_genotype.mk
+++ workflows/sufam_genotype.mk
@@ -3,8 +3,8 @@ include innovation-lab/genome_inc/b37.inc
 
 LOGDIR ?= log/sufam_genotype.$(NOW)
 
-sufam_genotype : $(foreach sample,$(SAMPLES),sufam_genotype/$(sample).txt) \
-		 summary/summary_genotype.txt
+sufam_genotype : $(foreach sample,$(SAMPLES),sufam_genotype/$(sample).txt)
+#		 summary/summary_genotype.txt
 
 MPILEUP_PARAMETERS = --count-orphans \
 		     --ignore-RG \
@@ -13,36 +13,28 @@ MPILEUP_PARAMETERS = --count-orphans \
 		     --max-idepth 250000
 
 define sufam-genotype
-sufam_genotype/$1.txt : bam/
+sufam_genotype/$1.txt : bam/$1.bam vcf/$1.vcf
 	$$(call RUN,-c -n 1 -s 4G -m 8G -v $(SUFAM_ENV),"set -o pipefail && \
 							 mkdir -p sufam_genotype && \
-								      cd genotype_variants && \
-								      $$(GENOTYPE_VARIANTS) small_variants all \
-								      -i ../maf/$1.maf \
-								      -r $$(REF_FASTA) \
-								      -p $1 \
-								      -b ../bam/$1_cl_aln_srt_MD_IR_FX_BR.bam \
-								      -d ../bam/$1_cl_aln_srt_MD_IR_FX_BR__aln_srt_IR_FX-duplex.bam \
-								      -s ../bam/$1_cl_aln_srt_MD_IR_FX_BR__aln_srt_IR_FX-simplex.bam \
-								      -g $$(GBCMS_PATH) \
-								      -fd $$(FILTER_DUPLICATES) \
-								      -fc $$(FRAGMENT_COUNT) \
-								      -mapq $$(MAPPING_QUALITY) \
-								      -t $$(THREADS) && \
-								      echo 'task completed!' > $1.taskcomplete")
+							 $$(SUFAM) \
+							 $$(REF_FASTA) \
+							 vcf/$1.vcf \
+							 bam/$1.bam \
+							 2> sufam_genotype/$1.log \
+							 > sufam_genotype/$1.txt")
 									 
 endef
 $(foreach sample,$(SAMPLES),\
 		$(eval $(call sufam-genotype,$(sample))))
 		
-summary/summary_genotype.txt : $(foreach sample,$(SAMPLES),genotype_variants/$(sample).taskcomplete)
+summary/summary_genotype.txt : $(foreach sample,$(SAMPLES),sufam_genotype/$(sample).txt)
 	$(call RUN, -c -n 1 -s 12G -m 24G,"set -o pipefail && \
-					  $(RSCRIPT) $(SCRIPTS_DIR)/summary/genotype_access.R --sample_names '$(SAMPLES)'")
+					  $(RSCRIPT) $(SCRIPTS_DIR)/summary/sufam_summary.R --sample_names '$(SAMPLES)'")
 
 		
 ..DUMMY := $(shell mkdir -p version; \
 	     $(SUFAM_ENV)/bin/$(SAMTOOLS) --version > version/sufam_genotype.txt; \
-	     $(SUFAM_ENV)/bin/sufam --version &>> version/sufam_genotype.txt; \
+	     $(SUFAM_ENV)/bin/$(SUFAM) --version &>> version/sufam_genotype.txt; \
 	     R --version >> version/sufam_genotype.txt)
 .DELETE_ON_ERROR:
 .SECONDARY:
