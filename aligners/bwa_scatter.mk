c232820f2fddd95346ab63a38857ac2772860cdc
David Brown <ndbrown6@gmail.com>
2022-05-05 14:15:18 -0400

    Update bwa_scatter.mk

--- aligners/bwa_scatter.mk
+++ aligners/bwa_scatter.mk
@@ -9,7 +9,6 @@ bwa_scatter : $(foreach sample,$(SAMPLES),bwa_scatter/$(sample)/$(sample)_R1.fas
 	      $(foreach sample,$(SAMPLES),bwa_scatter/$(sample)/$(sample)_R1.taskcomplete)
 	       
 CHUNKS = 10
-FASTQ_CHUNKS = $(shell seq 1 $(CHUNKS))
 
 define merge-fastq
 bwa_scatter/$1/$1_R1.fastq.gz : $$(foreach split,$2,$$(word 1, $$(fq.$$(split))))
@@ -25,6 +24,7 @@ define split-fastq
 bwa_scatter/$1/$1_R1.taskcomplete : bwa_scatter/$1/$1_R1.fastq.gz
 	$$(call RUN,-c -n $(CHUNKS) -s 1G -m 2G -v $(FASTQ_SPLITTER_ENV),"set -o pipefail && \
 									  cd bwa_scatter/$1 && \
+									  FASTQ_CHUNKS=$(shell seq 1 $(CHUNKS)) && \
 									  ALL_CHUNKS=() && \
 									  for chunk in '$${FASTQ_CHUNKS[@]}'; do ALL_CHUNKS+=" -o "$1.1.$(chunk).fastq.gz; done; && \
 									  fastqsplitter -i $1_R1.fastq.gz $${ALL_CHUNKS[@]} && \
