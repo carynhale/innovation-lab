7f029d3b101ba082cac6d7922a1ccfc5441219d0
David Brown <ndbrown6@gmail.com>
2022-07-13 18:05:15 -0400

    Update bwa_split.mk

--- aligners/bwa_split.mk
+++ aligners/bwa_split.mk
@@ -5,13 +5,14 @@ include innovation-lab/config/align.inc
 LOGDIR ?= log/bwa_split.$(NOW)
 
 FASTQ_SPLIT = 500
+FASTQ_SEQ = $(shell seq 1 $(FASTQ_SPLIT))
 
 bwa_split : $(foreach sample,$(SAMPLES),bwamem/$(sample)/$(sample)_R1.fastq.gz) \
 	    $(foreach sample,$(SAMPLES),bwamem/$(sample)/$(sample)_R2.fastq.gz) \
 	    $(foreach sample,$(SAMPLES),bwamem/$(sample)/taskcomplete_r1.txt) \
 	    $(foreach sample,$(SAMPLES),bwamem/$(sample)/taskcomplete_r2.txt) \
 	    $(foreach sample,$(SAMPLES), \
-		  	$(foreach n,$(FASTQ_SPLIT),bwamem/$(sample)/$(sample)_aln--$(n).bam))
+		  	$(foreach n,$(FASTQ_SEQ),bwamem/$(sample)/$(sample)_aln--$(n).bam))
 	    
 BWAMEM_THREADS = 12
 BWAMEM_MEM_PER_THREAD = 2G
@@ -67,7 +68,7 @@ bwamem/$1/$1_aln--$2.bam : bwamem/$1/taskcomplete_r1.txt bwamem/$1/taskcomplete_
 									       $$(SAMTOOLS) view -bhS - > $$(@)")
 endef
 $(foreach sample,$(SAMPLES), \
-	$(foreach n,$(FASTQ_SPLIT), \
+	$(foreach n,$(FASTQ_SEQ), \
 		$(eval $(call fastq-2bam,$(sample),$(n)))))
 
 ..DUMMY := $(shell mkdir -p version; \
