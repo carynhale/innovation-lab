487ec11c54433466493b289657723efa2aa85ac0
David Brown <ndbrown6@gmail.com>
2022-07-19 10:54:55 -0400

    Update bwa_split.mk

--- aligners/bwa_split.mk
+++ aligners/bwa_split.mk
@@ -92,17 +92,17 @@ bwamem/$1/$1--$2_aln.bam : bwamem/$1/$1--$(FASTQ_SPLIT)_R1.fastq.gz bwamem/$1/$1
 									       
 bwamem/$1/$1--$2_cl.fastq.gz : bwamem/$1/$1--$2_aln.bam
 	$$(call RUN,-c -n 1 -s 1G -m 2G,"set -o pipefail && \
-					  $$(MARK_ADAPTERS) \
-					  INPUT=$$(<) \
-					  OUTPUT=/dev/stdout \
-					  METRICS=bwamem/$1/$1--$2_adapter-metrics.txt | \
-					  $$(SAM_TO_FASTQ) \
-					  INPUT=/dev/stdin \
-					  FASTQ=$$(@) \
-					  INTERLEAVE=true \
-					  CLIPPING_ATTRIBUTE=XT \
-					  CLIPPING_ACTION=X \
-					  CLIPPING_MIN_LENGTH=25")
+					 $$(MARK_ADAPTERS) \
+					 INPUT=$$(<) \
+					 OUTPUT=/dev/stdout \
+					 METRICS=bwamem/$1/$1--$2_adapter-metrics.txt | \
+					 $$(SAM_TO_FASTQ) \
+					 INPUT=/dev/stdin \
+					 FASTQ=$$(@) \
+					 INTERLEAVE=true \
+					 CLIPPING_ATTRIBUTE=XT \
+					 CLIPPING_ACTION=X \
+					 CLIPPING_MIN_LENGTH=25")
 									       
 bwamem/$1/$1--$2_cl_aln.bam : bwamem/$1/$1--$2_cl.fastq.gz
 	$$(call RUN,-c -n $(BWAMEM_THREADS) -s 1G -m $(BWAMEM_MEM_PER_THREAD),"set -o pipefail && \
