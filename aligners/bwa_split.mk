26d8c3f0de5ba9d2a39015dfbb9b0109904528f8
David Brown <ndbrown6@gmail.com>
2022-07-19 16:26:16 -0400

    Update bwa_split.mk

--- aligners/bwa_split.mk
+++ aligners/bwa_split.mk
@@ -60,7 +60,7 @@ $(foreach sample,$(SAMPLES),\
 		$(eval $(call merge-fastq,$(sample),$(split.$(sample)))))
 		
 define split-fastq
-$(foreach n,$(FASTQ_SEQ),bwamem/$1/$1--$(n)_R1.fastq.gz) &: bwamem/$1/$1_R1.fastq.gz
+$(wildcard $(foreach n,$(FASTQ_SEQ),bwamem/$1/$1--$(n)_R1.fastq.gz)) &: bwamem/$1/$1_R1.fastq.gz
 	$$(call RUN,-c -n $(SPLIT_THREADS) -s 1G -m $(SPLIT_MEM_THREAD) -v $(FASTQ_SPLITTER_ENV),"set -o pipefail && \
 												  $(SCRIPTS_DIR)/fastq_tools/split_fastq.sh \
 												  $$(FASTQ_SPLIT) \
