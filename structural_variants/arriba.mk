cbfb0965eb35854539afb833a62d38fc19ab72dc
David Brown <ndbrown6@gmail.com>
2023-04-21 13:45:54 -0400

    Update arriba.mk

--- structural_variants/arriba.mk
+++ structural_variants/arriba.mk
@@ -64,7 +64,7 @@ arriba/$1/fusions.tsv : arriba/$1/$1.Aligned.out.bam
 							    -p $$(PROTEIN_DOMAINS_GFF3)")
 							    
 arriba/$1/fusions.pdf : arriba/$1/fusions.tsv arriba/$1/$1.Aligned.out.bam
-	$$(call RUN,-c -n 1 -s 24G -m 36G -v $(GENOMIC_ALIGNMENTS_ENV),"set -o pipefail && \
+	$$(call RUN,-c -n 1 -s 12G -m 24G -v $(GENOMIC_ALIGNMENTS_ENV),"set -o pipefail && \
 									$$(RSCRIPT) $$(DRAW_FUSIONS) \
 									--fusions=$$(<) \
 									--annotation=$$(ANNOTATION_GTF) \
